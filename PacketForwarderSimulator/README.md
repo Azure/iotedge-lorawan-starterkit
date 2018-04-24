6b006e1b52903b196101c72257520942cf5212af
Chatillon Mikhail <Mikhail>
2018-04-24 16:04:34 -0700

    added simulator

--- PacketForwarderSimulator/README.md
+++ PacketForwarderSimulator/README.md
@@ -3,3 +3,6 @@
 Command line tool that broadcasts recorded LoRa UDP packets to host and port specified on the command line.
 
 Currently plays back two recorded packets two times. The second playback is used to test frame duplication scenarios.
+
+Message sent will be like (gatewayinfo|jsonpayload) where gatewayinfo is a sequence of bytes taken from a LoRa Payload. The jsonpayload can be edited on the inputJson variable.
+Put the url/port of your udp listener in line 29.
---
+++ PacketForwarderSimulator/Simulator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Simulator
+{
+    class Program
+    {
+       
+       
+    
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                byte[] leadingByte = StringToByteArray("0205DB00AA555A0000000101");
+                string inputJson = "{\"rxpk\":[{\"tmst\":3121882787,\"chan\":2,\"rfch\":1,\"freq\":868.500000,\"stat\":1,\"modu\":\"LORA\",\"datr\":\"SF7BW125\",\"codr\":\"4/5\",\"lsnr\":7.0,\"rssi\":-16,\"size\":20,\"data\":\"QEa5KACANwAIXiRAODD6gSCHMSk=\"}]}";
+                byte[] message = leadingByte.Concat(Encoding.Default.GetBytes(inputJson)).ToArray();
+                UdpClient udpConnection = new UdpClient();
+                IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("10.0.28.34"), 1680);
+                udpConnection.Send(message, message.Length, ipEndPoint);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
