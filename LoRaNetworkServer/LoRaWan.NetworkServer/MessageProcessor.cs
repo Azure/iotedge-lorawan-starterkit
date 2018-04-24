38d3da162aa80906799ea4ad21132f25ed42cb1e
Sergiy Poplavskyi <s.poplavskiy@outlook.com>
2018-04-24 16:25:33 -0700

    Initial Commit

---
+++ LoRaNetworkServer/LoRaWan.NetworkServer/MessageProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoRaWan.NetworkServer
+{
+    public class MessageProcessor
+    {
+        public void processMessage(byte[] message)
+        {
+            //Decrypting message
+
+            Console.WriteLine(Encoding.UTF8.GetString(message));
+        }
+    }
+}
---
+++ LoRaNetworkServer/LoRaWan.NetworkServer/UdpServer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace LoRaWan.NetworkServer
+{
+    public class UdpServer
+    {
+        const int port = 1680;
+        public async void RunServer()
+        {
+            using (var udpClient = new UdpClient(port))
+            {
+                while (true)
+                {
+                    UdpReceiveResult receivedResults = await udpClient.ReceiveAsync();
+                    MessageProcessor messageProcessor = new MessageProcessor();
+                    Task.Run(() => messageProcessor.processMessage(receivedResults.Buffer));
+                }
+            }
+        }
+    }
+}
