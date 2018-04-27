756be5881f9915bb537b9eff7a84160601567ab0
Sergiy Poplavskyi <s.poplavskiy@outlook.com>
2018-04-27 11:11:01 -0700

    Added module twins.

---
+++ LoRaEngine/LoRaWan.NetworkServer/LoaraKeysManager.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Azure.Devices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoRaWan.NetworkServer
+{
+    public class LoaraKeysManager
+    {
+
+        private bool registryMode = false;
+        private RegistryManager registryManager;
+        public LoaraKeysManager(bool registrymode, string connectionString)
+        {
+            registryMode = registrymode;
+
+            if (registryMode)
+                registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+
+        }
+
+        public async Task<LoraKeys> GetKeys(string DevAddr)
+        {
+            if (registryMode)
+                return await GetKeysRegistryMode(DevAddr);
+            else
+                return null;
+        }
+
+        private async Task<LoraKeys> GetKeysRegistryMode(string DevAddr)
+        {
+
+            //need to fix sql injection attack
+            var query = registryManager.CreateQuery($"SELECT * FROM devices WHERE tags.DevAddr = '{DevAddr}'", 1);
+
+            LoraKeys loraKeys = new LoraKeys();
+            loraKeys.DevAddr = DevAddr;
+
+
+            while (query.HasMoreResults)
+            {
+                var page = await query.GetNextAsTwinAsync();
+                foreach (var twin in page)
+                {
+                    loraKeys.DeviceEUI = twin.DeviceId;
+                    loraKeys.AppSKey = twin.Tags["AppSKey"].Value;
+                    loraKeys.NwkSKey = twin.Tags["NwkSKey"].Value;
+                    loraKeys.Accept = true;
+                }
+            }
+
+            return loraKeys;
+
+        }
+    }
+
+
+
+    public class LoraKeys
+    {
+        public string DevAddr;
+        public string DeviceEUI;
+        public string NwkSKey;
+        public string AppSKey;
+        public bool Accept = false;
+    }
+}
---
+++ LoRaEngine/TestAppForEverything/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestAppForEverything
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            constructConnectionStringMask();
+            Console.ReadLine();
+        }
+
+        static string hubConnParams = "HostName=testloriotv3hub.azure-devices.net;SharedAccessKeyName=device;SharedAccessKey=Wt1vLxOMHtCVTILfxzFBAyE3wWguBQOM8NM14dz8YVw=";
+        static string originalConnectionString = "HostName=testloriotv3hub.azure-devices.net;GatewayHostName=cehackpi1;DeviceId=LoraEdgeGatewayPiRonnie;ModuleId=loraudpfiltermodule;SharedAccessKey=6YuLw1kWK0VKXgESQc6+T3Wc/WOOXSxB3km0WIATq5Q=";
+        static string connectionStringMask;
+        static void constructConnectionStringMask()
+        {
+            //Constructing new Connection String
+            connectionStringMask += $"HostName={getVal("HostName", originalConnectionString)};";
+            connectionStringMask += $"GatewayHostName={getVal("GatewayHostName", originalConnectionString)};";
+            connectionStringMask += $"DeviceId=<deviceId>;";
+            connectionStringMask += $"SharedAccessKeyName={getVal("SharedAccessKeyName", hubConnParams)};";
+            connectionStringMask += $"SharedAccessKey={getVal("SharedAccessKey", hubConnParams)};";
+
+            string getVal(string key, string connStr)
+            {
+                string val = null;
+
+                foreach (var keyVal in connStr.Split(';'))
+                {
+                    int splIndex = keyVal.IndexOf('=');
+                    string k = keyVal.Substring(0, splIndex);
+                    if(k.ToLower().Trim() == key.ToLower().Trim())
+                    {
+                        val = keyVal.Substring(splIndex + 1, keyVal.Length - splIndex - 1);
+                        break;
+                    }
+                }
+
+                if(string.IsNullOrEmpty(val))
+                {
+                    throw new Exception($"Key '{key}' not found.");
+                }
+                else
+                {
+                    return val;
+                }
+            }
+        }
+    }
+}
