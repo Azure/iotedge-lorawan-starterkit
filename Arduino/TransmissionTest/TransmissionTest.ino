3179dd4df4f864e1c20693306b63c766ecc26598
ronniesa <ronnies@microsoft.com>
2018-08-10 09:57:18 +0200

    Update TransmissionTest.ino

--- Arduino/TransmissionTest/TransmissionTest.ino
+++ Arduino/TransmissionTest/TransmissionTest.ino
@@ -11,7 +11,9 @@ char* appEui ="BE7A0000000014E2";
 iot hub OTAA tags for deviceid: 47AAC86800430028 
     "tags": {
     "AppEUI": "BE7A0000000014E2",
-    "AppKey": "8AFE71A145B253E49C3031AD068277A1"  
+    "AppKey": "8AFE71A145B253E49C3031AD068277A1",
+    "GatewayID" :"",
+    "SensorDecoder" :"DecoderRotatorySensor"
     },
   */
 
