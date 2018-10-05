8f74ff251924cdd3b897411c74e4ebafe815db53
Mikhail <mikhail.chatillon@hotmail.com>
2018-10-05 10:56:55 +0200

    fix2

--- LoRaEngine/modules/LoRaWanPktFwdModule/start_pktfwd.sh
+++ LoRaEngine/modules/LoRaWanPktFwdModule/start_pktfwd.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-if ["$region"=="US"]
-  echo "US region detected." 
-  cp global_conf.us.json global_conf.json 
+
+
+if [[ -z "${region}" ]]; then
+     echo "No region detected in environment variables, defaulting to EU" 
+else
+    if ["$region"=="US"]
+        echo "US region detected." 
+        cp global_conf.us.json global_conf.json 
+    fi
 fi
 ./reset_lgw.sh start $RESET_PIN
 ./lora_pkt_fwd
