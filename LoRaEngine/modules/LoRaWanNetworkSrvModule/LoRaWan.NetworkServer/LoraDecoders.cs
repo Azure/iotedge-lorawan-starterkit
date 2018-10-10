8973612fe642891d5fb9099b01335ee806cbba71
Kai Zimmermann <kai.zimmermann@microsoft.com>
2018-10-10 16:57:40 -0700

    Add new Geo decoder

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/LoraDecoders.cs
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/LoraDecoders.cs
@@ -7,11 +7,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace LoRaWan.NetworkServer
 {
-
-
     class LoraDecoders
     {
         public static string DecodeMessage(byte[] payload, uint fport, string SensorDecoder)
@@ -22,8 +22,8 @@ namespace LoRaWan.NetworkServer
 
             if (toInvoke != null)
             {
-       
-                return (string)toInvoke.Invoke(null, new object[] { payload, fport});
+
+                return (string)toInvoke.Invoke(null, new object[] { payload, fport });
             }
             else
             {
@@ -31,14 +31,14 @@ namespace LoRaWan.NetworkServer
                 return $"{{\"error\": \"No '{SensorDecoder}' decoder found\", \"rawpayload\": \"{base64Payload}\"}}";
             }
         }
-               
+
         private static string DecoderGpsSensor(byte[] payload, uint fport)
         {
             var result = Encoding.ASCII.GetString(payload);
             string[] values = result.Split(':');
             return String.Format("{{\"latitude\": {0} , \"longitude\": {1}}}", values[0], values[1]);
         }
-        
+
         private static string DecoderTemperatureSensor(byte[] payload, uint fport)
         {
             var result = Encoding.ASCII.GetString(payload);
@@ -49,7 +49,26 @@ namespace LoRaWan.NetworkServer
         {
             var result = Encoding.ASCII.GetString(payload);
             return String.Format("{{\"value\": {0}}}", result);
+        }
 
+        private static string DecoderSearchAndRescueGpsSensor(byte[] bytes, uint fport)
+        {
+
+            byte[] bytes_lat = { bytes[0], bytes[1], bytes[2], bytes[3] };
+            byte[] bytes_lng = { bytes[4], bytes[5], bytes[6], bytes[7] };
+
+
+            dynamic decoded = new JObject();
+
+            //default lora mote
+            var lat_val = BitConverter.ToInt32(bytes_lat, 0);
+            var lng_val = BitConverter.ToInt32(bytes_lng, 0);
+
+            decoded.lat = (float)lat_val / Math.Pow(2, 23) * 90;
+            decoded.lng = (float)lng_val / Math.Pow(2, 23) * 180;
+
+
+            return decoded.ToString(Newtonsoft.Json.Formatting.None);
         }
     }
 
