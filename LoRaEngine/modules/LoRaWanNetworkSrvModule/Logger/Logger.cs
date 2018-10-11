416c10f6168e89a696d543c696b7bf802c5d0654
Laurent Ellerbach <laurelle@microsoft.com>
2018-10-11 10:06:41 -0700

    Added support for UDP loging for Tests as well as VSTS container to run on the edge and run tests

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/Logger/Logger.cs
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/Logger/Logger.cs
@@ -1,25 +1,43 @@
-﻿using Microsoft.Azure.Devices.Client;
+﻿
+
+using Microsoft.Azure.Devices.Client;
 using System;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace LoRaWan
 {
- 
+
     public class Logger
     {
 
-        public enum LoggingLevel : int { Always=0, Full, Info, Error };
+        public enum LoggingLevel : int { Always = 0, Full, Info, Error };
 
         private static ModuleClient edgeModuleClient;
+
+        const int PORT = 1234;
+
+        static UdpClient udpClient;
+
+        static bool logToTest = false;
 
         public static void Init(ModuleClient moduleClient)
         {
             edgeModuleClient = moduleClient;
+            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("LOG_TO_TEST")))
+            {
+                logToTest = true;
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, PORT);
+                udpClient = new UdpClient(endPoint);
+            }
         }
 
         public static void Log(string message, LoggingLevel loggingLevel)
         {
             Log(null, message, loggingLevel);
+
         }
 
         public static void Log(string deviceId, string message, LoggingLevel loggingLevel)
@@ -27,7 +45,7 @@ namespace LoRaWan
             bool logToHub = false;
             bool logToConsole = true;
 
-            int loggingLevelSetting = (int)LoggingLevel.Always;
+            int loggingLevelSetting = (int)LoggingLevel.Error;
 
             if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("LOG_TO_HUB")))
                 logToHub = bool.Parse(Environment.GetEnvironmentVariable("LOG_TO_HUB"));
@@ -39,7 +57,7 @@ namespace LoRaWan
             if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("LOG_TO_CONSOLE")))
                 logToConsole = bool.Parse(Environment.GetEnvironmentVariable("LOG_TO_CONSOLE"));
 
-            if ((int)loggingLevel >= loggingLevelSetting  || loggingLevel == LoggingLevel.Always)
+            if ((int)loggingLevel >= loggingLevelSetting || loggingLevel == LoggingLevel.Always)
             {
 
                 string msg = "";
@@ -47,19 +65,31 @@ namespace LoRaWan
                 if (string.IsNullOrEmpty(deviceId))
                     msg = message;
                 else
-                    msg = $"{deviceId}: { message}";
+                    msg = $"{deviceId}: {message}";
 
-                if (logToHub)
+                if (logToHub || logToTest)
                 {
+                    var msgProp = new Message(UTF8Encoding.ASCII.GetBytes(msg));
+                    msgProp.Properties.Add("log", "1");
                     if (edgeModuleClient != null)
-                        edgeModuleClient.SendEventAsync(new Message(UTF8Encoding.ASCII.GetBytes(msg)));
-
+                        edgeModuleClient.SendEventAsync(msgProp);
+                    if (logToTest)
+                    {
+                        if (udpClient != null)
+                            UdpSendMessage(Encoding.UTF8.GetBytes(msg)).GetAwaiter().GetResult();
+                    }
                 }
                 if (logToConsole)
                     Console.WriteLine(msg);
+            }            
+        }
+
+        public static async Task UdpSendMessage(byte[] messageToSend)
+        {
+            if (messageToSend != null && messageToSend.Length != 0)
+            {
+                await udpClient.SendAsync(messageToSend, messageToSend.Length, IPAddress.Broadcast.ToString(), PORT);
             }
         }
     }
 }
-
-
