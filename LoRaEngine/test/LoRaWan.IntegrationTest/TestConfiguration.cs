8eb7d8e9281db0c8a1bf6c56fa2c4635bccb24c6
fbeltrao <fbeltrao@windowslive.com>
2018-11-06 15:03:12 +0100

    Initial pipeline commit

---
+++ LoRaEngine/test/LoRaWan.IntegrationTest/TestConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace LoRaWan.IntegrationTest
+{
+    public class TestConfiguration
+    {
+        public static TestConfiguration GetConfiguration()
+        {
+            var result = new TestConfiguration();
+
+            new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddJsonFile("appsettings.local.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build()
+                .GetSection("testConfiguration")
+                .Bind(result);
+
+            return result;
+        }
+
+
+
+        public string IoTHubEventHubConnectionString { get; set; }
+        public string IoTHubConnectionString { get; set; }
+        public int EnsureHasEventDelayBetweenReadsInSeconds { get; set; } = 2;
+        public int EnsureHasEventMaximumTries { get; set; } = 5;
+        public string IoTHubEventHubConsumerGroup { get; set; } = "$Default";
+
+        public string LeafDeviceSerialPort { get; set; } = "/dev/ttyACM";
+
+        public string LeafDeviceOTAAId { get; set; }
+        public string LeafDeviceAppKey { get; set; }
+        public string LeafDeviceAppEui { get; set; }
+        public string LeafDeviceABPId { get; set; }
+        public string LeafDeviceABPAddr { get; set; }
+        public string LeafDeviceABPAppSKey { get; set; }
+        public string LeafDeviceABPNetworkSKey { get; set; }
+    }
+
+}
