63eb06586f5134d185d0438c093886d0240b6b9e
Sascha Corti <sascha@corti.com>
2018-12-18 14:55:39 +0100

    Add sample for decoding binary payload
    - Add ByteArrayToString method
    - Add example decoders for string and binary payloads
    - Remove unnecessary using statements

--- Samples/DecoderSample/Classes/LoraDecoders.cs
+++ Samples/DecoderSample/Classes/LoraDecoders.cs
@@ -1,19 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using Newtonsoft.Json;
 
 namespace SensorDecoderModule.Classes
 {
     internal static class LoraDecoders
     {   
-        private static string DecoderValueSensor(byte[] payload, uint fport)
+        private static string DecoderStringValueSensor(byte[] payload, uint fport)
         {
+            // Convert payload containing a string back to string format
             var result = Encoding.UTF8.GetString(payload);            
             return JsonConvert.SerializeObject(new { value = result });
         }
+
+        private static string DecoderBinaryValueSensor(byte[] payload, uint fport)
+        {
+            // Convert payload containing binary data to HEX string
+            var hex_string = ConversionHelper.ByteArrayToString(payload);
+            return JsonConvert.SerializeObject(new { value = hex_string });
+        }
+
     }       
 }
--- Samples/DecoderSample/Startup.cs
+++ Samples/DecoderSample/Startup.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace SensorDecoderModule
 {
