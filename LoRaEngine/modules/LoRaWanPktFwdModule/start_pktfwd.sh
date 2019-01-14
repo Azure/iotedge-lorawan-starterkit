a5af5f945828ab79ddebeed371df1babc62a9caa
Mikhail <mikhail.chatillon@hotmail.com>
2019-01-14 13:52:16 +0100

    Add SPI 1.0 support for x86
    * Need a new environment variable PKT_FWD_SPI_DEV to specify SPI_DEV if
    running on x86 gateway.

--- LoRaEngine/modules/LoRaWanPktFwdModule/start_pktfwd.sh
+++ LoRaEngine/modules/LoRaWanPktFwdModule/start_pktfwd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#get current pkt fwd region
 if [ -z "$REGION" ]; then
      echo "No region detected in environment variables, defaulting to EU" 
 else
@@ -13,5 +14,21 @@ else
         fi
     fi
 fi
+
 ./reset_lgw.sh start $RESET_PIN
-./lora_pkt_fwd
+
+#get current architecture for the mess processor
+arch="$(uname -m)"
+if [[ $arch != *"arm"* ]]; then
+    if [ "$SPI_DEV" == "2" ]; then
+        ./lora_pkt_fwd_spidev2
+    else 
+        if [ "$SPI_DEV" == "1" ]; then
+            ./lora_pkt_fwd_spidev1
+        else
+            echo "SPI_DEV variables not present or valid in a x86 architecture. Please select a valid value (1 or 2)."
+        fi
+    fi
+else
+    ./lora_pkt_fwd
+fi  
