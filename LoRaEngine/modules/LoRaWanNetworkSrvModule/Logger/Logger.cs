9541dbebd7515606f148a1ce858822db1664b009
Sascha Corti <sascha@corti.com>
2019-01-28 08:20:30 +0100

    Removed casting of LogLevel to int for comarison.

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/Logger/Logger.cs
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/Logger/Logger.cs
@@ -66,7 +66,7 @@ namespace LoRaWan
 
         public static void Log(string deviceId, string message, LogLevel logLevel)
         {
-            if ((int)logLevel >= (int)configuration.LogLevel)
+            if (logLevel >= configuration.LogLevel)
             {
                 var msg = message;
 
