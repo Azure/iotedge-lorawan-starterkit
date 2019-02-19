3e82eb0cfbcc119193e069d4ea78eaae1c572b7a
Patrick Schuler <paschule@microsoft.com>
2019-02-19 16:24:16 +0100

    add abstraction for adr table store

---
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoraTools/ADR/ILoRaADRStore.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace LoRaTools.ADR
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Interface to implement to store ADR tables
+    /// </summary>
+    public interface ILoRaADRStore
+    {
+        Task AddTableEntry(LoRaADRTableEntry entry);
+
+        Task<LoRaADRTable> GetADRTable(string devEUI);
+    }
+}
