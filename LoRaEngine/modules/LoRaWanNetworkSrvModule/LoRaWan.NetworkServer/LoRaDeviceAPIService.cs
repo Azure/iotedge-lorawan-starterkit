6ccdef53a89b365ed9fdd972e008a82a91433d73
Patrick Schuler <paschule@microsoft.com>
2019-02-22 07:10:33 +0100

    undo some changes in the api service

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/LoRaDeviceAPIService.cs
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/LoRaDeviceAPIService.cs
@@ -5,8 +5,6 @@ namespace LoRaWan.NetworkServer
 {
     using System;
     using System.Collections.Generic;
-    using System.Net.Http;
-    using System.Net.Http.Headers;
     using System.Text;
     using System.Threading.Tasks;
     using LoRaTools.ADR;
@@ -33,107 +31,97 @@ namespace LoRaWan.NetworkServer
         {
             Logger.Log(devEUI, $"syncing FCntDown for multigateway", LogLevel.Information);
 
-            using (var client = this.serviceFacadeHttpClientProvider.GetHttpClient())
+            var client = this.serviceFacadeHttpClientProvider.GetHttpClient();
+            var url = $"{this.URL}NextFCntDown?code={this.AuthCode}&DevEUI={devEUI}&FCntDown={fcntDown}&FCntUp={fcntUp}&GatewayId={gatewayId}";
+            var response = await client.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
             {
-                var url = $"{this.URL}NextFCntDown?code={this.AuthCode}&DevEUI={devEUI}&FCntDown={fcntDown}&FCntUp={fcntUp}&GatewayId={gatewayId}";
-                var response = await client.GetAsync(url);
-                if (!response.IsSuccessStatusCode)
-                {
-                    Logger.Log(devEUI, $"error calling the NextFCntDown function, check the function log. {response.ReasonPhrase}", LogLevel.Error);
-                    return 0;
-                }
-
-                string fcntDownString = await response.Content.ReadAsStringAsync();
-
-                if (ushort.TryParse(fcntDownString, out var newFCntDown))
-                    return newFCntDown;
-
+                Logger.Log(devEUI, $"error calling the NextFCntDown function, check the function log. {response.ReasonPhrase}", LogLevel.Error);
                 return 0;
             }
+
+            string fcntDownString = await response.Content.ReadAsStringAsync();
+
+            if (ushort.TryParse(fcntDownString, out var newFCntDown))
+                return newFCntDown;
+
+            return 0;
         }
 
         public override async Task<DeduplicationResult> CheckDuplicateMsgAsync(string devEUI, int fcntUp, string gatewayId, int fcntDown)
         {
             Logger.Log(devEUI, $"check for duplicate message", LogLevel.Debug);
 
-            using (var client = this.serviceFacadeHttpClientProvider.GetHttpClient())
+            var client = this.serviceFacadeHttpClientProvider.GetHttpClient();
+            var url = $"{this.URL}DuplicateMsgCheck/{devEUI}?code={this.AuthCode}&FCntUp={fcntUp}&GatewayId={gatewayId}&FCntDown={fcntDown}";
+
+            var response = await client.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
             {
-                var url = $"{this.URL}DuplicateMsgCheck/{devEUI}?code={this.AuthCode}&FCntUp={fcntUp}&GatewayId={gatewayId}&FCntDown={fcntDown}";
-
-                var response = await client.GetAsync(url);
-                if (!response.IsSuccessStatusCode)
-                {
-                    Logger.Log(devEUI, $"error calling the DuplicateMsgCheck function, check the function log. {response.ReasonPhrase}", LogLevel.Error);
-                    return null;
-                }
-
-                var payload = await response.Content.ReadAsStringAsync();
-                Logger.Log(devEUI, $"deduplication response: '{payload}'", LogLevel.Debug);
-                return JsonConvert.DeserializeObject<DeduplicationResult>(payload);
+                Logger.Log(devEUI, $"error calling the DuplicateMsgCheck function, check the function log. {response.ReasonPhrase}", LogLevel.Error);
+                return null;
             }
+
+            var payload = await response.Content.ReadAsStringAsync();
+            Logger.Log(devEUI, $"deduplication response: '{payload}'", LogLevel.Debug);
+            return JsonConvert.DeserializeObject<DeduplicationResult>(payload);
         }
 
         public override async Task<LoRaADRResult> CalculateADRAndStoreFrame(string devEUI, LoRaADRRequest adrRequest)
         {
             Logger.Log(devEUI, $"calculate ADR and store frame info", LogLevel.Debug);
 
-            using (var client = this.serviceFacadeHttpClientProvider.GetHttpClient())
+            var client = this.serviceFacadeHttpClientProvider.GetHttpClient();
+            var url = $"{this.URL}ADRFunction/{devEUI}?code={this.AuthCode}";
+
+            var requestBody = JsonConvert.SerializeObject(adrRequest);
+
+            var response = await client.PostAsync(url, PreparePostContent(requestBody));
+            if (!response.IsSuccessStatusCode)
             {
-                var url = $"{this.URL}ADRFunction/{devEUI}?code={this.AuthCode}";
-
-                var requestBody = JsonConvert.SerializeObject(adrRequest);
-
-                var response = await client.PostAsync(url, PreparePostContent(requestBody));
-                if (!response.IsSuccessStatusCode)
-                {
-                    Logger.Log(devEUI, $"error calling the ADR function, check the function log. {response.ReasonPhrase}", LogLevel.Error);
-                    return null;
-                }
-
-                var payload = await response.Content.ReadAsStringAsync();
-                Logger.Log(devEUI, $"ADR Response: '{payload}'", LogLevel.Debug);
-                return JsonConvert.DeserializeObject<LoRaADRResult>(payload);
+                Logger.Log(devEUI, $"error calling the ADR function, check the function log. {response.ReasonPhrase}", LogLevel.Error);
+                return null;
             }
+
+            var payload = await response.Content.ReadAsStringAsync();
+            Logger.Log(devEUI, $"ADR Response: '{payload}'", LogLevel.Debug);
+            return JsonConvert.DeserializeObject<LoRaADRResult>(payload);
         }
 
         public override async Task<FunctionBundlerResult> FunctionBundler(string devEUI, FunctionBundlerRequest request)
         {
             Logger.Log(devEUI, $"Function bundler call", LogLevel.Debug);
 
-            using (var client = this.serviceFacadeHttpClientProvider.GetHttpClient())
+            var client = this.serviceFacadeHttpClientProvider.GetHttpClient();
+            var url = $"{this.URL}FunctionBundler/{devEUI}?code={this.AuthCode}";
+
+            var requestBody = JsonConvert.SerializeObject(request);
+
+            var response = await client.PostAsync(url, PreparePostContent(requestBody));
+            if (!response.IsSuccessStatusCode)
             {
-                var url = $"{this.URL}FunctionBundler/{devEUI}?code={this.AuthCode}";
-
-                var requestBody = JsonConvert.SerializeObject(request);
-
-                var response = await client.PostAsync(url, PreparePostContent(requestBody));
-                if (!response.IsSuccessStatusCode)
-                {
-                    Logger.Log(devEUI, $"error calling the ADR function, check the function log. {response.ReasonPhrase}", LogLevel.Error);
-                    return null;
-                }
-
-                var payload = await response.Content.ReadAsStringAsync();
-                Logger.Log(devEUI, $"ADR Response: '{payload}'", LogLevel.Debug);
-                return JsonConvert.DeserializeObject<FunctionBundlerResult>(payload);
+                Logger.Log(devEUI, $"error calling the ADR function, check the function log. {response.ReasonPhrase}", LogLevel.Error);
+                return null;
             }
+
+            var payload = await response.Content.ReadAsStringAsync();
+            Logger.Log(devEUI, $"ADR Response: '{payload}'", LogLevel.Debug);
+            return JsonConvert.DeserializeObject<FunctionBundlerResult>(payload);
         }
 
         public override async Task<bool> ABPFcntCacheResetAsync(string devEUI)
         {
             Logger.Log(devEUI, $"ABP FCnt cache reset for multigateway", LogLevel.Information);
-            using (var client = this.serviceFacadeHttpClientProvider.GetHttpClient())
+            var client = this.serviceFacadeHttpClientProvider.GetHttpClient();
+            var url = $"{this.URL}NextFCntDown?code={this.AuthCode}&DevEUI={devEUI}&ABPFcntCacheReset=true";
+            var response = await client.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
             {
-                var url = $"{this.URL}NextFCntDown?code={this.AuthCode}&DevEUI={devEUI}&ABPFcntCacheReset=true";
-                var response = await client.GetAsync(url);
-                if (!response.IsSuccessStatusCode)
-                {
-                    Logger.Log(devEUI, $"error calling the NextFCntDown function, check the function log, {response.ReasonPhrase}", LogLevel.Error);
-                    return false;
-                }
-
-                return true;
+                Logger.Log(devEUI, $"error calling the NextFCntDown function, check the function log, {response.ReasonPhrase}", LogLevel.Error);
+                return false;
             }
+
+            return true;
         }
 
         /// <inheritdoc />
@@ -149,70 +137,68 @@ namespace LoRaWan.NetworkServer
         /// </summary>
         async Task<SearchDevicesResult> SearchDevicesAsync(string gatewayID = null, string devAddr = null, string devEUI = null, string appEUI = null, string devNonce = null)
         {
-            using (var client = this.serviceFacadeHttpClientProvider.GetHttpClient())
+            var client = this.serviceFacadeHttpClientProvider.GetHttpClient();
+            var url = new StringBuilder();
+            url.Append(this.URL)
+                .Append("GetDevice?code=")
+                .Append(this.AuthCode);
+
+            if (!string.IsNullOrEmpty(gatewayID))
             {
-                var url = new StringBuilder();
-                url.Append(this.URL)
-                    .Append("GetDevice?code=")
-                    .Append(this.AuthCode);
-
-                if (!string.IsNullOrEmpty(gatewayID))
-                {
-                    url.Append("&GatewayId=")
-                        .Append(gatewayID);
-                }
-
-                if (!string.IsNullOrEmpty(devAddr))
-                {
-                    url.Append("&DevAddr=")
-                        .Append(devAddr);
-                }
-
-                if (!string.IsNullOrEmpty(devEUI))
-                {
-                    url.Append("&DevEUI=")
-                        .Append(devEUI);
-                }
-
-                if (!string.IsNullOrEmpty(appEUI))
-                {
-                    url.Append("&AppEUI=")
-                        .Append(appEUI);
-                }
-
-                if (!string.IsNullOrEmpty(devNonce))
-                {
-                    url.Append("&DevNonce=")
-                        .Append(devNonce);
-                }
-
-                var response = await client.GetAsync(url.ToString());
-                if (!response.IsSuccessStatusCode)
-                {
-                    if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
-                    {
-                        var badReqResult = await response.Content.ReadAsStringAsync();
-
-                        if (!string.IsNullOrEmpty(badReqResult) && string.Equals(badReqResult, "UsedDevNonce", StringComparison.InvariantCultureIgnoreCase))
-                        {
-                            Logger.Log(devEUI ?? string.Empty, $"DevNonce already used by this device", LogLevel.Information);
-                            return new SearchDevicesResult
-                            {
-                                IsDevNonceAlreadyUsed = true,
-                            };
-                        }
-                    }
-
-                    Logger.Log(devAddr, $"error calling façade api: {response.ReasonPhrase}, status: {response.StatusCode}, check the azure function log", LogLevel.Error);
-
-                    // TODO: FBE check if we return null or throw exception
-                    return new SearchDevicesResult();
-                }
-
-                var result = await response.Content.ReadAsStringAsync();
-                var devices = (List<IoTHubDeviceInfo>)JsonConvert.DeserializeObject(result, typeof(List<IoTHubDeviceInfo>));
-                return new SearchDevicesResult(devices);
+                url.Append("&GatewayId=")
+                    .Append(gatewayID);
             }
+
+            if (!string.IsNullOrEmpty(devAddr))
+            {
+                url.Append("&DevAddr=")
+                    .Append(devAddr);
+            }
+
+            if (!string.IsNullOrEmpty(devEUI))
+            {
+                url.Append("&DevEUI=")
+                    .Append(devEUI);
+            }
+
+            if (!string.IsNullOrEmpty(appEUI))
+            {
+                url.Append("&AppEUI=")
+                    .Append(appEUI);
+            }
+
+            if (!string.IsNullOrEmpty(devNonce))
+            {
+                url.Append("&DevNonce=")
+                    .Append(devNonce);
+            }
+
+            var response = await client.GetAsync(url.ToString());
+            if (!response.IsSuccessStatusCode)
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                {
+                    var badReqResult = await response.Content.ReadAsStringAsync();
+
+                    if (!string.IsNullOrEmpty(badReqResult) && string.Equals(badReqResult, "UsedDevNonce", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        Logger.Log(devEUI ?? string.Empty, $"DevNonce already used by this device", LogLevel.Information);
+                        return new SearchDevicesResult
+                        {
+                            IsDevNonceAlreadyUsed = true,
+                        };
+                    }
+                }
+
+                Logger.Log(devAddr, $"error calling façade api: {response.ReasonPhrase}, status: {response.StatusCode}, check the azure function log", LogLevel.Error);
+
+                // TODO: FBE check if we return null or throw exception
+                return new SearchDevicesResult();
+            }
+
+            var result = await response.Content.ReadAsStringAsync();
+            var devices = (List<IoTHubDeviceInfo>)JsonConvert.DeserializeObject(result, typeof(List<IoTHubDeviceInfo>));
+            return new SearchDevicesResult(devices);
         }
     }
 }
