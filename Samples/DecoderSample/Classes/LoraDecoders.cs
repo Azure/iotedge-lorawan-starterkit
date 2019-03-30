490bd6f8ca498451ce98a06b66916a75a7bf492f
Michael Gerfen <gerfen@hotmail.com>
2019-03-29 17:51:11 -0700

    Added exisiting 0.4.0-preview decoders

--- Samples/DecoderSample/Classes/LoraDecoders.cs
+++ Samples/DecoderSample/Classes/LoraDecoders.cs
@@ -4,6 +4,7 @@
 namespace SensorDecoderModule.Classes
 {
     using System;
+    using System.Collections.Generic;
     using System.Text;
     using Newtonsoft.Json;
 
@@ -49,6 +50,86 @@ namespace SensorDecoderModule.Classes
             // Return a JSON string containing the decoded data
             return JsonConvert.SerializeObject(resultObject);
 
+        }
+        private static readonly Dictionary<uint, string> ttnEvents_ = new Dictionary<uint, string> { {1, "setup"},  {2, "interval"}, {3, "motion"}, {4, "button"}};
+        private static string DecoderTheThingsNodeSensor(string devEUI, byte[] payload, uint fport) 
+        {
+
+            var eventName = ttnEvents_[fport];
+            var battery =  (payload[0] << 8) + payload[1];
+            var light = (payload[2] << 8) + payload[3];
+            var temperature = (payload[4] & 0x80) > 0 ?  Convert.ToDouble(((0xffff << 16) + (payload[4] << 8) + payload[5])) / 100 : Convert.ToDouble(((payload[4] << 8) + payload[5])) / 100;
+ 
+           var message =  $"{{\"event\": \"{eventName}\",\"battery\": {battery}, \"light\": {light}, \"temperature\": {temperature} }}";
+            //Logger.Log(message, Logger.LoggingLevel.Always);
+           return message;
+        }
+
+         private static string DecoderLansitecTemperatureHumiditySensor(string devEUI, byte[] payload, uint fport) 
+        {
+
+            var eventName = ttnEvents_[fport];
+            var battery =  (payload[0] << 8) + payload[1];
+            var light = (payload[2] << 8) + payload[3];
+            var temperature = (payload[4] & 0x80) > 0?  ((0xffff << 16) + (payload[4] << 8) + payload[5]) / 100 : ((payload[4] << 8) + payload[5]) / 100;
+ 
+           var message =  $"{{\"event\": \"{eventName}\",\"battery\": {battery}, \"light\": {light}, \"temperature\": {temperature} }}";
+            //Logger.Log(message, Logger.LoggingLevel.Always);
+           return message;
+        }
+
+        private static string DecoderDecentlabWaterLevelSensor(string devEUI, byte[] payload, uint fport) 
+        {
+            var bytes = payload;
+
+            var deviceId = (bytes[1] << 8) + bytes[2];
+            //var flags = ((bytes[3] << 8) + bytes[4]);
+
+            var integers = new List<int>();
+
+            // convert data to 16-bit integers
+            for (var i = 5; i < bytes.Length; i += 2)
+            {
+                integers.Add((bytes[i] << 8) + bytes[i + 1]);
+            }
+
+            var pressure = DecodePressure(integers[0]);
+            var temperature = DecodeTemperature(integers[1]);
+            var battery = DecodeBattery(integers[2]);
+
+            var message =  $"{{\"deviceId\": \"{deviceId}\",\"battery\": {battery}, \"pressure\": {pressure}, \"temperature\": {temperature} }}";
+            //Logger.Log(message, Logger.LoggingLevel.Always);
+           return message;
+        }
+
+        private static double DecodePressure(double datum)
+        {
+            return (datum - 16384) / 32768 * (1 - 0) + 0;
+        }
+
+        private static double DecodeTemperature(double datum)
+        {
+            return (datum - 384) / 64000 * 200 - 50;
+        }
+
+        private static double DecodeBattery(double datum)
+        {
+            //return datum / 1000;
+            return datum;
+        }
+  
+
+       private static string DecoderNetvoxTemperatureHumiditySensor(string devEUI, byte[] payload, uint fport) 
+        {
+             var bytes = payload;
+            
+            var hex = BitConverter.ToString(bytes);
+            var hexValues = hex.Split("-"); 
+            var battery = Convert.ToUInt16(hexValues[3], 16) * 100;
+            var temperature = Convert.ToInt16($"{hexValues[4]}{hexValues[5]}", 16) * 0.01;
+            var humidity = Convert.ToUInt16($"{hexValues[6]}{hexValues[7]}", 16) * 0.01;
+
+            return  $"{{\"battery\": {battery}, \"temperature\": {temperature}, \"humidity\": {humidity} }}";
         }
     }
 }
