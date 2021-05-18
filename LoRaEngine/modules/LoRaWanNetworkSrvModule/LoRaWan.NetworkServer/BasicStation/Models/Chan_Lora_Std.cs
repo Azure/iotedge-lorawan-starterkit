eb7daf9e812cafd9e76d207f37b8ee0eb1020281
Daniele Antonio Maggio <damaggio@microsoft.com>
2021-05-18 16:16:10 +0200

    Initial Models

---
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/BasicStation/Models/Chan_Lora_Std.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace LoRaWan.NetworkServer.BasicStation.Models
+{
+    using System.Text.Json.Serialization;
+
+    public class Chan_Lora_Std
+    {
+        [JsonPropertyName("enable")]
+        public bool Enable { get; set; }
+
+        [JsonPropertyName("radio")]
+        public int Radio { get; set; }
+
+        [JsonPropertyName("if")]
+        public int If { get; set; }
+
+        [JsonPropertyName("bandwidth")]
+        public int Bandwidth { get; set; }
+
+        [JsonPropertyName("spread_factor")]
+        public int Spread_factor { get; set; }
+    }
+}
---
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/BasicStation/Models/Radio.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace LoRaWan.NetworkServer.BasicStation.Models
+{
+    using System.Text.Json.Serialization;
+
+    public class Radio
+    {
+        [JsonPropertyName("enable")]
+        public bool Enable { get; set; }
+
+        [JsonPropertyName("freq")]
+        public int Freq { get; set; }
+    }
+}
