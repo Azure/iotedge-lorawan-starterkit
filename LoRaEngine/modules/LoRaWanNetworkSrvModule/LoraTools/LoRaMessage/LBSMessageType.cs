494c7caf1a4973dd0a779730952a5a74ab7728c4
Peeyush Chandel <555114+cpeeyush@users.noreply.github.com>
2021-05-19 14:44:18 +0200

    Constructor implementation to create LoRaPayloadData from LBSMessage (#301)
    * Adding a new constructor to construct LoRaPayloadData from LBS messages
    
    * Constructor implementation to create LoRaPayloadData from LBSMessage
    
    * Initialize LBSMessageType via constructor

---
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoraTools/LoRaMessage/LBSMessageType.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace LoRaTools.LoRaMessage
+{
+    /// <summary>
+    /// Represents various Lora basic station (LBS) messages.
+    /// </summary>
+    public enum LBSMessageType
+    {
+        Version,
+        Updf,
+        Jreq,
+    }
+}
