ae184e8d62c90bed744b59a06464a5e87895f619
Marc Gomez <marcgomez@microsoft.com>
2021-05-19 15:20:48 +0200

    Modularized code

--- Samples/UniversalDecoder/modules/UniversalDecoder/app.routes.js
+++ Samples/UniversalDecoder/modules/UniversalDecoder/app.routes.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const glob = require('glob');
 const express = require('express');
-const { param, query, validationResult } = require('express-validator');
-const { expressLogger, logger } = require('./app.logging');
+const {param, query, validationResult} = require('express-validator');
+const {expressLogger, logger} = require('./app.logging');
+const decoder = require('./app.decoder');
 
 const app = express();
 
 app.use(expressLogger);
 
-app.get('/api/:decodername', 
+app.get('/api/:decodername',
     param('decodername').notEmpty().withMessage("is missing"),
     query('payload').notEmpty().withMessage("is missing").isBase64().withMessage("must be a base64 encoded string"),
     query('fport').isInt(),
@@ -20,54 +20,21 @@ app.get('/api/:decodername',
             logger.warn(error);
             return res.status(400).send({error});
         }
-      
-      const decoderName = req.params.decodername;
-      const bytes = Buffer.from(req.query.payload, 'base64').toString('utf8').split('');
-      const fPort = parseInt(req.query.fport);
-    
-      const decoder = getDecoder(decoderName);
-    
-      const input = {
-        bytes,
-        fPort
-      };
-    
-      logger.debug(`Decoder ${decoderName} input: ${JSON.stringify(input)}`);
-      var output = decoder.decodeUplink(input);
-      logger.debug(`Decoder ${decoderName} output: ${JSON.stringify(output)}`);
-    
-      res.send({
-        value: output.data,
-      });
-});
 
-// gets decoder by name
-function getDecoder(decoderName) {
-  if (decoderName === 'DecoderValueSensor') {
-    // return inline decoder implementation
-    return {
-      decodeUplink: (input) => { return { data: input.bytes.join('') } }
-    }
-  }
-  
-  // search for codec in "codecs" directory
-  var files = glob.sync(`./codecs/**/${decoderName}.js`);
-  if (files.length === 0) {
-    throw new Error(`No codec found: ${decoderName}`);
-  } else if (files.length > 1) {
-    throw new Error(`Multiple codecs found: ${JSON.stringify(files)}`);
-  }
-  
-  return require(files[0]);
-}
+        const output = decoder.decode(req.params.decodername, req.query.payload, req.query.fport);
+
+        res.send({
+            value: output.data,
+        });
+    });
 
 // Error handling
 app.use(function (err, req, res, next) {
-  logger.error(err);
-  res.status(500).send({
-    error: err.message,
-    rawPayload: req.query.payload
-  });
+    logger.error(err);
+    res.status(500).send({
+        error: err.message,
+        rawPayload: req.query.payload
+    });
 })
 
 module.exports = app;
