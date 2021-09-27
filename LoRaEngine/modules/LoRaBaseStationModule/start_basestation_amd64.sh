fc2faebd72274148911a77c65a383f8579904dad
Mik <chmikhai@microsoft.com>
2021-09-27 08:30:35 +0200

    Add LBS modules

---
+++ LoRaEngine/modules/LoRaBaseStationModule/start_basestation_amd64.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ -z "$RESET_PIN" ]]; then
+    echo "No RESET_PIN environment variable set, skipping the pin reset. If you experience problem with starting the concentrator please set this variable to your manufacturer reset pin"
+else
+    echo "Resetting the pin"
+    ./reset_lgw.sh stop $RESET_PIN
+    ./reset_lgw.sh start $RESET_PIN
+    echo "Finished resetting the pin"
+fi
+
+if [[ -z "$TC_URI" ]]; then
+    echo "No TC_URI detected in environment variables."
+else
+    echo "TC_URI is set to: $TC_URI"
+    touch tc.uri && echo "$TC_URI" > tc.uri
+
+    #start basestation
+    echo "Starting base station..."
+    RADIODEV=/dev/spidev$SPI_DEV.0 ./station -f
+fi
