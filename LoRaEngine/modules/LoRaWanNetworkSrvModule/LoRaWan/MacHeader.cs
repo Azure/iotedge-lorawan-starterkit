a76ddbbcef66fc4b8f3e9674726e5d6160718f9f
Atif Aziz <code@raboof.com>
2021-10-07 21:27:52 +0200

    Add type for MHDR (MAC header)

---
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan/MacHeader.cs
@@ -0,0 +1,31 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace LoRaWan
+{
+    using System;
+    using System.Globalization;
+
+    /// <summary>
+    /// A MAC header that specifies the message type (MType) and according to which major version
+    /// (Major) of the frame format of the LoRaWAN layer specification the frame has been encoded.
+    /// </summary>
+    public readonly struct MacHeader : IEquatable<MacHeader>
+    {
+        readonly byte value;
+
+        public MacHeader(byte value) => this.value = value;
+
+        public MacMessageType MessageType => (MacMessageType)(this.value >> 5);
+        public int Major => this.value & 0b11;
+
+        public bool Equals(MacHeader other) => this.value == other.value;
+        public override bool Equals(object obj) => obj is MacHeader other && this.Equals(other);
+        public override int GetHashCode() => this.value.GetHashCode();
+
+        public override string ToString() => value.ToString("x2", CultureInfo.InvariantCulture);
+
+        public static bool operator ==(MacHeader left, MacHeader right) => left.Equals(right);
+        public static bool operator !=(MacHeader left, MacHeader right) => !left.Equals(right);
+    }
+}
