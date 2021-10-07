c82c667b050521378de6c8d1a967fab33cd2e894
Atif Aziz <code@raboof.com>
2021-10-07 18:01:29 +0200

    Add "JoinEui"

---
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan/JoinEui.cs
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace LoRaWan
+{
+    using System;
+    using System.Globalization;
+
+    /// <summary>
+    /// Global application ID in IEEE EUI-64 (64-bit Extended Unique Identifier) address space
+    /// that uniquely identifies the Join Server that is able to assist in the processing of
+    /// the Join procedure and the session keys derivation.
+    /// </summary>
+    /// <remarks>
+    /// For OTAA devices, the JoinEUI MUST be stored in the end-device before the Join procedure
+    /// is executed. The JoinEUI is not required for ABP only end-devices.
+    /// </remarks>
+    readonly struct JoinEui : IEquatable<JoinEui>
+    {
+        readonly ulong value;
+
+        public JoinEui(ulong value) => this.value = value;
+
+        public bool Equals(JoinEui other) => this.value == other.value;
+        public override bool Equals(object obj) => obj is JoinEui other && this.Equals(other);
+        public override int GetHashCode() => this.value.GetHashCode();
+
+        public override string ToString() => this.value.ToString("x16", CultureInfo.InvariantCulture);
+
+        public static bool operator ==(JoinEui left, JoinEui right) => left.Equals(right);
+        public static bool operator !=(JoinEui left, JoinEui right) => !left.Equals(right);
+    }
+}
