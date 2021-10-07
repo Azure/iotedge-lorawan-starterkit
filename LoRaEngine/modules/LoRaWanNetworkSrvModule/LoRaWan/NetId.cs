9934304739e2211e1d305d3ba8e6fef05f77d9f6
Atif Aziz <code@raboof.com>
2021-10-07 18:03:07 +0200

    Add "NetId"

---
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan/NetId.cs
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace LoRaWan
+{
+    using System;
+    using System.Globalization;
+
+    /// <summary>
+    /// NetID is a 24-bit value used for identifying LoRaWAN networks. It is assigned by the LoRa
+    /// Alliance. It is used by networks for assigning network-specific addresses to their
+    /// end-devices (i.e., DevAddr) so that uplink frames sent by those devices even when they are
+    /// roaming outside their home network can be forwarded to their home network.
+    /// </summary>
+    public readonly struct NetId : IEquatable<NetId>
+    {
+        readonly int value; // 24-bit
+
+        public NetId(int value)
+        {
+            if (unchecked((uint)value & 0xffff_0000_0000_0000) != 0)
+                throw new ArgumentOutOfRangeException(nameof(value), value, null);
+            this.value = value;
+        }
+
+        public bool Equals(NetId other) => this.value == other.value;
+        public override bool Equals(object obj) => obj is NetId other && this.Equals(other);
+        public override int GetHashCode() => this.value.GetHashCode();
+
+        public override string ToString() => value.ToString("x6", CultureInfo.InvariantCulture);
+
+        public static bool operator ==(NetId left, NetId right) => left.Equals(right);
+        public static bool operator !=(NetId left, NetId right) => !left.Equals(right);
+    }
+}
