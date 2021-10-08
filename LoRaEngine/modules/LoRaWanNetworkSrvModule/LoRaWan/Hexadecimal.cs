fbb821d1aae02ea2cc7bcd2f57a388151ca9dec8
Atif Aziz <code@raboof.com>
2021-10-08 16:39:49 +0200

    Add methods for parsing (hyphenated) hexadecimals

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan/Hexadecimal.cs
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan/Hexadecimal.cs
@@ -20,5 +20,45 @@ namespace LoRaWan
                 value >>= 4;
             }
         }
+
+        public static bool TryParse(ReadOnlySpan<char> chars, out ulong value, char? separator = null)
+        {
+            value = default;
+            if (chars.IsEmpty)
+                return false;
+            Span<byte> bytes = stackalloc byte[sizeof(ulong)];
+            if (!TryParse(chars, bytes, separator))
+                return false;
+            value = BinaryPrimitives.ReadUInt64BigEndian(bytes);
+            return true;
+        }
+
+        /// <remarks>
+        /// When this method returns <c>false</c>, the content of <paramref name="output"/> should
+        /// be discarded. This is because bytes are written to <paramref name="output"/> as they are
+        /// parsed. If parsing fails partway then <paramref name="output"/> will contain partial
+        /// results and should be discarded.
+        /// </remarks>
+        public static bool TryParse(ReadOnlySpan<char> chars, Span<byte> output, char? separator = null)
+        {
+            if (chars.Length == 1 || separator is null && chars.Length % 2 != 0)
+                return false;
+
+            while (!chars.IsEmpty)
+            {
+                if (!byte.TryParse(chars[..2], NumberStyles.AllowHexSpecifier, null, out var b))
+                    return false;
+                output[0] = b;
+                output = output[1..];
+                chars = chars[2..];
+                if (!chars.IsEmpty && separator is { } someSeparator)
+                {
+                    if (chars.Length < 3 || chars[0] != someSeparator)
+                        return false;
+                    chars = chars[1..];
+                }
+            }
+            return true;
+        }
     }
 }
