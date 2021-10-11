1a54cc9b602914877ff9c1eb0125ebff9541c0c3
Maggie Salak <masalak@microsoft.com>
2021-10-11 11:46:04 +0200

    add base class for Region implementation tests

---
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaToolsTest/RegionEU868Test.cs
@@ -0,0 +1,19 @@
+﻿namespace LoRaWanTest
+{
+    using System;
+    using System.Collections.Generic;
+    using LoRaTools.Regions;
+
+    public class RegionEU868Test : RegionTestBase
+    {
+        public RegionEU868Test()
+        {
+            _region = new RegionEU868();
+        }
+
+        protected override IEnumerable<(string inputDr, double inputFreq, string outputDr, double outputFreq)> CreateValidTestData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaToolsTest/RegionTestBase.cs
@@ -0,0 +1,55 @@
+﻿namespace LoRaWanTest
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using LoRaTools.LoRaPhysical;
+    using LoRaTools.Regions;
+    using Xunit;
+
+    public abstract class RegionTestBase
+    {
+        protected Region _region;
+
+        [Fact]
+        public virtual void Test_Region_Frequency_And_Data_Rate()
+        {
+            foreach (var data in CreateValidTestData())
+            {
+                var rxpk = GenerateRxpk(data.inputDr, data.inputFreq);
+                Assert.True(_region.TryGetDownstreamChannelFrequency(rxpk[0], out double frequency));
+                Assert.Equal(frequency, data.outputFreq);
+                Assert.Equal(_region.GetDownstreamDR(rxpk[0]), data.outputDr);
+            }
+        }
+
+        protected abstract IEnumerable<(string inputDr, double inputFreq, string outputDr, double outputFreq)> CreateValidTestData();
+
+        private static List<Rxpk> GenerateRxpk(string dr, double freq)
+        {
+            string jsonUplink =
+                @"{ ""rxpk"":[
+                {
+                    ""time"":""2013-03-31T16:21:17.528002Z"",
+                    ""tmst"":3512348611,
+                    ""chan"":2,
+                    ""rfch"":0,
+                    ""freq"":" + freq + @",
+                    ""stat"":1,
+                    ""modu"":""LORA"",
+                    ""datr"":""" + dr + @""",
+                    ""codr"":""4/6"",
+                    ""rssi"":-35,
+                    ""lsnr"":5.1,
+                    ""size"":32,
+                    ""data"":""AAQDAgEEAwIBBQQDAgUEAwItEGqZDhI=""
+                }]}";
+
+            var multiRxpkInput = Encoding.Default.GetBytes(jsonUplink);
+            byte[] physicalUpstreamPyld = new byte[12];
+            physicalUpstreamPyld[0] = 2;
+            List<Rxpk> rxpk = Rxpk.CreateRxpk(physicalUpstreamPyld.Concat(multiRxpkInput).ToArray());
+            return rxpk;
+        }
+    }
+}
