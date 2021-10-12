1492499586b679174549492795aeb77e9af617d9
Atif Aziz <code@raboof.com>
2021-10-12 19:47:25 +0200

    Add tests for valid "DataRate"

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaToolsTest/DataRateTests.cs
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaToolsTest/DataRateTests.cs
@@ -1,5 +1,6 @@
 namespace LoRaWanTest
 {
+    using System;
     using LoRaWan;
     using Xunit;
 
@@ -7,6 +8,26 @@ namespace LoRaWanTest
     {
         readonly DataRate subject = new(5);
         readonly DataRate other = new(10);
+
+        [Fact]
+        public void Initialization_Succeeds_For_Valid_Values()
+        {
+            for (var dr = 0; dr < 15; dr++)
+            {
+                var ex = Record.Exception(() => new DataRate(dr));
+                Assert.Null(ex);
+            }
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(16)]
+        public void Initialization_Throws_For_Invalid_Values(int dr)
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => new DataRate(dr));
+            Assert.Equal("value", ex.ParamName);
+            Assert.Equal(dr, ex.ActualValue);
+        }
 
         [Fact]
         public void Equals_Returns_True_When_Value_Equals()
