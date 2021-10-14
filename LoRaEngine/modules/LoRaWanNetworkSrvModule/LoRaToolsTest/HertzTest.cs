7fa981e65d776f7d8f81111afaf836d426706dcf
Atif Aziz <code@raboof.com>
2021-10-14 12:20:13 +0200

    Use more conventional names in "Hertz" tests

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaToolsTest/HertzTest.cs
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaToolsTest/HertzTest.cs
@@ -9,75 +9,75 @@ namespace LoRaWanTest
     public class HertzTest
     {
         const ulong EuropeanFrequencyInHertz = 863_000_000;
-        const ulong USFrequencyInHertz = 902_000_000;
+        const ulong AmericanFrequencyInHertz = 902_000_000;
 
-        readonly Hertz MHz863 = new(EuropeanFrequencyInHertz);
-        readonly Hertz MHz902 = new(USFrequencyInHertz);
+        readonly Hertz euFrequency = new(EuropeanFrequencyInHertz);
+        readonly Hertz usFrequency = new(AmericanFrequencyInHertz);
 
         [Fact]
         public void HertzConversions_Behave_Properly()
         {
-            Assert.Equal(863000, MHz863.Kilo);
-            Assert.Equal(863, MHz863.Mega);
-            Assert.Equal(0.863, MHz863.Giga);
+            Assert.Equal(863000, this.euFrequency.Kilo);
+            Assert.Equal(863, this.euFrequency.Mega);
+            Assert.Equal(0.863, this.euFrequency.Giga);
         }
 
         [Fact]
         public void Equals_Returns_True_When_Value_Equals()
         {
             var other = new Hertz(863_000_000);
-            Assert.True(this.MHz863.Equals(other));
+            Assert.True(this.euFrequency.Equals(other));
         }
 
         [Fact]
         public void Equals_Returns_False_When_Value_Is_Different()
         {
-            var other = this.MHz902;
-            Assert.False(this.MHz863.Equals(other));
+            var other = this.usFrequency;
+            Assert.False(this.euFrequency.Equals(other));
         }
 
         [Fact]
         public void Equals_Returns_False_When_Other_Type_Is_Different()
         {
-            Assert.False(this.MHz863.Equals(new object()));
+            Assert.False(this.euFrequency.Equals(new object()));
         }
 
         [Fact]
         public void Equals_Returns_False_When_Other_Type_Is_Null()
         {
-            Assert.False(this.MHz863.Equals(null));
+            Assert.False(this.euFrequency.Equals(null));
         }
 
         [Fact]
         public void Op_Equality_Returns_True_When_Values_Equal()
         {
             var other = new Hertz(EuropeanFrequencyInHertz);
-            Assert.True(this.MHz863 == other);
+            Assert.True(this.euFrequency == other);
         }
 
         [Fact]
         public void Op_Equality_Returns_False_When_Values_Differ()
         {
-            Assert.False(this.MHz863 == this.MHz902);
+            Assert.False(this.euFrequency == this.usFrequency);
         }
 
         [Fact]
         public void Op_Inequality_Returns_False_When_Values_Equal()
         {
             var other = new Hertz(EuropeanFrequencyInHertz);
-            Assert.False(this.MHz863 != other);
+            Assert.False(this.euFrequency != other);
         }
 
         [Fact]
         public void Op_Inequality_Returns_True_When_Values_Differ()
         {
-            Assert.True(this.MHz863 != this.MHz902);
+            Assert.True(this.euFrequency != this.usFrequency);
         }
 
         [Fact]
         public void ToString_Returns_Decimal_String()
         {
-            Assert.Equal("863000000", this.MHz863.ToString());
+            Assert.Equal("863000000", this.euFrequency.ToString());
         }
     }
 }
