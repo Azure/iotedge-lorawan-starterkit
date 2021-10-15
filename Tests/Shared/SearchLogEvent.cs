797624ff454a2f5e6f79b81e6a7834d8f294e176
Bastian Burger <baburger@microsoft.com>
2021-10-15 16:33:12 +0200

    Fix IDE0065: Using directives must be placed inside of a namespace declaration

--- Tests/Shared/SearchLogEvent.cs
+++ Tests/Shared/SearchLogEvent.cs
@@ -1,10 +1,10 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
-
 namespace LoRaWan.Tests.Shared
 {
+    using System;
+
     public class SearchLogEvent
     {
         public SearchLogEvent()
