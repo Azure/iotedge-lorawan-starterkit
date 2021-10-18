a0c6a283c1d37d3b9bff5b14824326d77da58f55
Daniele Antonio Maggio <damaggio@microsoft.com>
2021-10-18 19:31:25 +0200

    Adding Discovery JsonHandler implementation and tests

---
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/BasicsStation/JsonHandlers/Discovery.cs
@@ -0,0 +1,133 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace LoRaWan.NetworkServer.BasicsStation.JsonHandlers
+{
+    using System;
+    using System.Buffers.Binary;
+    using System.IO;
+    using System.Net.NetworkInformation;
+    using System.Text;
+    using System.Text.Json;
+
+    public static class Discovery
+    {
+        /// <summary>
+        /// Reads out a StationEui value from a Discovery Query JSON string.
+        /// </summary>
+        /// <param name="input">The input JSON string.</param>
+        /// <param name="stationEui">The StationEui parsed value.</param>
+        public static void ReadQuery(string input,
+                                     out StationEui stationEui)
+        {
+            var reader = new Utf8JsonReader(Encoding.UTF8.GetBytes(input));
+            _ = reader.Read();
+            if (reader.TokenType != JsonTokenType.StartObject)
+                throw new JsonException();
+            _ = reader.Read();
+
+            stationEui = default;
+            var readStationEui = false;
+
+            while (reader.TokenType != JsonTokenType.EndObject)
+            {
+                if (reader.ValueTextEquals("router"))
+                {
+                    _ = reader.Read();
+                    switch (reader.TokenType)
+                    {
+                        case JsonTokenType.Number:
+                        {
+                            var v = reader.GetUInt64();
+                            stationEui = new StationEui(v);
+                            break;
+                        }
+                        case JsonTokenType.String:
+                        {
+                            var s = reader.GetString();
+                            stationEui = s.Contains(':', StringComparison.Ordinal)
+                                       ? Id6.TryParse(s, out var id6) ? new StationEui(id6) : throw new JsonException()
+                                       : Hexadecimal.TryParse(s, out var hhd, '-') ? new StationEui(hhd) : throw new JsonException();
+                            break;
+                        }
+                        default:
+                            throw new NotSupportedException("'router' field should be either a number or a string.");
+                    }
+                    readStationEui = true;
+                    _ = reader.Read();
+                }
+                else
+                {
+                    _ = reader.Read();
+                    reader.Skip();
+                    _ = reader.Read();
+                }
+            }
+
+            if (!readStationEui)
+                throw new JsonException("Missing required property 'router' in input JSON.");
+        }
+
+        /// <summary>
+        /// Serializes the response for Discovery endpoint as a JSON string.
+        /// </summary>
+        /// <param name="station">The <see cref="StationEui"/> of the querying basic station.</param>
+        /// <param name="muxs">The identity of the LNS Data endpoint (<see cref="Id6"/> formatted).</param>
+        /// <param name="uri">The URI of the LNS Data endpoint.</param>
+        /// <param name="error">The error message. If not <see langword="null"/>, the Basic Station will retry discovery.</param>
+        /// <returns>The JSON string to be sent as Discovery endpoint response.</returns>
+        public static string SerializeResponse(StationEui station,
+                                               string muxs,
+                                               Uri uri,
+                                               string error)
+        {
+            if (!Id6.TryParse(muxs, out var _)) throw new ArgumentException("Argument should be a valid string in ID6 format.", nameof(muxs));
+            if (uri is null) throw new ArgumentNullException(nameof(uri));
+
+            using var ms = new MemoryStream();
+            using var writer = new Utf8JsonWriter(ms);
+            InternalWriteResponse(writer, station, muxs, uri, error);
+            writer.Flush();
+            return Encoding.UTF8.GetString(ms.ToArray());
+        }
+
+        private static void InternalWriteResponse(Utf8JsonWriter writer, StationEui station, string lnsEndpoint, Uri lnsUri, string error)
+        {
+            writer.WriteStartObject();
+
+            writer.WriteString("router", Id6.Format(station.AsUInt64, Id6.FormatOptions.Lowercase));
+            writer.WriteString("muxs", lnsEndpoint);
+            writer.WriteString("uri", lnsUri.ToString());
+            if (!string.IsNullOrEmpty(error))
+                writer.WriteString("error", error);
+
+            writer.WriteEndObject();
+        }
+
+        /// <summary>
+        /// Gets an ID6 compatible representation of the provided network interface MAC Address.
+        /// </summary>
+        /// <param name="networkInterface">The network interface for which the ID6 MAC Address representation should be extracted.</param>
+        /// <returns></returns>
+        internal static string GetMacAddressAsID6(NetworkInterface networkInterface)
+        {
+            var physicalAddress = 0UL;
+
+            if (networkInterface is not null)
+            {
+                // As per specification (https://doc.sm.tc/station/glossary.html?highlight=id6)
+                // for an ID6 based on a MAC Address we expect FFFE in the middle
+                var physicalAddress48 = networkInterface.GetPhysicalAddress().GetAddressBytes();
+
+                Span<byte> physicalAddress64 = stackalloc byte[8];
+                physicalAddress48[..3].CopyTo(physicalAddress64);
+                physicalAddress64[3] = 0xFF;
+                physicalAddress64[4] = 0xFE;
+                physicalAddress48[3..].CopyTo(physicalAddress64[5..]);
+                physicalAddress = BinaryPrimitives.ReadUInt64BigEndian(physicalAddress64);
+            }
+
+            return Id6.Format(physicalAddress, Id6.FormatOptions.FixedWidth);
+        }
+    }
+}
---
+++ LoRaEngine/test/LoRaWanNetworkServer.Test/BasicsStation/JsonHandlers/DiscoveryTests.cs
@@ -0,0 +1,102 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace LoRaWan.NetworkServer.Test.BasicsStation.JsonHandlers
+{
+    using LoRaWan;
+    using LoRaWan.NetworkServer.BasicsStation.JsonHandlers;
+    using Moq;
+    using System;
+    using System.Net.NetworkInformation;
+    using System.Text.Json;
+    using Xunit;
+
+    public class DiscoveryTests
+    {
+        [Theory]
+        [InlineData(@"{ ""router"": ""b827:ebff:fee1:e39a"" }")]
+        [InlineData(@"{ ""router"": ""b8-27-eb-ff-fe-e1-e3-9a"" }")]
+        [InlineData(@"{ ""router"": 13269834311795860378 }")]
+        [InlineData(@"{ ""onePropBefore"": { ""value"": 123 }, ""router"": 13269834311795860378 }")]
+        [InlineData(@"{ ""router"": 13269834311795860378 }, ""onePropAfter"": { ""value"": 123 }")]
+        public void ReadQuery(string json)
+        {
+            Discovery.ReadQuery(json, out var stationEui);
+            Assert.Equal(new StationEui(0xb827_ebff_fee1_e39aUL), stationEui);
+        }
+
+        [Theory]
+        [InlineData(@"{ ""nonRouter"": ""wrongValue"" }")]
+        [InlineData(@"{}")]
+        public void ReadQuery_Throws_OnMissingProperty(string json)
+        {
+            Assert.Throws<JsonException>(() => Discovery.ReadQuery(json, out var _));
+        }
+
+        [Theory]
+        [InlineData(@"{ ""router"": [""wrongValue""] }")]
+        [InlineData(@"{ ""router"": { ""value"": 13269834311795860378 } }")]
+        [InlineData(@"{ ""router"": true }")]
+        public void ReadQuery_Throws_OnInvalidPropertyType(string json)
+        {
+            Assert.Throws<NotSupportedException>(() => Discovery.ReadQuery(json, out var _));
+        }
+
+        const string validMuxs = "0000:00FF:FE00:0000";
+        const string validUriString = "ws://localhost:5000/router-data";
+
+        [Theory]
+        [InlineData("b8-27-eb-ff-fe-e1-e3-9a", validMuxs, validUriString, "", @"{""router"":""b827:ebff:fee1:e39a"",""muxs"":""0000:00FF:FE00:0000"",""uri"":""ws://localhost:5000/router-data""}")]
+        [InlineData("b827:ebff:fee1:e39a", validMuxs, validUriString, "", @"{""router"":""b827:ebff:fee1:e39a"",""muxs"":""0000:00FF:FE00:0000"",""uri"":""ws://localhost:5000/router-data""}")]
+        public void Serialize_Succeeds(string stationId6, string muxs, string url, string error, string expected)
+        {
+            var stationEui = stationId6.Contains(':', StringComparison.Ordinal)
+                             ? Id6.TryParse(stationId6, out var id6) ? new StationEui(id6) : throw new JsonException()
+                             : Hexadecimal.TryParse(stationId6, out var hhd, '-') ? new StationEui(hhd) : throw new JsonException();
+
+            var computed = Discovery.SerializeResponse(stationEui, muxs, new Uri(url), error);
+            Assert.Equal(expected, computed);
+        }
+
+        [Fact]
+        public void Serialize_Fails_BecauseOfNonId6Muxs()
+        {
+            var muxs = "000000FFFE000000";
+
+            _ = Id6.TryParse("b827:ebff:fee1:e39a", out var stationId6);
+            var stationEui = new StationEui(stationId6);
+
+            Assert.Throws<ArgumentException>(() => Discovery.SerializeResponse(stationEui, muxs, new Uri(validUriString), string.Empty));
+        }
+
+        [Fact]
+        public void Serialize_Fails_BecauseNullUri()
+        {
+            _ = Id6.TryParse("b827:ebff:fee1:e39a", out var stationId6);
+            var stationEui = new StationEui(stationId6);
+
+            Assert.Throws<ArgumentNullException>(() => Discovery.SerializeResponse(stationEui, validMuxs, null, string.Empty));
+        }
+
+        [Fact]
+        public void GetMacAddressAsID6_Succeeds_WithValidPhysicalAddress()
+        {
+            var networkInterface = new Mock<NetworkInterface>();
+            var physicalAddress48 = new byte[] { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66 };
+            var physicalAddress = new PhysicalAddress(physicalAddress48);
+            networkInterface.Setup(x => x.GetPhysicalAddress()).Returns(physicalAddress);
+            var expected = "1122:33FF:FE44:5566";
+
+            var id6Mac = Discovery.GetMacAddressAsID6(networkInterface.Object);
+            Assert.Equal(expected, id6Mac);
+        }
+
+        [Fact]
+        public void GetMacAddressAsID6_Returns_ZeroFilledId6_WithNoInterface()
+        {
+            var expected = "0000:0000:0000:0000";
+            var id6Mac = Discovery.GetMacAddressAsID6(null);
+            Assert.Equal(expected, id6Mac);
+        }
+    }
+}
