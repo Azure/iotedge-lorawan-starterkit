16f009f7053e3b0c818636eddf3919a59a6cf914
Spyros Giannakakis <spgianna@microsoft.com>
2021-10-18 18:06:01 +0200

    Removes LorakeysManagerFacade.Test proj.

--- Tests/Unit/MessageDeduplicationTests.cs
+++ Tests/Unit/MessageDeduplicationTests.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace LoraKeysManagerFacade.Test
+namespace LoRaWan.Tests.Unit
 {
     using System.Threading.Tasks;
     using LoraKeysManagerFacade.FunctionBundler;
