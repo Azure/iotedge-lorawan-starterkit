a213db5b9cf3f593476ba3d6e39b59bd1b6c8918
Daniele Antonio Maggio <damaggio@microsoft.com>
2021-10-19 19:01:40 +0200

    Getting rid of some temporary LnsJsonTests

--- LoRaEngine/test/LoRaWanNetworkServer.Test/BasicsStation/LnsJsonTests.cs
+++ LoRaEngine/test/LoRaWanNetworkServer.Test/BasicsStation/LnsJsonTests.cs
@@ -3,7 +3,6 @@
 
 namespace LoRaWan.NetworkServer.Test.BasicsStation
 {
-    using System.IO;
     using System.Text;
     using System.Text.Json;
     using LoRaWan;
@@ -45,36 +44,6 @@ namespace LoRaWan.NetworkServer.Test.BasicsStation
             Assert.Equal(new MacHeader(128), macHeader);
             Assert.Equal(new DevAddr(0x380CBF3), devAddr);
             Assert.Equal(new Mic(unchecked((uint)-1863929443)), mic);
-        }
-
-        [Fact]
-        public void WriteRouterConfig()
-        {
-            // const string expected = @"{""freq_range"":[863000000,870000000],""msgtype"":""router_config"",""JoinEUI"":[[13725282814365013217,13725282814365013219]],""NetID"":[1],""DRs"":[[12,125,0],[11,125,0],[10,125,0],[9,125,0],[8,125,0],[7,125,0],[7,250,0]],""hwspec"":""sx1301/1"",""region"":""EU863"",""nocca"":true,""nodc"":true,""nodwell"":true,""sx1301_conf"":[{""radio_0"":{""enable"":true,""freq"":867500000},""radio_1"":{""enable"":true,""freq"":868500000},""chan_FSK"":{""enable"":true,""radio"":1,""if"":300000},""chan_Lora_std"":{""enable"":true,""radio"":1,""if"":-200000,""bandwidth"":250000,""spread_factor"":7},""chan_multiSF_0"":{""enable"":true,""radio"":1,""if"":-400000},""chan_multiSF_1"":{""enable"":true,""radio"":1,""if"":-200000},""chan_multiSF_2"":{""enable"":true,""radio"":1,""if"":0},""chan_multiSF_3"":{""enable"":true,""radio"":0,""if"":-400000},""chan_multiSF_4"":{""enable"":true,""radio"":0,""if"":-200000},""chan_multiSF_5"":{""enable"":true,""radio"":0,""if"":0},""chan_multiSF_6"":{""enable"":true,""radio"":0,""if"":200000},""chan_multiSF_7"":{""enable"":true,""radio"":0,""if"":400000}}]}";
-
-            const string expected = @"{
-                    ""msgtype"": ""router_config"",
-                    ""freq_range"": [863000000, 870000000],
-                    ""JoinEUI"": [[13725282814365013217, 13725282814365013219]]
-                }";
-
-            var actual = LnsJson.WriteRouterConfig(
-                freqRange: (new(863000000), new(870000000)),
-                joinEuiRanges: new (JoinEui, JoinEui)[]
-                {
-                    (new(13725282814365013217), new(13725282814365013219))
-                });
-
-            Assert.Equal(TrimJson(expected), actual);
-        }
-
-        static string TrimJson(string json)
-        {
-            using var ms = new MemoryStream();
-            using var writer = new Utf8JsonWriter(ms);
-            JsonDocument.Parse(json).WriteTo(writer);
-            writer.Flush();
-            return Encoding.UTF8.GetString(ms.ToArray());
         }
     }
 }
