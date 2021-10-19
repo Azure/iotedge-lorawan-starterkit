c9d22a27d5f20b913b88360b322938b951584975
Daniele Antonio Maggio <damaggio@microsoft.com>
2021-10-19 17:22:12 +0200

    Adding LnsMessageType

---
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/BasicsStation/LnsMessageType.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace LoRaWan.NetworkServer.BasicsStation
+{
+    internal enum LnsMessageType
+    {
+        version,
+        router_config,
+        jreq,
+        updf,
+        dntxed,
+        dnmsg,
+    }
+}
