0bac12b7d3a09fb7e92691b296d6b6400dc8871c
Daniele Antonio Maggio <damaggio@microsoft.com>
2021-10-21 16:59:51 +0200

    Getting rid of loRaSyncWord and gFSKSyncWord from Region implementation (#604)
    * Getting rid of loRaSyncWord and gFSKSyncWord from Region implementation
    
    * Adapting CN470 to get rid of sync words
    
    * Formatting

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/LoraTools/Regions/Region.cs
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoraTools/Regions/Region.cs
@@ -16,10 +16,6 @@ namespace LoRaTools.Regions
 
         public LoRaRegionType LoRaRegion { get; set; }
 
-        public byte LoRaSyncWord { get; private set; }
-
-        public IReadOnlyList<byte> GFSKSyncWord { get; private set; }
-
         /// <summary>
         /// Gets or sets datarate to configuration and max payload size (M)
         /// max application payload size N should be N= M-8 bytes.
@@ -98,13 +94,10 @@ namespace LoRaTools.Regions
         /// </summary>
         public int MaxADRDataRate { get; set; }
 
-        protected Region(LoRaRegionType regionEnum, byte loRaSyncWord, byte[] gFSKSyncWord, (double frequency, ushort datr) rx2DefaultReceiveWindows)
+        protected Region(LoRaRegionType regionEnum, (double frequency, ushort datr) rx2DefaultReceiveWindows)
         {
             LoRaRegion = regionEnum;
             RetransmitTimeout = (min: 1, max: 3);
-
-            LoRaSyncWord = loRaSyncWord;
-            GFSKSyncWord = gFSKSyncWord;
 
             RX2DefaultReceiveWindows = rx2DefaultReceiveWindows;
             ReceiveDelay1 = 1;
