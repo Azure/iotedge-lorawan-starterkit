d4a6ae8b28ae53c476fad215876365d08a838d96
Atif Aziz <code@raboof.com>
2021-10-25 12:00:12 +0200

    Add back reader for object with single property

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/JsonReader.cs
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/JsonReader.cs
@@ -95,6 +95,15 @@ namespace LoRaWan.NetworkServer
             public Unit Default => default;
         }
 
+        public static IJsonReader<T> Object<T>(IJsonProperty<T> property) =>
+            Object(property, NonProperty.Instance, NonProperty.Instance,
+                   NonProperty.Instance, NonProperty.Instance, NonProperty.Instance,
+                   NonProperty.Instance, NonProperty.Instance, NonProperty.Instance,
+                   NonProperty.Instance, NonProperty.Instance, NonProperty.Instance,
+                   NonProperty.Instance, NonProperty.Instance, NonProperty.Instance,
+                   NonProperty.Instance,
+                   (v, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) => v);
+
         public static IJsonReader<TResult>
             Object<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>(
                 IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3,
