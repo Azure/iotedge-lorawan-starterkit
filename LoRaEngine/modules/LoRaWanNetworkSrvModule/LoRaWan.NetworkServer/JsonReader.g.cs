e21924441d29658f8d5745b717499737aeea421d
Atif Aziz <code@raboof.com>
2021-10-25 11:55:58 +0200

    Wrap projector paramater to next line

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/JsonReader.g.cs
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/JsonReader.g.cs
@@ -55,7 +55,8 @@ namespace LoRaWan.NetworkServer
 
         public static IJsonReader<TResult>
             Object<T1, T2, TResult>(
-                IJsonProperty<T1> property1, IJsonProperty<T2> property2, Func<T1, T2, TResult> projector) =>
+                IJsonProperty<T1> property1, IJsonProperty<T2> property2,
+                Func<T1, T2, TResult> projector) =>
             Object(property1, property2,
                    NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance,
                    (v1, v2, _, _, _, _, _, _, _, _, _, _, _, _, _, _) =>
@@ -63,7 +64,8 @@ namespace LoRaWan.NetworkServer
 
         public static IJsonReader<TResult>
             Object<T1, T2, T3, TResult>(
-                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, Func<T1, T2, T3, TResult> projector) =>
+                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3,
+                Func<T1, T2, T3, TResult> projector) =>
             Object(property1, property2, property3,
                    NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance,
                    (v1, v2, v3, _, _, _, _, _, _, _, _, _, _, _, _, _) =>
@@ -71,7 +73,8 @@ namespace LoRaWan.NetworkServer
 
         public static IJsonReader<TResult>
             Object<T1, T2, T3, T4, TResult>(
-                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, Func<T1, T2, T3, T4, TResult> projector) =>
+                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4,
+                Func<T1, T2, T3, T4, TResult> projector) =>
             Object(property1, property2, property3, property4,
                    NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance,
                    (v1, v2, v3, v4, _, _, _, _, _, _, _, _, _, _, _, _) =>
@@ -79,7 +82,8 @@ namespace LoRaWan.NetworkServer
 
         public static IJsonReader<TResult>
             Object<T1, T2, T3, T4, T5, TResult>(
-                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5, Func<T1, T2, T3, T4, T5, TResult> projector) =>
+                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5,
+                Func<T1, T2, T3, T4, T5, TResult> projector) =>
             Object(property1, property2, property3, property4, property5,
                    NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance,
                    (v1, v2, v3, v4, v5, _, _, _, _, _, _, _, _, _, _, _) =>
@@ -87,7 +91,8 @@ namespace LoRaWan.NetworkServer
 
         public static IJsonReader<TResult>
             Object<T1, T2, T3, T4, T5, T6, TResult>(
-                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5, IJsonProperty<T6> property6, Func<T1, T2, T3, T4, T5, T6, TResult> projector) =>
+                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5, IJsonProperty<T6> property6,
+                Func<T1, T2, T3, T4, T5, T6, TResult> projector) =>
             Object(property1, property2, property3, property4, property5, property6,
                    NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance,
                    (v1, v2, v3, v4, v5, v6, _, _, _, _, _, _, _, _, _, _) =>
@@ -95,7 +100,8 @@ namespace LoRaWan.NetworkServer
 
         public static IJsonReader<TResult>
             Object<T1, T2, T3, T4, T5, T6, T7, TResult>(
-                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5, IJsonProperty<T6> property6, IJsonProperty<T7> property7, Func<T1, T2, T3, T4, T5, T6, T7, TResult> projector) =>
+                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5, IJsonProperty<T6> property6, IJsonProperty<T7> property7,
+                Func<T1, T2, T3, T4, T5, T6, T7, TResult> projector) =>
             Object(property1, property2, property3, property4, property5, property6, property7,
                    NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance,
                    (v1, v2, v3, v4, v5, v6, v7, _, _, _, _, _, _, _, _, _) =>
@@ -103,7 +109,8 @@ namespace LoRaWan.NetworkServer
 
         public static IJsonReader<TResult>
             Object<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(
-                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5, IJsonProperty<T6> property6, IJsonProperty<T7> property7, IJsonProperty<T8> property8, Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> projector) =>
+                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5, IJsonProperty<T6> property6, IJsonProperty<T7> property7, IJsonProperty<T8> property8,
+                Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> projector) =>
             Object(property1, property2, property3, property4, property5, property6, property7, property8,
                    NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance,
                    (v1, v2, v3, v4, v5, v6, v7, v8, _, _, _, _, _, _, _, _) =>
@@ -111,7 +118,8 @@ namespace LoRaWan.NetworkServer
 
         public static IJsonReader<TResult>
             Object<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>(
-                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5, IJsonProperty<T6> property6, IJsonProperty<T7> property7, IJsonProperty<T8> property8, IJsonProperty<T9> property9, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult> projector) =>
+                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5, IJsonProperty<T6> property6, IJsonProperty<T7> property7, IJsonProperty<T8> property8, IJsonProperty<T9> property9,
+                Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult> projector) =>
             Object(property1, property2, property3, property4, property5, property6, property7, property8, property9,
                    NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance,
                    (v1, v2, v3, v4, v5, v6, v7, v8, v9, _, _, _, _, _, _, _) =>
@@ -119,7 +127,8 @@ namespace LoRaWan.NetworkServer
 
         public static IJsonReader<TResult>
             Object<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>(
-                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5, IJsonProperty<T6> property6, IJsonProperty<T7> property7, IJsonProperty<T8> property8, IJsonProperty<T9> property9, IJsonProperty<T10> property10, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult> projector) =>
+                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5, IJsonProperty<T6> property6, IJsonProperty<T7> property7, IJsonProperty<T8> property8, IJsonProperty<T9> property9, IJsonProperty<T10> property10,
+                Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult> projector) =>
             Object(property1, property2, property3, property4, property5, property6, property7, property8, property9, property10,
                    NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance,
                    (v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, _, _, _, _, _, _) =>
@@ -127,7 +136,8 @@ namespace LoRaWan.NetworkServer
 
         public static IJsonReader<TResult>
             Object<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>(
-                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5, IJsonProperty<T6> property6, IJsonProperty<T7> property7, IJsonProperty<T8> property8, IJsonProperty<T9> property9, IJsonProperty<T10> property10, IJsonProperty<T11> property11, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult> projector) =>
+                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5, IJsonProperty<T6> property6, IJsonProperty<T7> property7, IJsonProperty<T8> property8, IJsonProperty<T9> property9, IJsonProperty<T10> property10, IJsonProperty<T11> property11,
+                Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult> projector) =>
             Object(property1, property2, property3, property4, property5, property6, property7, property8, property9, property10, property11,
                    NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance,
                    (v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, _, _, _, _, _) =>
@@ -135,7 +145,8 @@ namespace LoRaWan.NetworkServer
 
         public static IJsonReader<TResult>
             Object<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult>(
-                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5, IJsonProperty<T6> property6, IJsonProperty<T7> property7, IJsonProperty<T8> property8, IJsonProperty<T9> property9, IJsonProperty<T10> property10, IJsonProperty<T11> property11, IJsonProperty<T12> property12, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult> projector) =>
+                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5, IJsonProperty<T6> property6, IJsonProperty<T7> property7, IJsonProperty<T8> property8, IJsonProperty<T9> property9, IJsonProperty<T10> property10, IJsonProperty<T11> property11, IJsonProperty<T12> property12,
+                Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult> projector) =>
             Object(property1, property2, property3, property4, property5, property6, property7, property8, property9, property10, property11, property12,
                    NonProperty.Instance, NonProperty.Instance, NonProperty.Instance, NonProperty.Instance,
                    (v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, _, _, _, _) =>
@@ -143,7 +154,8 @@ namespace LoRaWan.NetworkServer
 
         public static IJsonReader<TResult>
             Object<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>(
-                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5, IJsonProperty<T6> property6, IJsonProperty<T7> property7, IJsonProperty<T8> property8, IJsonProperty<T9> property9, IJsonProperty<T10> property10, IJsonProperty<T11> property11, IJsonProperty<T12> property12, IJsonProperty<T13> property13, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult> projector) =>
+                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5, IJsonProperty<T6> property6, IJsonProperty<T7> property7, IJsonProperty<T8> property8, IJsonProperty<T9> property9, IJsonProperty<T10> property10, IJsonProperty<T11> property11, IJsonProperty<T12> property12, IJsonProperty<T13> property13,
+                Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult> projector) =>
             Object(property1, property2, property3, property4, property5, property6, property7, property8, property9, property10, property11, property12, property13,
                    NonProperty.Instance, NonProperty.Instance, NonProperty.Instance,
                    (v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, _, _, _) =>
@@ -151,7 +163,8 @@ namespace LoRaWan.NetworkServer
 
         public static IJsonReader<TResult>
             Object<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult>(
-                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5, IJsonProperty<T6> property6, IJsonProperty<T7> property7, IJsonProperty<T8> property8, IJsonProperty<T9> property9, IJsonProperty<T10> property10, IJsonProperty<T11> property11, IJsonProperty<T12> property12, IJsonProperty<T13> property13, IJsonProperty<T14> property14, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult> projector) =>
+                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5, IJsonProperty<T6> property6, IJsonProperty<T7> property7, IJsonProperty<T8> property8, IJsonProperty<T9> property9, IJsonProperty<T10> property10, IJsonProperty<T11> property11, IJsonProperty<T12> property12, IJsonProperty<T13> property13, IJsonProperty<T14> property14,
+                Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult> projector) =>
             Object(property1, property2, property3, property4, property5, property6, property7, property8, property9, property10, property11, property12, property13, property14,
                    NonProperty.Instance, NonProperty.Instance,
                    (v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, _, _) =>
@@ -159,7 +172,8 @@ namespace LoRaWan.NetworkServer
 
         public static IJsonReader<TResult>
             Object<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>(
-                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5, IJsonProperty<T6> property6, IJsonProperty<T7> property7, IJsonProperty<T8> property8, IJsonProperty<T9> property9, IJsonProperty<T10> property10, IJsonProperty<T11> property11, IJsonProperty<T12> property12, IJsonProperty<T13> property13, IJsonProperty<T14> property14, IJsonProperty<T15> property15, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult> projector) =>
+                IJsonProperty<T1> property1, IJsonProperty<T2> property2, IJsonProperty<T3> property3, IJsonProperty<T4> property4, IJsonProperty<T5> property5, IJsonProperty<T6> property6, IJsonProperty<T7> property7, IJsonProperty<T8> property8, IJsonProperty<T9> property9, IJsonProperty<T10> property10, IJsonProperty<T11> property11, IJsonProperty<T12> property12, IJsonProperty<T13> property13, IJsonProperty<T14> property14, IJsonProperty<T15> property15,
+                Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult> projector) =>
             Object(property1, property2, property3, property4, property5, property6, property7, property8, property9, property10, property11, property12, property13, property14, property15,
                    NonProperty.Instance,
                    (v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, _) =>
