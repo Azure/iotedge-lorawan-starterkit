51322b4a49c478ccf2ce858727909b37f0e095df
Bastian Burger <baburger@microsoft.com>
2021-10-25 16:41:33 +0200

    Fix broken unit test

--- Tests/Common/MessageProcessorTestBase.cs
+++ Tests/Common/MessageProcessorTestBase.cs
@@ -110,12 +110,12 @@ namespace LoRaWan.Tests.Common
                                                          IPacketForwarder packetForwarder = null,
                                                          TimeSpan? startTimeOffset = null,
                                                          TimeSpan? constantElapsedTime = null,
-                                                         bool useNonDeterministicTiming = false)
+                                                         bool useRealTimer = false)
         {
             var requestStartTime = startTimeOffset.HasValue ? DateTime.UtcNow.Subtract(startTimeOffset.Value) : DateTime.UtcNow;
             var request = new WaitableLoRaRequest(rxpk, packetForwarder ?? PacketForwarder, requestStartTime);
 
-            if (!useNonDeterministicTiming)
+            if (!useRealTimer)
             {
                 constantElapsedTime ??= TimeSpan.Zero;
                 Assert.True(RegionManager.TryResolveRegion(rxpk, out var region));
