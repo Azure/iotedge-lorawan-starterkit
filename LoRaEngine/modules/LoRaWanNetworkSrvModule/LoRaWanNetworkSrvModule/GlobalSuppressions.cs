509c89bc11874617c1b57c979b79d126685d1ab5
Mikhail Chatillon <chmikhai@microsoft.com>
2021-10-31 20:12:13 +0100

    Enforce file header validation

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWanNetworkSrvModule/GlobalSuppressions.cs
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWanNetworkSrvModule/GlobalSuppressions.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System.Diagnostics.CodeAnalysis;
 
 // Bug: https://github.com/dotnet/roslyn-analyzers/issues/5184
