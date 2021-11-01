939312bcecfad1174ff0e041e51361328b05c888
Daniele Antonio Maggio <damaggio@microsoft.com>
2021-11-01 21:35:55 +0100

    Adding JsonReaders for RadioMetadata, UpstreamDataFrame and JoinRequestFrame

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/BasicsStation/JsonHandlers/LnsData.cs
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/BasicsStation/JsonHandlers/LnsData.cs
@@ -40,5 +40,86 @@ namespace LoRaWan.NetworkServer.BasicsStation.JsonHandlers
 
         internal static readonly IJsonReader<string> VersionMessageReader =
             JsonReader.Object(JsonReader.Property("station", JsonReader.String()));
+
+        
+        /*
+                  {
+                    ...
+                    "DR"    : INT,
+                    "Freq"  : INT,
+                    "upinfo": {
+                      "rctx"    : INT64,
+                      "xtime"   : INT64,
+                      "gpstime" : INT64,
+                      "rssi"    : FLOAT,
+                      "snr"     : FLOAT
+                    }
+                  }
+               */
+        internal static readonly IJsonReader<RadioMetadata> RadioMetadataReader =
+            JsonReader.Object(JsonReader.Property("DR", JsonReader.UInt32()),
+                              JsonReader.Property("Freq", JsonReader.UInt32()),
+                              JsonReader.Property("upinfo",
+                                                  JsonReader.Object(JsonReader.Property("rctx", JsonReader.UInt32()),
+                                                                    JsonReader.Property("xtime", JsonReader.UInt64()),
+                                                                    JsonReader.Property("gpstime", JsonReader.UInt32()),
+                                                                    JsonReader.Property("rssi", JsonReader.Double()),
+                                                                    JsonReader.Property("snr", JsonReader.Single()),
+                                                                    (Rctx, Xtime, GpsTime, Rssi, Snr) => (Rctx, Xtime, GpsTime, Rssi, Snr))),
+                              (DR, Freq, upinfo) => new RadioMetadata(new DataRate((int)DR), new Hertz(Freq), upinfo.Rctx, upinfo.Xtime, upinfo.GpsTime, upinfo.Rssi, upinfo.Snr));
+
+        /*
+            {
+              "msgtype"   : "updf",
+              "MHdr"      : UINT,
+              "DevAddr"   : INT32,
+              "FCtrl"     : UINT,
+              "FCnt",     : UINT,
+              "FOpts"     : "HEX",
+              "FPort"     : INT(-1..255),
+              "FRMPayload": "HEX",
+              "MIC"       : INT32,
+              ..
+              RADIOMETADATA
+            }
+         */
+        internal static readonly IJsonReader<UpstreamDataFrame> UpstreamDataFrameReader =
+            JsonReader.Object(JsonReader.Property("msgtype", from s in JsonReader.String()
+                                                             select s == "updf" ? LnsMessageType.UplinkDataFrame : throw new JsonException("Invalid or unsupported message type: " + s)),
+                              JsonReader.Property("MHdr", JsonReader.Byte()),
+                              JsonReader.Property("DevAddr", JsonReader.UInt32()),
+                              JsonReader.Property("FCtrl", JsonReader.Byte()),
+                              JsonReader.Property("FCnt", JsonReader.UInt16()),
+                              JsonReader.Property("FOpts", JsonReader.String()),
+                              JsonReader.Property("FPort", JsonReader.Byte()),
+                              JsonReader.Property("FRMPayload", JsonReader.String()),
+                              JsonReader.Property("MIC", JsonReader.Int32()),
+                              (_, MHdr, DevAddr, FCtrl, FCnt, FOpts, FPort, FRMPayload, MIC) =>
+                                new UpstreamDataFrame(new MacHeader(MHdr), new DevAddr(DevAddr), new FrameControl(FCtrl), FCnt, FOpts, new FramePort(FPort), FRMPayload, new Mic((uint)MIC)));
+        /*
+         * {
+              "msgtype" : "jreq",
+              "MHdr"    : UINT,
+              "JoinEui" : EUI,
+              "DevEui"  : EUI,
+              "DevNonce": UINT,
+              "MIC"     : INT32,
+              ..
+              RADIOMETADATA
+            }
+         */
+        internal static readonly IJsonReader<JoinRequestFrame> JoinRequestFrameReader =
+            JsonReader.Object(JsonReader.Property("msgtype", from s in JsonReader.String()
+                                                             select s == "jreq" ? LnsMessageType.UplinkDataFrame : throw new JsonException("Invalid or unsupported message type: " + s)),
+                              JsonReader.Property("MHdr", JsonReader.Byte()),
+                              JsonReader.Property("JoinEui", from s in JsonReader.String()
+                                                             select Hexadecimal.TryParse(s, out var joinEuiLong, '-') ? joinEuiLong : throw new JsonException("Could not parse JoinEui.")),
+                              JsonReader.Property("DevEui", from s in JsonReader.String()
+                                                            select Hexadecimal.TryParse(s, out var devEuiLong, '-') ? devEuiLong : throw new JsonException("Could not parse JoinEui.")),
+                              JsonReader.Property("DevNonce", JsonReader.UInt16()),
+                              JsonReader.Property("MIC", JsonReader.Int32()),
+                              (_, MHdr, JoinEuiLong, DevEuiLong, DevNonce, MIC) =>
+                                new JoinRequestFrame(new MacHeader(MHdr), new JoinEui(JoinEuiLong), new DevEui(DevEuiLong), new DevNonce(DevNonce), new Mic((uint)MIC)));
+
     }
 }
