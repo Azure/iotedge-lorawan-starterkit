4319c25561e8e39fe56e4fa7858585b7052b25e7
Daniele Antonio Maggio <damaggio@microsoft.com>
2021-11-02 16:33:51 +0100

    Including a GetRegionFromBasicsStationConfiguration for IBasicsStationConfigurationService

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/BasicsStation/BasicsStationConfigurationService.cs
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/BasicsStation/BasicsStationConfigurationService.cs
@@ -7,6 +7,7 @@ namespace LoRaWan.NetworkServer.BasicsStation
     using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
+    using LoRaTools.Regions;
     using LoRaWan.NetworkServer.BasicsStation.JsonHandlers;
     using Microsoft.Azure.Devices.Client.Exceptions;
     using Microsoft.Extensions.Caching.Memory;
@@ -33,6 +34,12 @@ namespace LoRaWan.NetworkServer.BasicsStation
         }
 
         public void Dispose() => this.cacheSemaphore.Dispose();
+
+        public async Task<Region> GetRegionFromBasicsStationConfiguration(StationEui stationEui, CancellationToken cancellationToken)
+        {
+            var config = await this.GetRouterConfigMessageAsync(stationEui, cancellationToken);
+            return LnsStationConfiguration.GetRegion(config);
+        }
 
         public async Task<string> GetRouterConfigMessageAsync(StationEui stationEui, CancellationToken cancellationToken)
         {
