94cd18cfa9f2a358d80a5b9c8d6ef138f2cff9e4
Daniele Antonio Maggio <damaggio@microsoft.com>
2021-11-03 21:15:13 +0100

    Adding LnsMessageTypeExtensions

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/BasicsStation/LnsMessageType.cs
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/BasicsStation/LnsMessageType.cs
@@ -3,6 +3,8 @@
 
 namespace LoRaWan.NetworkServer.BasicsStation
 {
+    using System;
+    using System.Runtime.CompilerServices;
     internal enum LnsMessageType
     {
         Version,                // version
@@ -21,5 +23,41 @@ namespace LoRaWan.NetworkServer.BasicsStation
         RemoteShell,            // rmtsh
 
         */
+    }
+
+    internal static class LnsMessageTypeExtensions
+    {
+        internal static bool TryParseLnsMessageType(string s, out LnsMessageType? lnsMessageType)
+        {
+            lnsMessageType = s switch
+            {
+                "version" => LnsMessageType.Version,
+                "router_config" => LnsMessageType.RouterConfig,
+                "jreq" => LnsMessageType.JoinRequest,
+                "updf" => LnsMessageType.UplinkDataFrame,
+                "dntxed" => LnsMessageType.TransmitConfirmation,
+                "dnmsg" => LnsMessageType.DownlinkMessage,
+                _ => null
+            };
+            return lnsMessageType is not null;
+        }
+
+        internal static LnsMessageType ParseAndValidate(string s, LnsMessageType? expectedType) =>
+            TryParseLnsMessageType(s, out var parsedType)
+               ? expectedType is null ^ parsedType == expectedType
+                   ? parsedType.Value
+                   : throw new InvalidOperationException($"Input msgtype parsed as {parsedType}, but was expecting {expectedType}.")
+               : throw new ArgumentException($"Could not parse {s} as a valid {nameof(LnsMessageType)}.");
+
+        internal static string ToString(this LnsMessageType lnsMessageType) => lnsMessageType switch
+        {
+            LnsMessageType.Version              => "version",
+            LnsMessageType.RouterConfig         => "router_config",
+            LnsMessageType.JoinRequest          => "jreq",
+            LnsMessageType.UplinkDataFrame      => "updf",
+            LnsMessageType.TransmitConfirmation => "dntxed",
+            LnsMessageType.DownlinkMessage      => "dnmsg",
+            _ => throw new SwitchExpressionException(lnsMessageType)
+        };
     }
 }
