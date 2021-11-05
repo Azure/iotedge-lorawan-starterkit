cef93480a7b5f8476e5804671c099900d40dec60
Daniele Antonio Maggio <damaggio@microsoft.com>
2021-11-05 14:11:40 +0100

    Commenting Tmst purpose

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/BasicsStation/BasicStationToRxpk.cs
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/BasicsStation/BasicStationToRxpk.cs
@@ -10,22 +10,19 @@ namespace LoRaWan.NetworkServer.BasicsStation
 
     public class BasicStationToRxpk : Rxpk
     {
-        /*
-           rxpk fields defined here: https://github.com/Lora-net/packet_forwarder/blob/d0226eae6e7b6bbaec6117d0d2372bf17819c438/PROTOCOL.TXT#L131-L147
-         */
         public BasicStationToRxpk(RadioMetadata radioMetadata, Region region)
         {
             if (radioMetadata is null) throw new ArgumentNullException(nameof(radioMetadata));
             if (region is null) throw new ArgumentNullException(nameof(region));
             
-            Tmst = (uint)(radioMetadata.UpInfo.Xtime & 0xFFFFFFFF); // https://github.com/lorabasics/basicstation/blob/bd17e53ab1137de6abb5ae48d6f3d52f6c268299/examples/station2pkfwd/pkfwdc.py#L145
             Freq = radioMetadata.Frequency.Mega;
             Datr = region.DRtoConfiguration[(ushort)radioMetadata.DataRate.AsInt32].configuration;
             Rssi = Convert.ToInt32(radioMetadata.UpInfo.ReceivedSignalStrengthIndication);
             Lsnr = radioMetadata.UpInfo.SignalNoiseRatio;
 
+            Tmst = unchecked((uint)radioMetadata.UpInfo.Xtime); // This is used by former computation only. Should go away when we drop PktFwd support.
             Rfch = radioMetadata.UpInfo.AntennaPreference; // This is not used in any computation. It is only reported in the device telemetry.
-            Chan = (uint)radioMetadata.DataRate.AsInt32; // This is not used in any computation. It is only reported in the device telemetry.
+            Chan = unchecked((uint)radioMetadata.DataRate.AsInt32); // This is not used in any computation. It is only reported in the device telemetry.
             Stat = 1; // This is not used in any computation. It is only reported in the device telemetry.
             Tmms = radioMetadata.UpInfo.GpsTime; // This is not used in any computation. It is only reported in the device telemetry.
             Time = radioMetadata.UpInfo.Xtime.ToString(CultureInfo.InvariantCulture); // This is not used in any computation. It is only reported in the device telemetry.
