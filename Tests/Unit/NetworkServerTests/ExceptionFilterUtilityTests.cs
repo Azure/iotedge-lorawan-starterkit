fb04272164f1082daf9d1ee5d5e4684c591e15db
Bastian Burger <baburger@microsoft.com>
2021-11-05 10:51:44 +0100

    Remove unnecessary test interface

--- Tests/Unit/NetworkServerTests/ExceptionFilterUtilityTests.cs
+++ Tests/Unit/NetworkServerTests/ExceptionFilterUtilityTests.cs
@@ -1,10 +1,11 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace LoRaWan.Tests.Unit.NetworkServerTests
 {
     using LoRaWan.NetworkServer;
     using Moq;
+    using System;
     using Xunit;
 
     public class ExceptionFilterUtilityTests
@@ -13,33 +14,28 @@ namespace LoRaWan.Tests.Unit.NetworkServerTests
         public void True_SuccessCase()
         {
             // arrange
-            var action = new Mock<IAction>();
+            var action = new Mock<Action>();
 
             // act
-            var result = ExceptionFilterUtility.True(action.Object.Act);
+            var result = ExceptionFilterUtility.True(action.Object);
 
             // assert
             Assert.True(result);
-            action.Verify(a => a.Act(), Times.Once);
+            action.Verify(a => a.Invoke(), Times.Once);
         }
 
         [Fact]
         public void False_SuccessCase()
         {
             // arrange
-            var action = new Mock<IAction>();
+            var action = new Mock<Action>();
 
             // act
-            var result = ExceptionFilterUtility.False(action.Object.Act);
+            var result = ExceptionFilterUtility.False(action.Object);
 
             // assert
             Assert.False(result);
-            action.Verify(a => a.Act(), Times.Once);
-        }
-
-        public interface IAction
-        {
-            void Act();
+            action.Verify(a => a.Invoke(), Times.Once);
         }
     }
 }
