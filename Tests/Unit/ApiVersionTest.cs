5acd3271f512792881db3e7e0e92bb9ca63182ae
Spyros Giannakakis <spgianna@microsoft.com>
2021-11-09 23:21:15 +0100

    Restructures FacadeTests/. Removes Tests from the folder name.

--- Tests/Unit/ApiVersionTest.cs
+++ Tests/Unit/ApiVersionTest.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace LoRaWan.Tests.Unit.CommonAPI
+namespace LoRaWan.Tests.Unit.LoRaTools.CommonAPI
 {
     using System;
     using System.Collections.Generic;
