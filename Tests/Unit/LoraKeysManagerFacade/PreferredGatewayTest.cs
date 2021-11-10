d60e03982ae79eb35f9784828eae6990c80224a9
Spyros Giannakakis <spgianna@microsoft.com>
2021-11-10 13:34:07 +0100

    Not used variable.

--- Tests/Unit/LoraKeysManagerFacade/PreferredGatewayTest.cs
+++ Tests/Unit/LoraKeysManagerFacade/PreferredGatewayTest.cs
@@ -91,7 +91,6 @@ namespace LoRaWan.Tests.Unit.LoraKeysManagerFacade.FunctionBundler
 
             var req2 = new FunctionBundlerRequest() { GatewayId = "gateway2", ClientFCntUp = staleFcntUp, Rssi = -90 };
             var pipeline2 = new FunctionBundlerPipelineExecuter(new IFunctionBundlerExecutionItem[] { this.preferredGatewayExecutionItem }, devEUI, req2);
-            var res2 = await this.preferredGatewayExecutionItem.ExecuteAsync(pipeline2);
 
             var t1 = Task.Run(() => this.preferredGatewayExecutionItem.ExecuteAsync(pipeline1));
 
