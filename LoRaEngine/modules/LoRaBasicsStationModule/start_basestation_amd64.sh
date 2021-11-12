e255d508db6151f3360c3b2f8ed534117512b98d
Daniele Antonio Maggio <damaggio@microsoft.com>
2021-11-12 09:46:59 +0100

    Introducing TC_TRUST_PATH in BasicsStation Docker Image (#736)
    * Introducing TC_TRUST_PATH variable and copy
    
    * Adding a default TC_TRUST_PATH

--- LoRaEngine/modules/LoRaBasicsStationModule/start_basestation_amd64.sh
+++ LoRaEngine/modules/LoRaBasicsStationModule/start_basestation_amd64.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DEFAULT_TC_TRUST_PATH="/var/lorastarterkit/certs/tc.trust"
+
 if [[ -z "$RESET_PIN" ]]; then
     echo "No RESET_PIN environment variable set, skipping the pin reset. If you experience problem with starting the concentrator please set this variable to your manufacturer reset pin"
 else
@@ -13,6 +15,18 @@ fi
 if [[ -z "$SPI_DEV" ]] || [[ $SPI_DEV == '$LBS_SPI_DEV' ]]; then
     echo "No custom SPI dev set up, defaulting to spi dev 0"
     SPI_DEV=0
+fi
+
+if [[ -z "$TC_TRUST_PATH" ]]; then
+    echo "No TC_TRUST_PATH detected in environment variables. Trying to check for default location."
+    if [ -z "$(ls -A $DEFAULT_TC_TRUST_PATH 2> /dev/null)" ]; then
+        echo "No file found at $DEFAULT_TC_TRUST_PATH. Nothing was copied over."
+    else
+        echo "A file was found at $DEFAULT_TC_TRUST_PATH. Copying it over."
+        cp -v $DEFAULT_TC_TRUST_PATH .
+    fi
+else
+    cp -v $TC_TRUST_PATH tc.trust
 fi
 
 if [[ -z "$TC_URI" ]]; then
