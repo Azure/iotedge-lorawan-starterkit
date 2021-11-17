10a13600b0cc7fcd6d556736767c41ce66951173
Mikhail Chatillon <chmikhai@microsoft.com>
2021-11-17 09:25:29 +0100

    Address comments

--- .github/actions/iotedgedev/entrypoint.sh
+++ .github/actions/iotedgedev/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This file generate an IoT Edge deployment file using token replacement to replace secrets with environment variables
+# then the file proceed to remove the previous deployment and replace it with the current one
 sudo -E iotedgedev genconfig -f $2/$1 -P $3  --fail-on-validation-error
 sudo az extension add --name azure-iot
 sudo -E az iot edge deployment delete --login "$IOTHUB_CONNECTION_STRING" --deployment-id "$IOT_EDGE_DEPLOYMENT_ID"
