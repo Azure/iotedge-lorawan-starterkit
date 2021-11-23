a49f2d82b08026fc601502cd63fa5bf25c293563
Atif Aziz <code@raboof.com>
2021-11-23 13:59:26 +0100

    Make "TcpLogSink" a private type

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/Logger/TcpLogger.cs
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/Logger/TcpLogger.cs
@@ -32,12 +32,12 @@ namespace Logger
     internal sealed class TcpLogger : ILogger
     {
         private readonly LogLevel logLevel;
-        private readonly TcpLogSink logSink;
+        private readonly Action<string> logger;
 
-        public TcpLogger(LogLevel logLevel, TcpLogSink logSink)
+        public TcpLogger(LogLevel logLevel, Action<string> logger)
         {
             this.logLevel = logLevel;
-            this.logSink = logSink;
+            this.logger = logger;
         }
 
         /// <summary>
@@ -59,21 +59,21 @@ namespace Logger
 
             var formattedMessage = LoggerHelper.AddScopeInformation(ExternalScopeProvider, formatter(state, exception), ":");
 
-            this.logSink.Log(formattedMessage);
+            this.logger(formattedMessage);
         }
     }
 
     public static class TcpLoggerExtensions
     {
-        public static ILoggingBuilder AddTcpLogger(this ILoggingBuilder builder, TcpLoggerConfiguration configuration, ILogger<TcpLogSink>? tcpLogSinkLogger = null)
+        public static ILoggingBuilder AddTcpLogger(this ILoggingBuilder builder, TcpLoggerConfiguration configuration, ILoggerFactory? loggerFactory = null)
         {
             _ = builder ?? throw new ArgumentNullException(nameof(builder));
 
             builder.AddConfiguration();
-            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, TcpLoggerProvider>(_ => new TcpLoggerProvider(configuration.LogLevel, Init(configuration, tcpLogSinkLogger))));
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, TcpLoggerProvider>(_ => new TcpLoggerProvider(configuration.LogLevel, Init(configuration, loggerFactory?.CreateLogger(typeof(TcpLogSink).FullName!)))));
             return builder;
 
-            static TcpLogSink Init(TcpLoggerConfiguration configuration, ILogger<TcpLogSink>? tcpLogSinkLogger = null)
+            static TcpLogSink Init(TcpLoggerConfiguration configuration, ILogger? tcpLogSinkLogger = null)
             {
                 if (configuration == null) throw new ArgumentNullException(nameof(configuration));
 
@@ -144,7 +144,7 @@ namespace Logger
             }
 
             public ILogger CreateLogger(string categoryName) =>
-                this.loggers.GetOrAdd(categoryName, _ => new TcpLogger(this.logLevel, this.logSink)
+                this.loggers.GetOrAdd(categoryName, _ => new TcpLogger(this.logLevel, this.logSink.Log)
                 {
                     ExternalScopeProvider = this.externalScopeProvider
                 });
@@ -155,163 +155,163 @@ namespace Logger
                 this.loggers.Clear();
             }
         }
-    }
 
-    public sealed class TcpLogSink : IDisposable
-    {
-        private readonly ILogger? logger;
-        private readonly IPEndPoint serverEndpoint;
-        private readonly Func<string, string>? formatter;
-        private readonly int maxRetryAttempts;
-        private readonly TimeSpan retryDelay;
-        private readonly CancellationTokenSource cancellationTokenSource = new();
-        private readonly Channel<string> channel;
-
-        public static TcpLogSink Start(IPEndPoint serverEndPoint,
-                                       int? maxRetryAttempts = null,
-                                       TimeSpan? retryDelay = null,
-                                       int? backlogCapacity = null,
-                                       Func<string, string>? formatter = null,
-                                       ILogger<TcpLogSink>? logger = null)
+        private sealed class TcpLogSink : IDisposable
         {
-            var sink = new TcpLogSink(serverEndPoint, maxRetryAttempts, retryDelay, backlogCapacity, formatter, logger);
-            _ = Task.Run(() => sink.SendAllLogMessagesAsync(sink.cancellationTokenSource.Token));
-            return sink;
-        }
+            private readonly ILogger? logger;
+            private readonly IPEndPoint serverEndpoint;
+            private readonly Func<string, string>? formatter;
+            private readonly int maxRetryAttempts;
+            private readonly TimeSpan retryDelay;
+            private readonly CancellationTokenSource cancellationTokenSource = new();
+            private readonly Channel<string> channel;
 
-        private TcpLogSink(IPEndPoint serverEndpoint,
-                           int? maxRetryAttempts, TimeSpan? retryDelay, int? backlogCapacity,
-                           Func<string, string>? formatter,
-                           ILogger? logger)
-        {
-            this.serverEndpoint = serverEndpoint;
-            this.formatter = formatter;
-            this.maxRetryAttempts = maxRetryAttempts ?? 6;
-            this.retryDelay = retryDelay ?? TimeSpan.FromSeconds(10);
-            this.channel = Channel.CreateBounded<string>(new BoundedChannelOptions(backlogCapacity ?? 1000)
+            public static TcpLogSink Start(IPEndPoint serverEndPoint,
+                                           int? maxRetryAttempts = null,
+                                           TimeSpan? retryDelay = null,
+                                           int? backlogCapacity = null,
+                                           Func<string, string>? formatter = null,
+                                           ILogger? logger = null)
             {
-                SingleReader = true,
-                FullMode = BoundedChannelFullMode.DropOldest,
-            });
-            this.logger = logger;
-        }
-
-        public void Dispose()
-        {
-            try
-            {
-                this.cancellationTokenSource.Cancel();
-            }
-            catch (ObjectDisposedException)
-            {
-                return; // bail out because this means object has already been disposed before
-            }
-            catch (AggregateException ex)
-            {
-                // "AggregateException" is thrown and contains all the exceptions thrown by the
-                // registered callbacks on the associated "CancellationToken". However,
-                // "IDisposable.Dispose" methods are not expected to throw exceptions thus it is
-                // simply ignore here.
-
-                Debug.WriteLine(ex);
+                var sink = new TcpLogSink(serverEndPoint, maxRetryAttempts, retryDelay, backlogCapacity, formatter, logger);
+                _ = Task.Run(() => sink.SendAllLogMessagesAsync(sink.cancellationTokenSource.Token));
+                return sink;
             }
 
-            this.cancellationTokenSource.Dispose();
-        }
-
-        private async Task SendAllLogMessagesAsync(CancellationToken cancellationToken)
-        {
-            var encoding = Encoding.UTF8;
-            var buffers = ArrayPool<byte>.Shared;
-            var client = new TcpClient();
-
-            try
+            private TcpLogSink(IPEndPoint serverEndpoint,
+                               int? maxRetryAttempts, TimeSpan? retryDelay, int? backlogCapacity,
+                               Func<string, string>? formatter,
+                               ILogger? logger)
             {
-                await foreach (var log in this.channel.Reader.ReadAllAsync(cancellationToken))
+                this.serverEndpoint = serverEndpoint;
+                this.formatter = formatter;
+                this.maxRetryAttempts = maxRetryAttempts ?? 6;
+                this.retryDelay = retryDelay ?? TimeSpan.FromSeconds(10);
+                this.channel = Channel.CreateBounded<string>(new BoundedChannelOptions(backlogCapacity ?? 1000)
                 {
-                    var lines = log.IndexOfAny(NewLineChars) >= 0
-                              ? new StringValues(SplitIntoLines(log).Append(string.Empty).ToArray()) // re-normalize
-                              : new StringValues(log);
+                    SingleReader = true,
+                    FullMode = BoundedChannelFullMode.DropOldest,
+                });
+                this.logger = logger;
+            }
 
-                    foreach (var line in lines)
+            public void Dispose()
+            {
+                try
+                {
+                    this.cancellationTokenSource.Cancel();
+                }
+                catch (ObjectDisposedException)
+                {
+                    return; // bail out because this means object has already been disposed before
+                }
+                catch (AggregateException ex)
+                {
+                    // "AggregateException" is thrown and contains all the exceptions thrown by the
+                    // registered callbacks on the associated "CancellationToken". However,
+                    // "IDisposable.Dispose" methods are not expected to throw exceptions thus it is
+                    // simply ignore here.
+
+                    Debug.WriteLine(ex);
+                }
+
+                this.cancellationTokenSource.Dispose();
+            }
+
+            private async Task SendAllLogMessagesAsync(CancellationToken cancellationToken)
+            {
+                var encoding = Encoding.UTF8;
+                var buffers = ArrayPool<byte>.Shared;
+                var client = new TcpClient();
+
+                try
+                {
+                    await foreach (var log in this.channel.Reader.ReadAllAsync(cancellationToken))
                     {
-                        for (var attempt = 1; ; attempt++)
+                        var lines = log.IndexOfAny(NewLineChars) >= 0
+                                  ? new StringValues(SplitIntoLines(log).Append(string.Empty).ToArray()) // re-normalize
+                                  : new StringValues(log);
+
+                        foreach (var line in lines)
                         {
-                            try
+                            for (var attempt = 1; ; attempt++)
                             {
-                                if (!client.Connected)
-                                {
-                                    this.logger?.LogDebug("Connecting to log server at (attempt {Attempt}/{MaxRetryAttempts}): {ServerEndpoint}", attempt, this.maxRetryAttempts, this.serverEndpoint);
-                                    await client.ConnectAsync(this.serverEndpoint.Address, this.serverEndpoint.Port);
-                                }
-
-                                var size = encoding.GetByteCount(line) + 2;
-                                var buffer = buffers.Rent(size);
-                                var bi = encoding.GetBytes(line, buffer);
-                                buffer[bi++] = 13; // CR
-                                buffer[bi++] = 10; // LF
-                                Debug.Assert(bi == size);
-
                                 try
                                 {
-                                    await client.GetStream().WriteAsync(buffer.AsMemory(..bi), cancellationToken);
-                                    break; // don't retry on success
+                                    if (!client.Connected)
+                                    {
+                                        this.logger?.LogDebug("Connecting to log server at (attempt {Attempt}/{MaxRetryAttempts}): {ServerEndpoint}", attempt, this.maxRetryAttempts, this.serverEndpoint);
+                                        await client.ConnectAsync(this.serverEndpoint.Address, this.serverEndpoint.Port);
+                                    }
+
+                                    var size = encoding.GetByteCount(line) + 2;
+                                    var buffer = buffers.Rent(size);
+                                    var bi = encoding.GetBytes(line, buffer);
+                                    buffer[bi++] = 13; // CR
+                                    buffer[bi++] = 10; // LF
+                                    Debug.Assert(bi == size);
+
+                                    try
+                                    {
+                                        await client.GetStream().WriteAsync(buffer.AsMemory(..bi), cancellationToken);
+                                        break; // don't retry on success
+                                    }
+                                    finally
+                                    {
+                                        buffers.Return(buffer);
+                                    }
                                 }
-                                finally
+                                catch (Exception ex) when (ex is SocketException or IOException)
                                 {
-                                    buffers.Return(buffer);
+                                    this.logger?.LogError(ex, "Error writing to the logging socket.");
+                                    client.Dispose();
+                                    client = new TcpClient();
+                                    if (attempt == this.maxRetryAttempts)
+                                    {
+                                        this.logger?.LogWarning("Dropping message after all attempts failed: {Line}", line);
+                                        break;
+                                    }
+                                    this.logger?.LogDebug("Waiting (delay = {RetryDelay}) before retrying to connecting to logging server.", this.retryDelay);
+                                    await Task.Delay(this.retryDelay, cancellationToken);
                                 }
-                            }
-                            catch (Exception ex) when (ex is SocketException or IOException)
-                            {
-                                this.logger?.LogError(ex, "Error writing to the logging socket.");
-                                client.Dispose();
-                                client = new TcpClient();
-                                if (attempt == this.maxRetryAttempts)
-                                {
-                                    this.logger?.LogWarning("Dropping message after all attempts failed: {Line}", line);
-                                    break;
-                                }
-                                this.logger?.LogDebug("Waiting (delay = {RetryDelay}) before retrying to connecting to logging server.", this.retryDelay);
-                                await Task.Delay(this.retryDelay, cancellationToken);
                             }
                         }
                     }
                 }
+                finally
+                {
+                    client.Dispose();
+                }
+
+                static IEnumerable<string> SplitIntoLines(string input)
+                {
+                    using var reader = new StringReader(input);
+                    while (reader.ReadLine() is { } line)
+                        yield return line;
+                }
             }
-            finally
+
+            private static readonly char[] NewLineChars = { '\n', '\r' };
+
+            public void Log(string message)
             {
-                client.Dispose();
+                if (message == null) throw new ArgumentNullException(nameof(message));
+
+                // NOTE! The following will induce an "ObjectDisposedException" in two ways.
+                // When this object is disposed, it cancels the cancellation token source then proceeds
+                // to dispose it. If the cancellation token source is disposed then accessing its token
+                // will "ObjectDisposedException". If it's been cancelled but not yet disposed then the
+                // cancellation token query will return true and we'll throw "ObjectDisposedException".
+                // Either way, if the object is being disposed while we end up here then an
+                // "ObjectDisposedException" will get thrown. If by chance it gets disposed past the
+                // following gate, then the worst that can happen is that another message can end up on
+                // the channel, which is harmless.
+
+                if (this.cancellationTokenSource.Token.IsCancellationRequested)
+                    throw new ObjectDisposedException(nameof(TcpLogSink));
+
+                _ = this.channel.Writer.TryWrite(this.formatter?.Invoke(message) ?? message);
             }
-
-            static IEnumerable<string> SplitIntoLines(string input)
-            {
-                using var reader = new StringReader(input);
-                while (reader.ReadLine() is { } line)
-                    yield return line;
-            }
-        }
-
-        private static readonly char[] NewLineChars = { '\n', '\r' };
-
-        public void Log(string message)
-        {
-            if (message == null) throw new ArgumentNullException(nameof(message));
-
-            // NOTE! The following will induce an "ObjectDisposedException" in two ways.
-            // When this object is disposed, it cancels the cancellation token source then proceeds
-            // to dispose it. If the cancellation token source is disposed then accessing its token
-            // will "ObjectDisposedException". If it's been cancelled but not yet disposed then the
-            // cancellation token query will return true and we'll throw "ObjectDisposedException".
-            // Either way, if the object is being disposed while we end up here then an
-            // "ObjectDisposedException" will get thrown. If by chance it gets disposed past the
-            // following gate, then the worst that can happen is that another message can end up on
-            // the channel, which is harmless.
-
-            if (this.cancellationTokenSource.Token.IsCancellationRequested)
-                throw new ObjectDisposedException(nameof(TcpLogSink));
-
-            _ = this.channel.Writer.TryWrite(this.formatter?.Invoke(message) ?? message);
         }
     }
 }
