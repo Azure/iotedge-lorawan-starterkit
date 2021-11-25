65d4086930ac94f6d3c44ce6d129509c0282110f
Bastian Burger <baburger@microsoft.com>
2021-11-25 18:28:33 +0100

    Make RotateCertificateOptions immutable

--- Tools/Cli-LoRa-Device-Provisioning/CommandLineOptions/RotateCertificateOptions.cs
+++ Tools/Cli-LoRa-Device-Provisioning/CommandLineOptions/RotateCertificateOptions.cs
@@ -8,19 +8,26 @@ namespace LoRaWan.Tools.CLI.CommandLineOptions
     [Verb("rotate-certificate", HelpText = "Rotates the certificates for a station.")]
     public class RotateCertificateOptions
     {
+        public RotateCertificateOptions(string stationEui, string certificateBundleLocation, string clientCertificateThumbprint)
+        {
+            StationEui = stationEui;
+            CertificateBundleLocation = certificateBundleLocation;
+            ClientCertificateThumbprint = clientCertificateThumbprint;
+        }
+
         [Option("stationeui",
                 Required = true,
                 HelpText = "Station EUI: Required id '--concentrator' switch is set. A 16 bit hex string ('AABBCCDDEEFFGGHH').")]
-        public string StationEui { get; set; }
+        public string StationEui { get; }
 
         [Option("certificate-bundle-location",
                 Required = true,
                 HelpText = "Certificate bundle location: Points to the location of the (UTF-8-encoded) certificate bundle file.")]
-        public string CertificateBundleLocation { get; set; }
+        public string CertificateBundleLocation { get; }
 
         [Option("client-certificate-thumbprint",
                 Required = true,
                 HelpText = "Client certificate thumbprint: A client certificate thumbprint that should be accepted by the CUPS/LNS endpoints.")]
-        public string ClientCertificateThumbprint { get; set; }
+        public string ClientCertificateThumbprint { get; }
     }
 }
