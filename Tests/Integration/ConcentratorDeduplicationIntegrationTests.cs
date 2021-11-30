f62897619594114fea3c7b1db785fe3b6747dbba
Spyros Giannakakis <spgianna@microsoft.com>
2021-11-30 09:05:00 +0100

    Clean up integration test.

--- Tests/Integration/ConcentratorDeduplicationIntegrationTests.cs
+++ Tests/Integration/ConcentratorDeduplicationIntegrationTests.cs
@@ -20,6 +20,14 @@ namespace LoRaWan.Tests.Integration
         private WaitableLoRaRequest loraRequest;
         private LoRaDevice loRaDevice;
 
+        /// <summary>
+        /// This test integrates <code>DefaultLoRaDataRequestHandler</code> with <code>ConcentratorDeduplication</code>
+        /// with a partial mock on <code>DefaultLoRaDataRequestHandler</code> that stops execution after the deduplication takes place.
+        /// </summary>
+        /// <param name="station1"></param>
+        /// <param name="station2"></param>
+        /// <param name="expectedFailure"></param>
+        /// <returns></returns>
         [Theory]
         [InlineData("11-11-11-11-11-11-11-11", "11-11-11-11-11-11-11-11", LoRaDeviceRequestFailedReason.UnknownDevice)]
         [InlineData("11-11-11-11-11-11-11-11", "22-22-22-22-22-22-22-22", LoRaDeviceRequestFailedReason.DeduplicationDrop)]
@@ -29,22 +37,21 @@ namespace LoRaWan.Tests.Integration
             var simulatedDevice = new SimulatedDevice(TestDeviceInfo.CreateABPDevice(0));
             var dataPayload = simulatedDevice.CreateConfirmedDataUpMessage("payload");
             this.loraRequest = WaitableLoRaRequest.Create(dataPayload);
-
-            loraRequest.UseTimeWatcher(new TestLoRaOperationTimeWatcher(new RegionEU868(), new[] { TimeSpan.FromMilliseconds(10) }));
-            loraRequest.SetStationEui(StationEui.Parse(station1));
+            this.loraRequest.UseTimeWatcher(new TestLoRaOperationTimeWatcher(new RegionEU868(), new[] { TimeSpan.FromMilliseconds(10) }));
+            this.loraRequest.SetStationEui(StationEui.Parse(station1));
 
             this.loRaDevice = new LoRaDevice(simulatedDevice.DevAddr, simulatedDevice.DevEUI, ConnectionManager)
             {
                 Deduplication = DeduplicationMode.Drop
             };
-            
+
             var deduplicationFactory = new DeduplicationStrategyFactory(NullLoggerFactory.Instance, NullLogger<DeduplicationStrategyFactory>.Instance);
             using var cache = new MemoryCache(new MemoryCacheOptions());
             var registryMock = new Mock<WebSocketWriterRegistry<StationEui, string>>(MockBehavior.Default, NullLogger<WebSocketWriterRegistry<StationEui, string>>.Instance);
             _ = registryMock.Setup(r => r.IsSocketWriterOpen(It.IsAny<StationEui>())).Returns(true);
-
             using var concentratorDeduplication = new ConcentratorDeduplication(cache, deduplicationFactory, registryMock.Object, NullLogger<IConcentratorDeduplication>.Instance);
-            var mock = new Mock<DefaultLoRaDataRequestHandler>(MockBehavior.Default,
+
+            var dataRequestHandlerMock = new Mock<DefaultLoRaDataRequestHandler>(MockBehavior.Default,
                 ServerConfiguration,
                 FrameCounterUpdateStrategyProvider,
                 concentratorDeduplication,
@@ -53,22 +60,23 @@ namespace LoRaWan.Tests.Integration
                 new LoRaADRStrategyProvider(NullLoggerFactory.Instance),
                 new LoRAADRManagerFactory(LoRaDeviceApi.Object, NullLoggerFactory.Instance),
                 new FunctionBundlerProvider(LoRaDeviceApi.Object, NullLoggerFactory.Instance, NullLogger<FunctionBundlerProvider>.Instance),
-                 NullLogger<DefaultLoRaDataRequestHandler>.Instance)
+                NullLogger<DefaultLoRaDataRequestHandler>.Instance)
             {
                 CallBase = true
             };
-            _ = mock.Setup(x => x.DetermineIfFramecounterIsFromNewlyStartedDeviceAsync(It.IsAny<LoRaDevice>(), 1, It.IsAny<ILoRaDeviceFrameCounterUpdateStrategy>()))
+            _ = dataRequestHandlerMock.Setup(x => x.DetermineIfFramecounterIsFromNewlyStartedDeviceAsync(It.IsAny<LoRaDevice>(), 1, It.IsAny<ILoRaDeviceFrameCounterUpdateStrategy>()))
                 .ReturnsAsync(true);
             var result1 = true;
-            var result2 = new LoRaDeviceRequestProcessResult(loRaDevice, loraRequest, LoRaDeviceRequestFailedReason.UnknownDevice);
-            _ = mock.Setup(x => x.ValidateRequest(It.IsAny<LoRaRequest>(), It.IsAny<bool>(), It.IsAny<uint>(), It.IsAny<LoRaDevice>(), It.IsAny<bool>(), out result1, out result2))
+            var result2 = new LoRaDeviceRequestProcessResult(this.loRaDevice, this.loraRequest, LoRaDeviceRequestFailedReason.UnknownDevice);
+            _ = dataRequestHandlerMock.Setup(x => x.ValidateRequest(It.IsAny<LoRaRequest>(), It.IsAny<bool>(), It.IsAny<uint>(), It.IsAny<LoRaDevice>(), It.IsAny<bool>(), out result1, out result2))
                 .Returns(false);
 
-            _ = await mock.Object.ProcessRequestAsync(this.loraRequest, this.loRaDevice);
+            // first request
+            _ = await dataRequestHandlerMock.Object.ProcessRequestAsync(this.loraRequest, this.loRaDevice);
 
             // act
             this.loraRequest.SetStationEui(StationEui.Parse(station2));
-            var actual = await mock.Object.ProcessRequestAsync(this.loraRequest, this.loRaDevice);
+            var actual = await dataRequestHandlerMock.Object.ProcessRequestAsync(this.loraRequest, this.loRaDevice);
 
             // assert
             Assert.Equal(expectedFailure, actual.FailedReason);
