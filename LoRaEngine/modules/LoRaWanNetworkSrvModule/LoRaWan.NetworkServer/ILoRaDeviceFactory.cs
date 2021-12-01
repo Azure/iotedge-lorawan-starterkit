fca2195c443f41d7e721741981a7f1b0c5516504
Atif Aziz <code@raboof.com>
2021-12-01 17:31:08 +0100

    Remove empty param doc

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/ILoRaDeviceFactory.cs
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/ILoRaDeviceFactory.cs
@@ -15,8 +15,6 @@ namespace LoRaWan.NetworkServer
         /// is added to the connection manager in any case to have a way to refresh the information and
         /// detect owner change for cached devices.
         /// </summary>
-        /// <param name="deviceInfo"></param>
-        /// <param name="cancellationToken"></param>
         /// <returns>A new <see cref="LoRaDevice"/></returns>
         /// <exception cref="InvalidOperationException">If the device was previously registered.</exception>
         /// <exception cref="ArgumentNullException">If the deviceInfo is null.</exception>
