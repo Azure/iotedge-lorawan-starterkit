182223aa385c9b734843b17aaed0e640f8acd05a
Bastian Burger <baburger@microsoft.com>
2021-12-02 13:10:43 +0100

    Dispose logger provider in IoT Hub Logger tests

--- Tests/Unit/Logger/IoTHubLoggerTests.cs
+++ Tests/Unit/Logger/IoTHubLoggerTests.cs
@@ -23,13 +23,13 @@ namespace LoRaWan.Tests.Unit.Logger
         public async Task If_Log_Level_Is_Higher_Log_Is_Written(LogLevel configuredLevel, LogLevel entryLevel, int sendMessageCount)
         {
             // arrange
-            var testableLogger = SetupProviderAndLogger(new IotHubLoggerConfiguration(configuredLevel, default, false));
+            using var testableLogger = SetupProviderAndLogger(new IotHubLoggerConfiguration(configuredLevel, default, false));
 
             // act
-            testableLogger.Object.Log(entryLevel, "foo");
+            testableLogger.Value.Object.Log(entryLevel, "foo");
 
             // assert
-            await VerifySendMessageCountAsync(testableLogger, sendMessageCount);
+            await VerifySendMessageCountAsync(testableLogger.Value, sendMessageCount);
         }
 
         [Theory]
@@ -40,67 +40,67 @@ namespace LoRaWan.Tests.Unit.Logger
         public async Task When_EventId_Changes_Output_Is_Written_When_Matching_Or_Omitted(int configuredEventId, int loggedEventId, int sendMessageCount)
         {
             // arrange
-            var testableLogger = SetupProviderAndLogger(new IotHubLoggerConfiguration(LogLevel.Trace, configuredEventId, false));
+            using var testableLogger = SetupProviderAndLogger(new IotHubLoggerConfiguration(LogLevel.Trace, configuredEventId, false));
 
             // act
-            testableLogger.Object.LogInformation(loggedEventId, "Test");
+            testableLogger.Value.Object.LogInformation(loggedEventId, "Test");
 
             // assert
-            await VerifySendMessageCountAsync(testableLogger, sendMessageCount);
+            await VerifySendMessageCountAsync(testableLogger.Value, sendMessageCount);
         }
 
         [Fact]
         public async Task When_Structured_Parameters_Are_Passed_They_Are_Replaced()
         {
             // arrange
-            var testableLogger = SetupProviderAndLogger();
+            using var testableLogger = SetupProviderAndLogger();
 
             // act
-            testableLogger.Object.LogInformation("{Id}", 1);
+            testableLogger.Value.Object.LogInformation("{Id}", 1);
 
             // assert
-            await VerifyMessageAsync(testableLogger, "1");
+            await VerifyMessageAsync(testableLogger.Value, "1");
         }
 
         [Fact]
         public async void Scope_Is_Set_In_Message_When_Activated()
         {
             // arrange
-            var testableLogger = SetupProviderAndLogger(new IotHubLoggerConfiguration(LogLevel.Trace, default, UseScopes: true));
+            using var testableLogger = SetupProviderAndLogger(new IotHubLoggerConfiguration(LogLevel.Trace, default, UseScopes: true));
             const string devAddr = "12345678ADDR";
             const string message = "foo";
 
             // act
-            using var scope = testableLogger.Object.BeginDeviceAddressScope(devAddr);
-            testableLogger.Object.LogInformation(message);
+            using var scope = testableLogger.Value.Object.BeginDeviceAddressScope(devAddr);
+            testableLogger.Value.Object.LogInformation(message);
 
             // assert
-            await VerifyMessageAsync(testableLogger, devAddr + " " + message);
+            await VerifyMessageAsync(testableLogger.Value, devAddr + " " + message);
         }
 
         [Fact]
         public async void Scope_Is_Not_Set_In_Message_When_Deactivated()
         {
             // arrange
-            var testableLogger = SetupProviderAndLogger(new IotHubLoggerConfiguration(LogLevel.Trace, default, UseScopes: false));
+            using var testableLogger = SetupProviderAndLogger(new IotHubLoggerConfiguration(LogLevel.Trace, default, UseScopes: false));
             const string devAddr = "12345678ADDR";
             const string message = "foo";
 
             // act
-            using var scope = testableLogger.Object.BeginDeviceAddressScope(devAddr);
-            testableLogger.Object.LogInformation(message);
+            using var scope = testableLogger.Value.Object.BeginDeviceAddressScope(devAddr);
+            testableLogger.Value.Object.LogInformation(message);
 
             // assert
-            await VerifyMessageAsync(testableLogger, message);
+            await VerifyMessageAsync(testableLogger.Value, message);
         }
 
-        private static Mock<IotHubLogger> SetupProviderAndLogger() =>
+        private static DisposableValue<Mock<IotHubLogger>> SetupProviderAndLogger() =>
             SetupProviderAndLogger(new IotHubLoggerConfiguration(LogLevel.Trace, default, false));
 
-        private static Mock<IotHubLogger> SetupProviderAndLogger(IotHubLoggerConfiguration configuration)
+        private static DisposableValue<Mock<IotHubLogger>> SetupProviderAndLogger(IotHubLoggerConfiguration configuration)
         {
             var provider = new IotHubLoggerProvider(configuration, new Lazy<Task<ModuleClient>>((Task<ModuleClient>)null!));
-            return new Mock<IotHubLogger>(provider, null);
+            return new DisposableValue<Mock<IotHubLogger>>(new Mock<IotHubLogger>(provider, null), provider);
         }
 
         private static async Task VerifyMessageAsync(Mock<IotHubLogger> logger, string message)
