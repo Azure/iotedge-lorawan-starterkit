Azure/iotedge-lorawan-starterkit

commit 29e011ef980e07c0f269b26976e204f05b81dedf

Fix "FreqHertz" of "Rxpk" & "Txpk" to be mutable

LoRaEngine/modules/LoRaWanNetworkSrvModule/LoraTools/LoRaPhysical/Rxpk.cs
@@ -25,7 +25,7 @@ namespace LoRaTools.LoRaPhysical
         [JsonProperty("freq")]
         public double Freq { get; set; }
         [JsonIgnore]
-        public Hertz FreqHertz => Hertz.FromMega(Freq);
+        public Hertz FreqHertz { get => Hertz.FromMega(Freq); set => Freq = value.Mega; }
         [JsonProperty("chan")]
         public uint Chan { get; set; }
 

LoRaEngine/modules/LoRaWanNetworkSrvModule/LoraTools/LoRaPhysical/Txpk.cs
@@ -25,7 +25,7 @@ namespace LoRaTools.LoRaPhysical
         [JsonProperty("freq")]
         public double Freq { get; set; }
         [JsonIgnore]
-        public Hertz FreqHertz => Hertz.FromMega(Freq);
+        public Hertz FreqHertz { get => Hertz.FromMega(Freq); set => Freq = value.Mega; }
 
         [JsonProperty("rfch")]
         public uint Rfch { get; set; }

commit c4ffeb4fc1c461d5ed4bf34bfdb5d5bb50b12720

Fix compilation errors caused by 29e011ef*
* = 29e011ef980e07c0f269b26976e204f05b81dedf

LoRaEngine/modules/LoRaWanNetworkSrvModule/LoraTools/LoRaPhysical/Rxpk.cs
@@ -25,7 +25,7 @@ namespace LoRaTools.LoRaPhysical
         [JsonProperty("freq")]
         public double Freq { get; set; }
         [JsonIgnore]
-        public Hertz FreqHertz { get => Hertz.FromMega(Freq); set => Freq = value.Mega; }
+        public Hertz FreqHertz { get => Hertz.Mega(Freq); set => Freq = value.InMega; }
         [JsonProperty("chan")]
         public uint Chan { get; set; }
 

LoRaEngine/modules/LoRaWanNetworkSrvModule/LoraTools/LoRaPhysical/Txpk.cs
@@ -25,7 +25,7 @@ namespace LoRaTools.LoRaPhysical
         [JsonProperty("freq")]
         public double Freq { get; set; }
         [JsonIgnore]
-        public Hertz FreqHertz { get => Hertz.FromMega(Freq); set => Freq = value.Mega; }
+        public Hertz FreqHertz { get => Hertz.Mega(Freq); set => Freq = value.InMega; }
 
         [JsonProperty("rfch")]
         public uint Rfch { get; set; }