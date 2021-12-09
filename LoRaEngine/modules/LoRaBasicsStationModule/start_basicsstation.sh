6cdab19bd60bb2325ca224cf2df84a3816923b13
Daniele Antonio Maggio <damaggio@microsoft.com>
2021-12-09 12:57:57 +0100

    Script refactoring

--- LoRaEngine/modules/LoRaBasicsStationModule/start_basicsstation.sh
+++ LoRaEngine/modules/LoRaBasicsStationModule/start_basicsstation.sh
@@ -10,7 +10,7 @@ if [[ -z "$TC_URI" ]] && [[ -z "$CUPS_URI" ]]; then
 fi
 
 if [[ -z "$STATION_PATH" ]]; then
-    $STATION_PATH=/basicstation
+    STATION_PATH=/basicstation
 fi
 
 resetPin
@@ -18,27 +18,25 @@ setFixedStationEui
 conditionallySetupCups
 conditionallySetupTc
 
-if [[ -z "$SPI_DEV" ]] || [[ $SPI_DEV == '$LBS_SPI_DEV' ]]; then
-    echo "No custom SPI dev set up, defaulting to spi dev 0"
-    SPI_DEV=0
+if [[ -z "$RADIODEV" ]]; then
+    if [[ -z "$SPI_DEV" ]] || [[ $SPI_DEV == '$LBS_SPI_DEV' ]]; then
+        echo "No custom SPI dev set up, defaulting to spi dev 0"
+        SPI_DEV=0
+    fi
+    export RADIODEV=/dev/spidev$SPI_DEV.0
 fi
 
 #start basestation
 echo "Starting base station..."
-if [[ -z "$SPI_SPEED"  ]] || [[ "$SPI_SPEED" == '$LBS_SPI_SPEED' ]]; then
-    echo "No SPI Speed found defaulting to 8mbps"
-    RADIODEV=/dev/spidev$SPI_DEV.0 $STATION_PATH/station.std -f
+if [[ -z "$SPI_SPEED" ]] || [[ "$SPI_SPEED" == '$LBS_SPI_SPEED' ]] || [ "$SPI_SPEED" == "8" ]; then
+    echo "Spi speed set to 8 mbps"
+    $STATION_PATH/station.std -f
 else
     if [ "$SPI_SPEED" == "2" ]; then
         echo "Spi speed set to 2 mbps"
-        RADIODEV=/dev/spidev$SPI_DEV.0 $STATION_PATH/station.spispeed2 -f
+        $STATION_PATH/station.spispeed2 -f
     else
-        if [ "$SPI_SPEED" == "8" ]; then
-            echo "Spi speed set to 8 mbps"
-            RADIODEV=/dev/spidev$SPI_DEV.0 $STATION_PATH/station.std -f
-        else
-            echo "The value $SPI_SPEED is not supported as custom value. Supported values are 2 or 8"
-            exit 1;
-        fi
+        echo "The value $SPI_SPEED is not supported as custom value. Supported values are 2 or 8"
+        exit 1;
     fi
 fi
