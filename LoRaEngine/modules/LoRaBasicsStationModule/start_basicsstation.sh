0fa0415911aa6f969b6588c5223dafb49bd5a4ac
Daniele Antonio Maggio <damaggio@microsoft.com>
2021-12-09 11:42:51 +0100

    Making station path configurable

--- LoRaEngine/modules/LoRaBasicsStationModule/start_basicsstation.sh
+++ LoRaEngine/modules/LoRaBasicsStationModule/start_basicsstation.sh
@@ -9,6 +9,10 @@ if [[ -z "$TC_URI" ]] && [[ -z "$CUPS_URI" ]]; then
     exit 1
 fi
 
+if [[ -z "$STATION_PATH" ]]; then
+    $STATION_PATH=/basicstation
+fi
+
 resetPin
 setFixedStationEui
 conditionallySetupCups
@@ -23,15 +27,15 @@ fi
 echo "Starting base station..."
 if [[ -z "$SPI_SPEED"  ]] || [[ "$SPI_SPEED" == '$LBS_SPI_SPEED' ]]; then
     echo "No SPI Speed found defaulting to 8mbps"
-    RADIODEV=/dev/spidev$SPI_DEV.0 /basicstation/station.std -f
+    RADIODEV=/dev/spidev$SPI_DEV.0 $STATION_PATH/station.std -f
 else
     if [ "$SPI_SPEED" == "2" ]; then
         echo "Spi speed set to 2 mbps"
-        RADIODEV=/dev/spidev$SPI_DEV.0 /basicstation/station.spispeed2 -f
+        RADIODEV=/dev/spidev$SPI_DEV.0 $STATION_PATH/station.spispeed2 -f
     else
         if [ "$SPI_SPEED" == "8" ]; then
             echo "Spi speed set to 8 mbps"
-            RADIODEV=/dev/spidev$SPI_DEV.0 /basicstation/station.std -f
+            RADIODEV=/dev/spidev$SPI_DEV.0 $STATION_PATH/station.std -f
         else
             echo "The value $SPI_SPEED is not supported as custom value. Supported values are 2 or 8"
             exit 1;
