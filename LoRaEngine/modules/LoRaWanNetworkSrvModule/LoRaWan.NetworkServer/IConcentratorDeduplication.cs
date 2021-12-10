0bad5044cb58294bf9aa4b184407dfe970aabd2a
Patrick Schuler <paschule@microsoft.com>
2021-12-10 13:56:17 +0100

    add interface documentation

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/IConcentratorDeduplication.cs
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/IConcentratorDeduplication.cs
@@ -7,15 +7,45 @@ namespace LoRaWan.NetworkServer
 {
     public interface IConcentratorDeduplication
     {
+        /// <summary>
+        /// Validates if a particular Join request was handled by this LNS before.
+        /// </summary>
+        /// <param name="loRaRequest">The Join request</param>
+        /// <returns><see cref="ConcentratorDeduplicationResult.NotDuplicate"/> if this join request was not processed before
+        /// on this LNS otherwise <see cref="ConcentratorDeduplicationResult.Duplicate"/></returns>
         public ConcentratorDeduplicationResult CheckDuplicateJoin(LoRaRequest loRaRequest);
+
+        /// <summary>
+        /// Validates if a particular telemetry message has been processed before by this LNS and
+        /// if so, what continuation strategy we should use depending on the deduplication strategy of
+        /// the device.
+        /// </summary>
+        /// <param name="loRaRequest">The telemetry request</param>
+        /// <param name="loRaDevice">The device that sent the message</param>
+        /// <returns>Any of the <see cref="ConcentratorDeduplicationResult"/> values described.</returns>
         public ConcentratorDeduplicationResult CheckDuplicateData(LoRaRequest loRaRequest, LoRaDevice loRaDevice);
     }
 
     public enum ConcentratorDeduplicationResult
     {
+        /// <summary>
+        /// First message on this LNS
+        /// </summary>
         NotDuplicate,
+        /// <summary>
+        /// Duplicate message due to resubmit of a confirmed
+        /// message of the same station (concentrator).
+        /// </summary>
         DuplicateDueToResubmission,
-        SoftDuplicateDueToDeduplicationStrategy, // detected as a duplicate but due to the DeduplicationStrategy, marked only as a "soft" duplicate
+        /// <summary>
+        /// Detected as a duplicate but due to the DeduplicationStrategy,
+        /// marked only as a "soft" duplicate - allow upstream (Mark and None)
+        /// </summary>
+        SoftDuplicateDueToDeduplicationStrategy,
+        /// <summary>
+        /// Message is a duplicate and does not need to be
+        /// sent upstream.
+        /// </summary>
         Duplicate
     }
 }
