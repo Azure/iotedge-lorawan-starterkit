ed86080a6de1f7fcde17517c05924426721dc209
Atif Aziz <code@raboof.com>
2021-12-16 14:46:18 +0100

    Add tests for "JsonReader.Enum"

--- Tests/Unit/NetworkServer/JsonReaderTest.cs
+++ Tests/Unit/NetworkServer/JsonReaderTest.cs
@@ -460,5 +460,29 @@ namespace LoRaWan.Tests.Unit.NetworkServer
         {
             Assert.Throws<JsonException>(() => _ = EitherReader.Read(JsonUtil.Strictify(json)));
         }
+
+        [Theory]
+        [InlineData(Bandwidth.BW125, "125")]
+        [InlineData(Bandwidth.BW250, "250")]
+        [InlineData(Bandwidth.BW500, "500")]
+        public void Enum_With_Valid_Input(Bandwidth expected, string json)
+        {
+            var reader = JsonReader.Int32().Enum(n => (Bandwidth)n);
+            var result = reader.Read(JsonUtil.Strictify(json));
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("225")]
+        [InlineData("350")]
+        [InlineData("600")]
+        public void Enum_With_Invalid_Input(string json)
+        {
+            var reader = JsonReader.Int32().Enum(n => (Bandwidth)n);
+
+            var ex = Assert.Throws<JsonException>(() => reader.Read(JsonUtil.Strictify(json)));
+            Assert.Equal($"Invalid member for {typeof(Bandwidth)}: {json}", ex.Message);
+        }
     }
 }
