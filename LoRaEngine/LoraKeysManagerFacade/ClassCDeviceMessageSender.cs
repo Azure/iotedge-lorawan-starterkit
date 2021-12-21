1ff56b0c37f8a067fb07be3eb74ae85df845aee5
Maggie Salak <masalak@microsoft.com>
2021-12-21 17:18:18 +0100

    add functionality to get device twin from iot hub and send message using direct method

--- LoRaEngine/LoraKeysManagerFacade/ClassCDeviceMessageSender.cs
+++ LoRaEngine/LoraKeysManagerFacade/ClassCDeviceMessageSender.cs
@@ -4,24 +4,30 @@
 namespace LoraKeysManagerFacade
 {
     using System;
+    using System.Net;
+    using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Azure.Devices;
+    using Microsoft.Azure.Devices.Client.Exceptions;
     using Microsoft.Azure.WebJobs;
     using Microsoft.Azure.WebJobs.Extensions.Http;
     using Microsoft.Extensions.Logging;
     using Microsoft.Extensions.Primitives;
+    using Newtonsoft.Json;
 
     public class ClassCDeviceMessageSender
     {
         private readonly RegistryManager registryManager;
-        private readonly ILoRaDeviceCacheStore cacheStore;
+        private readonly IServiceClient serviceClient;
+        private readonly ILogger log;
 
-        public ClassCDeviceMessageSender(RegistryManager registryManager, ILoRaDeviceCacheStore cacheStore)
+        public ClassCDeviceMessageSender(RegistryManager registryManager, IServiceClient serviceClient, ILogger<ClassCDeviceMessageSender> log)
         {
             this.registryManager = registryManager;
-            this.cacheStore = cacheStore;
+            this.serviceClient = serviceClient;
+            this.log = log;
         }
 
         /// <summary>
@@ -30,7 +36,7 @@ namespace LoraKeysManagerFacade
         [FunctionName(nameof(SendToClassCDevice))]
         public async Task<IActionResult> SendToClassCDevice(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            CancellationToken cancellationToken)
         {
             if (req is null) throw new ArgumentNullException(nameof(req));
 
@@ -40,19 +46,19 @@ namespace LoraKeysManagerFacade
             }
             catch (IncompatibleVersionException ex)
             {
-                log.LogError(ex, "Invalid version");
+                this.log.LogError(ex, "Invalid version");
                 return new BadRequestObjectResult(ex.Message);
             }
 
-            return await RunSendToClassCDevice(req, log);
+            return await RunSendToClassCDevice(req, cancellationToken);
         }
 
-        private async Task<IActionResult> RunSendToClassCDevice(HttpRequest req, ILogger log)
+        private async Task<IActionResult> RunSendToClassCDevice(HttpRequest req, CancellationToken cancellationToken)
         {
             var devEUI = req.Query["DevEUI"];
             if (StringValues.IsNullOrEmpty(devEUI))
             {
-                log.LogError("DevEUI missing in request");
+                this.log.LogError("DevEUI missing in request");
                 return new BadRequestObjectResult("DevEUI missing in request");
             }
             try
@@ -67,16 +73,91 @@ namespace LoraKeysManagerFacade
             var fPort = req.Query["FPort"];
             if (StringValues.IsNullOrEmpty(fPort))
             {
-                log.LogError("FPort missing in request");
+                this.log.LogError("FPort missing in request");
                 return new BadRequestObjectResult("FPort missing in request");
             }
 
             var payload = req.Query["RawPayload"];
             if (StringValues.IsNullOrEmpty(payload))
             {
-                log.LogError("RawPayload missing in request");
+                this.log.LogError("RawPayload missing in request");
                 return new BadRequestObjectResult("RawPayload missing in request");
             }
+
+            var twin = await this.registryManager.GetTwinAsync(devEUI, cancellationToken);
+            if (twin != null)
+            {
+                if (string.Equals("c", twin.Properties?.Desired?.GetTwinPropertyStringSafe(LoraKeysManagerFacadeConstants.TwinProperty_ClassType), StringComparison.OrdinalIgnoreCase))
+                {
+                    var gatewayID = twin.Properties?.Reported?.GetTwinPropertyStringSafe(LoraKeysManagerFacadeConstants.TwinProperty_PreferredGatewayID);
+                    if (string.IsNullOrEmpty(gatewayID))
+                    {
+                        gatewayID = twin.Properties?.Desired?.GetTwinPropertyStringSafe(LoraKeysManagerFacadeConstants.TwinProperty_GatewayID);
+                    }
+
+                    if (!string.IsNullOrEmpty(gatewayID))
+                    {
+                        return await SendMessageViaDirectMethodAsync(gatewayID, devEUI, payload);
+                    }
+
+                    // class c device that did not send a single upstream message
+                    return new BadRequestObjectResult($"Gateway ID for device {devEUI} is unknown; cannot send a downstream message.");
+                }
+
+                // Not a class C device
+                return new BadRequestObjectResult($"Provided device {devEUI} is not a class C device");
+            }
+            else
+            {
+                this.log.LogInformation($"Searching for {devEUI} returned 0 devices");
+                return new NotFoundResult();
+            }
         }
+
+        private async Task<IActionResult> SendMessageViaDirectMethodAsync(
+            string preferredGatewayID,
+            string devEUI,
+            string payload)
+        {
+            try
+            {
+                var method = new CloudToDeviceMethod(LoraKeysManagerFacadeConstants.CloudToDeviceMessageMethodName, TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(1));
+                _ = method.SetPayloadJson(JsonConvert.SerializeObject(payload));
+
+                var res = await this.serviceClient.InvokeDeviceMethodAsync(preferredGatewayID, LoraKeysManagerFacadeConstants.NetworkServerModuleId, method);
+                if (IsSuccessStatusCode(res.Status))
+                {
+                    this.log.LogInformation("Direct method call to {gatewayID} and {devEUI} succeeded with {statusCode}", preferredGatewayID, devEUI, res.Status);
+
+                    return new OkObjectResult(res);
+                }
+
+                this.log.LogError("Direct method call to {gatewayID} failed with {statusCode}. Response: {response}", preferredGatewayID, res.Status, res.GetPayloadAsJson());
+
+                return new ObjectResult(res.GetPayloadAsJson())
+                {
+                    StatusCode = res.Status,
+                };
+            }
+            catch (JsonSerializationException ex)
+            {
+
+                this.log.LogError(ex, "Failed to serialize the message payload");
+                return new ObjectResult("Failed to serialize payload")
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError
+                };
+            }
+            catch (IotHubException ex)
+            {
+                this.log.LogError(ex, "Failed to get device twin for devEUI {devEUI} from the IoT Hub", devEUI);
+                return new ObjectResult("Failed to get device twin from the IoT Hub")
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError
+                };
+            }
+        }
+
+        private static bool IsSuccessStatusCode(int statusCode) => statusCode is >= 200 and <= 299;
     }
 }
