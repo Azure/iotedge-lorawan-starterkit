1a1c08f7cbb0ec74f1bdc95151b83ff1077d359f
Bastian Burger <22341213+bastbu@users.noreply.github.com>
2022-01-05 16:57:59 +0100

    Dwell time support for AS923 (#1119)
    Introducing Dwell Time support on region AS923. Concentrators within that region will include a `desiredTxParam` configuration. Based on this configuration, the station negotiates effective Dwell Time settings with the LoRa device using TxParamSetupReq/Ans MAC commands.

---
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoraTools/Regions/DwellTimeSetting.cs
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#nullable enable
+
+namespace LoRaTools.Regions
+{
+    public sealed record DwellTimeSetting(bool DownlinkDwellTime, bool UplinkDwellTime, uint MaxEirp);
+}
---
+++ Tests/Unit/LoRaTools/DwellTimeLimitedRegionTests.cs
@@ -0,0 +1,56 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#nullable enable
+
+namespace LoRaWan.Tests.Unit.LoRaTools
+{
+    using System;
+    using System.Collections.Generic;
+    using global::LoRaTools.LoRaPhysical;
+    using global::LoRaTools.Regions;
+    using global::LoRaTools.Utils;
+    using LoRaWan;
+    using Xunit;
+
+    public sealed class DwellTimeLimitedRegionTests
+    {
+        [Fact]
+        public void When_Accessing_Uninitialized_DwellTimeSetting_Throws()
+        {
+            Assert.Throws<InvalidOperationException>(() => new DwellTimeLimitedTestRegion().DesiredDwellTimeSetting);
+        }
+
+        [Fact]
+        public void Setting_And_Getting_DesiredDwellTimeSetting_Success()
+        {
+            var dwellTimeSetting = new DwellTimeSetting(true, false, 4);
+            var subject = new DwellTimeLimitedTestRegion { DesiredDwellTimeSetting = dwellTimeSetting };
+            Assert.Equal(dwellTimeSetting, subject.DesiredDwellTimeSetting);
+        }
+
+        private class DwellTimeLimitedTestRegion : DwellTimeLimitedRegion
+        {
+            public DwellTimeLimitedTestRegion() : base(LoRaRegionType.AS923)
+            { }
+
+            public override IReadOnlyDictionary<DataRateIndex, (DataRate DataRate, uint MaxPayloadSize)> DRtoConfiguration =>
+                throw new NotImplementedException();
+
+            public override IReadOnlyDictionary<uint, double> TXPowertoMaxEIRP => throw new NotImplementedException();
+
+            public override IReadOnlyList<IReadOnlyList<DataRateIndex>> RX1DROffsetTable => throw new NotImplementedException();
+
+            protected override DwellTimeSetting DefaultDwellTimeSetting => new DwellTimeSetting(true, false, 4);
+
+            public override RX2ReceiveWindow GetDefaultRX2ReceiveWindow(DeviceJoinInfo? deviceJoinInfo = null) =>
+                throw new NotImplementedException();
+
+            public override bool TryGetDownstreamChannelFrequency(Hertz upstreamFrequency, out Hertz downstreamFrequency, DataRateIndex? upstreamDataRate = null, DeviceJoinInfo? deviceJoinInfo = null) =>
+                throw new NotImplementedException();
+
+            public override void UseDwellTimeSetting(DwellTimeSetting dwellTimeSetting) =>
+                throw new NotImplementedException();
+        }
+    }
+}
