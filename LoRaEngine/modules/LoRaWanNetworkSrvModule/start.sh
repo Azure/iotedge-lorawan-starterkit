5aa5b43040848ada23e046aafbceafe52f9ed531
Daniele Antonio Maggio <damaggio@microsoft.com>
2022-01-12 09:47:03 +0100

    Allowing CLIENT_CA_PATH to be a ';' separated list of files

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/start.sh
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/start.sh
@@ -3,32 +3,36 @@ set -e
 
 DEFAULT_CERTS_PATH="/var/lorastarterkit/certs"
 DEFAULT_CLIENT_CA_PATH="$DEFAULT_CERTS_PATH/client.ca.crt"
-CA_PATH="/usr/local/share/ca-certificates/lnsclientca.crt"
+CA_PATH="/usr/local/share/ca-certificates/"
 
 conditionalCopy() {
-    if [[ -z "$1" ]]; then
-        echo "No proper path detected in environment variables. Trying to check for default location."
-        if [ -z "$(ls -A $2 2> /dev/null)" ]; then
-            echo "No file found at $2. Nothing was copied over."
-        else
-            echo "A file was found at $2. Copying it over."
-            cp -v $2 $CA_PATH
-        fi
+    if [ -z "$(ls -A $1 2> /dev/null)" ]; then
+        echo "No file found at $1. Nothing was copied over."
     else
         cp -v $1 $CA_PATH
+        certificateCopied=0
     fi
 }
 
 if [[ -z "$LNS_SERVER_PFX_PATH" ]]; then
     echo "No PFX is set for the server side authentication. No need to trust any certificate."
 else
-    conditionalCopy "$CLIENT_CA_PATH" "$DEFAULT_CLIENT_CA_PATH"
+    if [[ -z "$CLIENT_CA_PATH" ]]; then
+        conditionalCopy $DEFAULT_CLIENT_CA_PATH
+    else
+        #Split the CLIENT_CA_PATH based on the delimiter ';'
+        readarray -d ';' -t strarr <<< "$CLIENT_CA_PATH"
+        for (( n=0; n < ${#strarr[*]}; n++))
+        do
+            conditionalCopy "${strarr[n]}"
+        done
+    fi
 fi
 
-if [ -z "$(ls -A $CA_PATH 2> /dev/null)" ]; then
-    echo "No file found at $CA_PATH therefore no update-ca-certificates needed."
-else
+if [[ ! -z $certificateCopied ]] && [[ $certificateCopied -eq 0 ]]; then
     update-ca-certificates --fresh
+else
+    echo "No certifiate copied, therefore no update-ca-certificates needed."
 fi
 
 dotnet LoRaWanNetworkSrvModule.dll
