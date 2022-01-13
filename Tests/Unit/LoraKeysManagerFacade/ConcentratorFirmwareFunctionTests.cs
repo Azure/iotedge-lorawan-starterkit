350a6491c2a3ac14c9c0f62a4b8e60a7dd11a6d0
Maggie Salak <masalak@microsoft.com>
2022-01-13 13:55:04 +0100

    add unit test for success case

--- Tests/Unit/LoraKeysManagerFacade/ConcentratorFirmwareFunctionTests.cs
+++ Tests/Unit/LoraKeysManagerFacade/ConcentratorFirmwareFunctionTests.cs
@@ -3,24 +3,88 @@
 
 namespace LoRaWan.Tests.Unit.LoraKeysManagerFacade
 {
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Azure;
     using Azure.Storage.Blobs;
+    using Azure.Storage.Blobs.Models;
     using global::LoraKeysManagerFacade;
+    using LoRaWan.Tests.Common;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
     using Microsoft.Azure.Devices;
+    using Microsoft.Azure.Devices.Shared;
     using Microsoft.Extensions.Azure;
     using Microsoft.Extensions.Logging.Abstractions;
+    using Microsoft.Extensions.Primitives;
     using Moq;
+    using Xunit;
 
     public class ConcentratorFirmwareFunctionTests
     {
+        private const string BlobContent = "testcontent";
+
         private readonly Mock<RegistryManager> registryManager;
         private readonly Mock<IAzureClientFactory<BlobServiceClient>> azureClientFactory;
+        private readonly Mock<BlobClient> blobClient;
         private readonly ConcentratorFirmwareFunction concentratorFirmware;
+
+        private readonly StationEui TestStationEui = StationEui.Parse("11-11-11-11-11-11-11-11");
 
         public ConcentratorFirmwareFunctionTests()
         {
             this.registryManager = new Mock<RegistryManager>();
             this.azureClientFactory = new Mock<IAzureClientFactory<BlobServiceClient>>();
+            this.blobClient = new Mock<BlobClient>();
             this.concentratorFirmware = new ConcentratorFirmwareFunction(registryManager.Object, azureClientFactory.Object, NullLogger<ConcentratorFirmwareFunction>.Instance);
+
+            var blobServiceClient = new Mock<BlobServiceClient>();
+            var blobContainerClient = new Mock<BlobContainerClient>();
+            var blobContainerClientResponseMock = new Mock<Response>();
+            var blobClientResponseMock = new Mock<Response>();
+
+            blobContainerClient.Setup(m => m.GetBlobClient(It.IsAny<string>()))
+                               .Returns(Response.FromValue(this.blobClient.Object, blobClientResponseMock.Object));
+
+            blobServiceClient.Setup(m => m.GetBlobContainerClient(It.IsAny<string>()))
+                             .Returns(Response.FromValue(blobContainerClient.Object, blobContainerClientResponseMock.Object));
+
+            this.azureClientFactory.Setup(m => m.CreateClient(FacadeStartup.WebJobsStorageClientName))
+                                   .Returns(blobServiceClient.Object);
+
+            var blobBytes = Encoding.UTF8.GetBytes(BlobContent);
+            using var blobStream = new MemoryStream(blobBytes);
+            this.blobClient.Setup(m => m.OpenReadAsync(It.IsAny<BlobOpenReadOptions>(), It.IsAny<CancellationToken>()))
+                           .Returns(Task.FromResult(blobStream as Stream));
+        }
+
+        [Fact]
+        public async Task RunFetchConcentratorFirmware_Succeeds()
+        {
+            var httpRequest = new Mock<HttpRequest>();
+            var queryCollection = new QueryCollection(new Dictionary<string, StringValues>()
+            {
+                { "StationEui", new StringValues(TestStationEui.ToString()) }
+            });
+            httpRequest.SetupGet(x => x.Query).Returns(queryCollection);
+
+            var twin = new Twin();
+            twin.Properties.Desired = new TwinCollection(JsonUtil.Strictify(@"{'cups': {
+                'package': '1.0.1',
+                'fwUrl': 'https://storage.blob.core.windows.net/container/blob',
+                'fwKeyChecksum': 123456,
+                'fwSignature': '123'
+            }}"));
+            this.registryManager.Setup(m => m.GetTwinAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                                .Returns(Task.FromResult(twin));
+
+            var result = await this.concentratorFirmware.RunFetchConcentratorFwUpgrade(httpRequest.Object, CancellationToken.None);
+
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result);
         }
     }
 }
