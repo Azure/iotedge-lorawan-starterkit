de60a092a5a22f4aec3a8e689ae8cd1a55936d28
Bastian Burger <baburger@microsoft.com>
2022-01-17 07:52:00 +0100

    Add null parameter validation

--- Tests/Common/TestOutputLogger.cs
+++ Tests/Common/TestOutputLogger.cs
@@ -30,7 +30,9 @@ namespace LoRaWan.Tests.Common
 
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
         {
+            if (formatter is null) throw new ArgumentNullException(nameof(formatter));
             if (!IsEnabled(logLevel)) return;
+
             var message = formatter(state, exception);
             this.testOutputHelper.WriteLine(message);
         }
