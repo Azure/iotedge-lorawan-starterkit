9239c31eddc44a40e893928a8d0f6e8316aecab9
Atif Aziz <code@raboof.com>
2022-01-24 16:49:49 +0100

    Re-enable "CloudToDeviceMessageSizeLimitShouldAcceptTests"

--- Tests/Integration/CloudToDeviceMessageSizeLimitShouldAcceptTests.cs
+++ Tests/Integration/CloudToDeviceMessageSizeLimitShouldAcceptTests.cs
@@ -22,7 +22,7 @@ namespace LoRaWan.Tests.Integration
     {
         public CloudToDeviceMessageSizeLimitShouldAcceptTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper) { }
 
-        [Theory(Skip = "Fails on CI - works locally. To enable with #562")]
+        [Theory(/*Skip = "Fails on CI - works locally. To enable with #562"*/)]
         [CombinatorialData]
         public async Task Should_Accept(
             bool isConfirmed,
