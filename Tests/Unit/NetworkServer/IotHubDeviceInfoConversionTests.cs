d4f1b9e3e9269eff6afe5ff97665b5a69290e5d3
Bastian Burger <baburger@microsoft.com>
2022-01-25 11:26:42 +0100

    Ensure that IotHubDeviceInfo is serialized correctly

---
+++ Tests/Unit/NetworkServer/IotHubDeviceInfoConversionTests.cs
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#nullable enable
+
+namespace LoRaWan.Tests.Unit.NetworkServer
+{
+    using global::LoraKeysManagerFacade;
+    using Newtonsoft.Json;
+    using Xunit;
+
+    public sealed class IotHubDeviceInfoConversionTests
+    {
+        [Fact]
+        public void Serialization_And_Deserialization_Preserves_Information_Using_JsonNet()
+        {
+            // arrange
+            var original = new IoTHubDeviceInfo { DevAddr = new DevAddr(123), DevEUI = new DevEui(234), PrimaryKey = "someprimarykey" };
+
+            // act
+            var result = JsonConvert.DeserializeObject<LoRaWan.NetworkServer.IoTHubDeviceInfo>(JsonConvert.SerializeObject(original));
+
+            // assert
+            Assert.NotNull(result);
+            Assert.Equal(original.DevEUI, result!.DevEUI);
+            Assert.Equal(original.PrimaryKey, result.PrimaryKey);
+            Assert.Equal(original.DevAddr, result.DevAddr);
+        }
+    }
+}
