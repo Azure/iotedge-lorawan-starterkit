cb4b47420c9d455cf5995887aadfd81abb7fdaef
Maggie Salak <masalak@microsoft.com>
2022-01-26 11:03:32 +0100

    simplify if else

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/LoRaDevice.cs
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/LoRaDevice.cs
@@ -740,14 +740,10 @@ namespace LoRaWan.NetworkServer
                     reportedProperties[TwinProperty.RX1DROffset] = null;
                 }
 
-                if (DesiredRX2DataRate != DefaultJoinValues && DesiredRX2DataRate.HasValue && currentRegion.RegionLimits.IsCurrentDownstreamDRIndexWithinAcceptableValue(DesiredRX2DataRate.Value))
-                {
-                    reportedProperties[TwinProperty.RX2DataRate] = DesiredRX2DataRate;
-                }
-                else
-                {
-                    reportedProperties[TwinProperty.RX2DataRate] = null;
-                }
+                reportedProperties[TwinProperty.RX2DataRate] =
+                    DesiredRX2DataRate != DefaultJoinValues && DesiredRX2DataRate.HasValue && currentRegion.RegionLimits.IsCurrentDownstreamDRIndexWithinAcceptableValue(DesiredRX2DataRate.Value)
+                    ? DesiredRX2DataRate
+                    : null;
 
                 reportedProperties[TwinProperty.RXDelay] = DesiredRXDelay is var rxd and not DefaultJoinRxDelay
                                                            && Enum.IsDefined(rxd) ? rxd : null;
