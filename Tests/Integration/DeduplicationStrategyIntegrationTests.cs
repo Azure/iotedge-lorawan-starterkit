Azure/iotedge-lorawan-starterkit

commit 7ba298ca4439ae098cb20e36256274240f6f75ee

Bug/900 respect deduplication strategy (#937)
* WIP

* Adjust logic to create cache key based.
Fixes some of the unit tests.

* Enables for data payload.

* Fixes unit test for join request.

* Integrates with code for data messages.
Adds integration test.

* More consistent unit test.

* Clean up integration test.

* Removes entry points from LnsProcessor

* Integrates the deduplication on the join requests.

* Adds unit tests for ShouldProcess method.

* Fixes build.

* Fixes semantics of metric for join requests.

* Polishing test code.

* Avoids making helper type/method public.

* Removes not required usings.

* avoids CS8352 on CI.

* Moves dedup check earlier for join requests.
Allows null lora device for that case.

* f

* Introduces enum for various deduplication results.
Drop case is covered.
Removes helper method in Socket registry that is not needed anymore.

* Use function only when the result is Allow.

* Handles resubmissions.
Fixes integration tests.

* AllowButSkipConfirmation case.

* Resubmissions should get confirmation.

* MemoryCache is shared and should not be disposed.
It is bound to ApplicationDomainUnloaded event.

Enables disabled unit tests.

* Reverts ValidateRequest to previous behavior.

* Contact the Function also in the case of Resubmission.
This is required in order to get the frame counter down.

* Mark logic uses also the concentrator dedup result.

* Fixes mock parameters.

* E2E tests depend on a specific log message.

* Improved names, logs and docs.

* Fixes debug information for  E2E test logs.

* Revert "Fixes debug information for  E2E test logs."

This reverts commit a40b5953853a4c4d51a18a7c561663bbef4a1dcb.

* Adds missing case.

* Adds unit tests for internal method.

* Move static helper method to LoRaRequest.
Unit test it.

* Removes not needed dependency on socket registry.

* Integrates with additional deduplication modes.

Tests/Integration/ProcessingTests.cs
@@ -566,7 +566,7 @@ namespace LoRaWan.Tests.Integration
         [InlineData(ServerGatewayID, 21)]
         [InlineData(null, 21)]
         [InlineData(null, 30)]
-        public async Task When_ConfirmedUp_Message_With_Same_Fcnt_Should_Return_Ack_And_Not_Send_To_Hub(string deviceGatewayID, uint expectedFcntDown)
+        public async Task When_ConfirmedUp_Message_With_Same_Fcnt_Should_Send_To_Hub_And_Return_Ack(string deviceGatewayID, uint expectedFcntDown)
         {
             const uint initialFcntUp = 100;
             const uint initialFcntDown = 20;
@@ -585,10 +585,13 @@ namespace LoRaWan.Tests.Integration
             LoRaDeviceClient.Setup(x => x.ReceiveAsync(It.IsNotNull<TimeSpan>()))
                 .ReturnsAsync((Message)null);
 
+            LoRaDeviceClient.Setup(x => x.SendEventAsync(It.IsNotNull<LoRaDeviceTelemetry>(), null))
+                .ReturnsAsync(true);
+
             // In this case we expect a call to update the reported properties of the framecounter.
             if (expectedFcntDown % 10 == 0)
             {
-                this.LoRaDeviceClient.Setup(x => x.UpdateReportedPropertiesAsync(It.IsAny<TwinCollection>())).ReturnsAsync(true);
+                LoRaDeviceClient.Setup(x => x.UpdateReportedPropertiesAsync(It.IsAny<TwinCollection>())).ReturnsAsync(true);
             }
             // Lora device api
 
@@ -659,8 +662,8 @@ namespace LoRaWan.Tests.Integration
                 Assert.False(loRaDevice.HasFrameCountChanges);
             }
 
-            // message should not be sent to iot hub
-            LoRaDeviceClient.Verify(x => x.SendEventAsync(It.IsAny<LoRaDeviceTelemetry>(), It.IsAny<Dictionary<string, string>>()), Times.Never);
+            // message should be sent to iot hub
+            LoRaDeviceClient.Verify(x => x.SendEventAsync(It.IsAny<LoRaDeviceTelemetry>(), It.IsAny<Dictionary<string, string>>()), Times.Once);
             LoRaDeviceClient.VerifyAll();
             LoRaDeviceApi.VerifyAll();
         }

LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/BasicsStation/BasicsStationNetworkServerStartup.cs
@@ -99,7 +99,7 @@ namespace LoRaWan.NetworkServer.BasicsStation
                         .AddSingleton(new LoRaDeviceCacheOptions { MaxUnobservedLifetime = TimeSpan.FromDays(10), RefreshInterval = TimeSpan.FromDays(2), ValidationInterval = TimeSpan.FromMinutes(10) })
                         .AddTransient<ILnsProtocolMessageProcessor, LnsProtocolMessageProcessor>()
                         .AddTransient<ICupsProtocolMessageProcessor, CupsProtocolMessageProcessor>()
-                        .AddSingleton(typeof(IConcentratorDeduplication<>), typeof(ConcentratorDeduplication<>))
+                        .AddSingleton<IConcentratorDeduplication, ConcentratorDeduplication>()
                         .AddSingleton(new RegistryMetricTagBag(NetworkServerConfiguration))
                         .AddSingleton(_ => new Meter(MetricRegistry.Namespace, MetricRegistry.Version))
                         .AddHostedService(sp =>

LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/BasicsStation/Processors/LnsProtocolMessageProcessor.cs
@@ -30,8 +30,6 @@ namespace LoRaWan.NetworkServer.BasicsStation.Processors
         private readonly WebSocketWriterRegistry<StationEui, string> socketWriterRegistry;
         private readonly IPacketForwarder downstreamSender;
         private readonly IMessageDispatcher messageDispatcher;
-        private readonly IConcentratorDeduplication<UpstreamDataFrame> upstreamDeduplication;
-        private readonly IConcentratorDeduplication<JoinRequestFrame> joinRequestDeduplication;
         private readonly ILogger<LnsProtocolMessageProcessor> logger;
         private readonly RegistryMetricTagBag registryMetricTagBag;
         private readonly Counter<int> joinRequestCounter;
@@ -42,8 +40,6 @@ namespace LoRaWan.NetworkServer.BasicsStation.Processors
                                            WebSocketWriterRegistry<StationEui, string> socketWriterRegistry,
                                            IPacketForwarder packetForwarder,
                                            IMessageDispatcher messageDispatcher,
-                                           IConcentratorDeduplication<UpstreamDataFrame> upstreamDeduplication,
-                                           IConcentratorDeduplication<JoinRequestFrame> joinRequestDeduplication,
                                            ILogger<LnsProtocolMessageProcessor> logger,
                                            RegistryMetricTagBag registryMetricTagBag,
                                            Meter meter)
@@ -52,8 +48,6 @@ namespace LoRaWan.NetworkServer.BasicsStation.Processors
             this.socketWriterRegistry = socketWriterRegistry;
             this.downstreamSender = packetForwarder;
             this.messageDispatcher = messageDispatcher;
-            this.upstreamDeduplication = upstreamDeduplication;
-            this.joinRequestDeduplication = joinRequestDeduplication;
             this.logger = logger;
             this.registryMetricTagBag = registryMetricTagBag;
             this.joinRequestCounter = meter?.CreateCounter<int>(MetricRegistry.JoinRequests);
@@ -207,13 +201,6 @@ namespace LoRaWan.NetworkServer.BasicsStation.Processors
                     {
                         var jreq = LnsData.JoinRequestFrameReader.Read(json);
 
-                        if (this.joinRequestDeduplication.ShouldDrop(jreq, stationEui))
-                        {
-                            break;
-                        }
-
-                        this.joinRequestCounter?.Add(1);
-
                         var routerRegion = await this.basicsStationConfigurationService.GetRegionAsync(stationEui, cancellationToken);
                         var rxpk = new BasicStationToRxpk(jreq.RadioMetadata, routerRegion);
 
@@ -238,11 +225,6 @@ namespace LoRaWan.NetworkServer.BasicsStation.Processors
                     try
                     {
                         var updf = LnsData.UpstreamDataFrameReader.Read(json);
-
-                        if (this.upstreamDeduplication.ShouldDrop(updf, stationEui))
-                        {
-                            break;
-                        }
 
                         using var scope = this.logger.BeginDeviceAddressScope(updf.DevAddr);
                         this.uplinkMessageCounter?.Add(1);

commit 9141b56ffc579b77ec0163b9764e45bc7f29d78a

Bug/1032 ensure upstream messages (#1388)
* Ensures that upstream messages are sent in the case of None or Mark strategies.

* Extends integration tests to test when second message does not get a valid frame counter.
In order to test different code paths, we are using confirmed data messages in this case.

* Unrelated.

* Extends test to test downstream messages.

* Re-uses code.

* Adds docs about proper handling.

* Will (partially) revert: ensures E2E test fails before the fix

* - Fixes bug in logging introduced in #937

* Ensures test uses confirmed messages in order to test the right code path.

* Fixes test assertion.

* Confusing log message when log not found.

* Ensures that test fails instead of only warn when log not found in IoTHub

* Revert "Will (partially) revert: ensures E2E test fails before the fix"

This reverts commit eb03edf1d450ff162774eb7ac104f21340270447.

* Automatic merge failed, aligns with redesign of ValidateRequest to not use out parameter.

LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/DefaultLoRaDataRequestHandler.cs
@@ -189,12 +189,10 @@ namespace LoRaWan.NetworkServer
                 {
                     fcntDown = await EnsureHasFcntDownAsync(loRaDevice, fcntDown, payloadFcntAdjusted, frameCounterStrategy);
 
-                    // Failed to update the fcnt down
-                    // In multi gateway scenarios it means another gateway has won the race to handle this message, can stop now
-                    if (fcntDown <= 0)
-                    {
-                        return new LoRaDeviceRequestProcessResult(loRaDevice, request, LoRaDeviceRequestFailedReason.HandledByAnotherGateway);
-                    }
+                    var result = HandleFrameCounterDownResult(fcntDown, loRaDevice, ref skipDownstreamToAvoidCollisions);
+
+                    if (result != null)
+                        return new LoRaDeviceRequestProcessResult(loRaDevice, request, result.Value);
                 }
                 #endregion
 
@@ -245,10 +243,10 @@ namespace LoRaWan.NetworkServer
                     {
                         fcntDown = await EnsureHasFcntDownAsync(loRaDevice, fcntDown, payloadFcntAdjusted, frameCounterStrategy);
 
-                        if (!fcntDown.HasValue || fcntDown <= 0)
-                        {
-                            return new LoRaDeviceRequestProcessResult(loRaDevice, request, LoRaDeviceRequestFailedReason.HandledByAnotherGateway);
-                        }
+                        var result = HandleFrameCounterDownResult(fcntDown, loRaDevice, ref skipDownstreamToAvoidCollisions);
+
+                        if (result != null)
+                            return new LoRaDeviceRequestProcessResult(loRaDevice, request, result.Value);
 
                         requiresConfirmation = true;
                     }
@@ -759,6 +757,9 @@ namespace LoRaWan.NetworkServer
 
         private void LogNotNullFrameCounterDownState(LoRaDevice loRaDevice, uint? newFcntDown)
         {
+            if (!newFcntDown.HasValue)
+                return;
+
             if (newFcntDown <= 0)
             {
                 this.logger.LogDebug("another gateway has already sent ack or downlink msg");
@@ -767,6 +768,32 @@ namespace LoRaWan.NetworkServer
             {
                 this.logger.LogDebug($"down frame counter: {loRaDevice.FCntDown}");
             }
+        }
+
+        /// <summary>
+        /// Handles the result of frame counter down, depending on the <code>DeduplicationMode</code> used.
+        /// Specifically, for invalid frame counter down:
+        /// - when mode is Drop, we do not send the message upstream nor downstream
+        /// - when mode is Mark or None, we allow upstream but skip downstream to avoid collisions
+        /// </summary>
+        /// <param name="skipDownstreamToAvoidCollisions">boolean that is used while deciding to send messages downstream</param>
+        /// <returns><code>LoRaDeviceRequestFailedReason</code> when Drop, otherwise null</returns>
+        private static LoRaDeviceRequestFailedReason? HandleFrameCounterDownResult(uint? fcntDown, LoRaDevice loRaDevice, ref bool skipDownstreamToAvoidCollisions)
+        {
+            LoRaDeviceRequestFailedReason? result = null;
+
+            if (fcntDown <= 0)
+            {
+                // Failed to update the fcnt down:
+                // This can only happen in multi gateway scenarios and
+                // it means that another gateway has won the race to handle this message.
+                if (loRaDevice.Deduplication == DeduplicationMode.Drop)
+                    result = LoRaDeviceRequestFailedReason.HandledByAnotherGateway;
+                else
+                    skipDownstreamToAvoidCollisions = true;
+            }
+
+            return result;
         }
 
         protected virtual async Task<FunctionBundlerResult> TryUseBundler(LoRaRequest request, LoRaDevice loRaDevice, LoRaPayloadData loraPayload, bool useMultipleGateways)
@@ -840,8 +867,8 @@ namespace LoRaWan.NetworkServer
         /// <param name="payloadFcnt"></param>
         /// <param name="loRaDevice"></param>
         /// <param name="isConfirmedResubmit"><code>True</code> when it's a confirmation resubmit.</param>
-        /// <param name="result">When request is not valid, indicates the reason.</param>
-        /// <returns><code>True</code> when the provided request is valid, false otherwise.</returns>
+        /// <returns><code>LoRaDeviceRequestFailedReason</code> when the provided request is
+        /// invalid, otherwise null.</returns>
         internal virtual LoRaDeviceRequestFailedReason? ValidateRequest(LoRaPayloadData payload, bool isFrameCounterFromNewlyStartedDevice, uint payloadFcnt, LoRaDevice loRaDevice, ConcentratorDeduplicationResult concentratorDeduplicationResult, out bool isConfirmedResubmit)
         {
             isConfirmedResubmit = false;

Tests/Common/IntegrationTestFixtureBase.Asserts.cs
@@ -72,7 +72,7 @@ namespace LoRaWan.Tests.Common
                 if (!searchResult.Found)
                 {
                     var logs = string.Join("\n\t", searchResult.Logs.TakeLast(5));
-                    TestLogger.Log($"[WARN] \"{targetJsonProperty}\": {expectedJsonValue} for device {deviceID} found in logs? {searchResult.Found}. Logs: [{logs}]");
+                    TestLogger.Log($"[WARN] \"{targetJsonProperty}\": {expectedJsonValue} for device {deviceID} was not found. Logs: [{logs}]");
                 }
             }
         }
@@ -243,7 +243,7 @@ namespace LoRaWan.Tests.Common
                     if (!searchResult.Found)
                     {
                         var logs = string.Join("\n\t", searchResult.Logs.TakeLast(5));
-                        TestLogger.Log($"[WARN] '{options.Description}' found in logs? {searchResult.Found}. Logs: [{logs}]");
+                        TestLogger.Log($"[WARN] '{options.Description}' was not found. Logs: [{logs}]");
                     }
                 }
             }

Tests/E2E/MultiGatewayTests.cs
@@ -98,12 +98,12 @@ namespace LoRaWan.Tests.E2E
             for (var i = 0; i < 10; i++)
             {
                 var msg = PayloadGenerator.Next().ToString(CultureInfo.InvariantCulture);
-                await ArduinoDevice.transferPacketAsync(msg, 10);
+                await ArduinoDevice.transferPacketWithConfirmedAsync(msg, 10);
                 await Task.Delay(Constants.DELAY_FOR_SERIAL_AFTER_SENDING_PACKET);
 
                 // After transferPacket: Expectation from serial
-                // +MSG: Done
-                await AssertUtils.ContainsWithRetriesAsync("+MSG: Done", ArduinoDevice.SerialLogs);
+                // +CMSG: ACK Received
+                await AssertUtils.ContainsWithRetriesAsync("+CMSG: ACK Received", ArduinoDevice.SerialLogs);
 
                 var allGwGotIt = await TestFixtureCi.ValidateMultiGatewaySources((log) => log.IndexOf($"deduplication Strategy: {strategy}", StringComparison.OrdinalIgnoreCase) != -1);
                 if (allGwGotIt)
@@ -123,7 +123,8 @@ namespace LoRaWan.Tests.E2E
                     {
                         case "Mark":
                             var expectedProperty = "dupmsg";
-                            await TestFixture.AssertIoTHubDeviceMessageExistsAsync(device.DeviceID, expectedProperty, "true", new SearchLogOptions(expectedProperty));
+                            await TestFixture.AssertIoTHubDeviceMessageExistsAsync(device.DeviceID, expectedProperty, "true", new SearchLogOptions(expectedProperty) { SourceIdFilter = duplicateResult.MatchedEvent.SourceId, TreatAsError = true });
+                            await TestFixture.AssertIoTHubDeviceMessageExistsAsync(device.DeviceID, expectedProperty, "true", new SearchLogOptions(expectedProperty) { SourceIdFilter = notDuplicateResult.MatchedEvent.SourceId, TreatAsError = true });
                             break;
                         case "Drop":
                             var logMsg = $"{device.DeviceID}: duplication strategy indicated to not process message";
@@ -131,7 +132,7 @@ namespace LoRaWan.Tests.E2E
                             Assert.NotNull(droppedLog.MatchedEvent);
 
                             var expectedPayload = $"{{\"value\":{msg}}}";
-                            await TestFixtureCi.AssertIoTHubDeviceMessageExistsAsync(device.DeviceID, expectedPayload, new SearchLogOptions(expectedPayload));
+                            await TestFixtureCi.AssertIoTHubDeviceMessageExistsAsync(device.DeviceID, expectedPayload, new SearchLogOptions(expectedPayload) { TreatAsError = true });
                             break;
                         default:
                             throw new SwitchExpressionException();

Tests/Integration/DeduplicationStrategyIntegrationTests.cs
@@ -19,36 +19,64 @@ namespace LoRaWan.Tests.Integration
     {
         private readonly ITestOutputHelper testOutputHelper;
         private readonly object functionLock = new object();
+        private readonly SimulatedDevice simulatedDevice;
 
         public DeduplicationStrategyIntegrationTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
         {
             this.testOutputHelper = testOutputHelper;
+            this.simulatedDevice = new SimulatedDevice(TestDeviceInfo.CreateABPDevice(1));
         }
 
         [Theory]
-        [InlineData(DeduplicationMode.Mark)]
-        [InlineData(DeduplicationMode.Drop)]
-        [InlineData(DeduplicationMode.None)]
-        public async Task Validate_Dup_Message_Processing(DeduplicationMode mode)
+        [InlineData(DeduplicationMode.Mark, false)]
+        [InlineData(DeduplicationMode.Drop, false)]
+        [InlineData(DeduplicationMode.None, false)]
+        [InlineData(DeduplicationMode.Mark, true)]
+        [InlineData(DeduplicationMode.Drop, true)]
+        [InlineData(DeduplicationMode.None, true)]
+        public async Task When_Different_Strategies_Are_Used_Ensures_Correct_Upstream_And_Downstream_Processing(DeduplicationMode mode, bool confirmedMessages)
         {
-            var simulatedDevice = new SimulatedDevice(TestDeviceInfo.CreateABPDevice(1));
             var messageProcessed = false;
+            var counterFctnDown = 0;
 
             foreach (var api in new[] { LoRaDeviceApi, SecondLoRaDeviceApi })
             {
-                api.Setup(x => x.ExecuteFunctionBundlerAsync(simulatedDevice.DevEUI, It.IsNotNull<FunctionBundlerRequest>()))
+                api.Setup(x => x.ExecuteFunctionBundlerAsync(this.simulatedDevice.DevEUI, It.IsNotNull<FunctionBundlerRequest>()))
                    .ReturnsAsync((DevEui _, FunctionBundlerRequest _) =>
                    {
-                       lock (this.functionLock)
+                       if (mode != DeduplicationMode.None)
                        {
-                           var isDup = messageProcessed;
-                           messageProcessed = true;
-                           return new FunctionBundlerResult()
+                           lock (this.functionLock)
                            {
-                               DeduplicationResult = new DeduplicationResult { IsDuplicate = isDup }
-                           };
+                               var isDup = messageProcessed;
+                               messageProcessed = true;
+                               return new FunctionBundlerResult()
+                               {
+                                   DeduplicationResult = new DeduplicationResult { IsDuplicate = isDup }
+                               };
+                           }
                        }
+
+                       // when DeduplicationMode is None, the Bundler deduplication is not invoked
+                       return new FunctionBundlerResult();
                    });
+
+                if (confirmedMessages)
+                {
+                    api.Setup(x => x.NextFCntDownAsync(It.IsAny<DevEui>(), It.IsAny<uint>(), It.IsAny<uint>(), It.IsAny<string>()))
+                       .ReturnsAsync(() =>
+                       {
+                           lock (this.functionLock)
+                           {
+                               counterFctnDown++;
+                               return counterFctnDown switch
+                               {
+                                   1 => this.simulatedDevice.FrmCntDown + 1, // the first time the message is encountered, it gets a valid frame counter down
+                                   _ => 0                                    // any other time, it does not
+                               };
+                           }
+                       });
+                }
 
                 api.Setup(x => x.ABPFcntCacheResetAsync(It.IsAny<DevEui>(), It.IsAny<uint>(), It.IsNotNull<string>()))
                    .ReturnsAsync(true);
@@ -64,8 +92,9 @@ namespace LoRaWan.Tests.Integration
                 .Setup(x => x.ReceiveAsync(It.IsAny<TimeSpan>()))
                 .ReturnsAsync((Message)null);
 
-            await SendTwoMessages(mode);
+            await SendTwoMessages(mode, confirmedMessages);
 
+            // upstream
             var actualCounts =
                 (NullCount: actualDeviceTelemetries.Count(t => t.DupMsg == null),
                  TrueCount: actualDeviceTelemetries.Count(t => t.DupMsg is { }),
@@ -80,11 +109,18 @@ namespace LoRaWan.Tests.Integration
             };
 
             Assert.Equal(expectedCounts, actualCounts);
+
+            // downstream
+            var sentDownstream = PacketForwarder.DownlinkMessages.Concat(SecondPacketForwarder.DownlinkMessages).ToArray();
+
+            if (confirmedMessages)
+                Assert.Single(sentDownstream);
+            else
+                Assert.Empty(sentDownstream);
         }
 
-        private async Task SendTwoMessages(DeduplicationMode mode)
+        private async Task SendTwoMessages(DeduplicationMode mode, bool confirmedMessages)
         {
-            var simulatedDevice = new SimulatedDevice(TestDeviceInfo.CreateABPDevice(1));
             var (messageProcessor1, dispose1) = CreateMessageDispatcher(ServerConfiguration, FrameCounterUpdateStrategyProvider, LoRaDeviceApi.Object);
             var (messageProcessor2, dispose2) = CreateMessageDispatcher(SecondServerConfiguration, SecondFrameCounterUpdateStrategyProvider, SecondLoRaDeviceApi.Object);
             using (messageProcessor1)
@@ -92,7 +128,7 @@ namespace LoRaWan.Tests.Integration
             using (messageProcessor2)
             using (dispose2)
             {
-                var payload = simulatedDevice.CreateUnconfirmedDataUpMessage("1234", fcnt: 1);
+                var payload = confirmedMessages ? this.simulatedDevice.CreateConfirmedDataUpMessage("1234", fcnt: 1) : this.simulatedDevice.CreateUnconfirmedDataUpMessage("1234", fcnt: 1);
 
                 using var request1 = CreateWaitableRequest(payload);
                 using var request2 = CreateWaitableRequest(payload);
@@ -113,7 +149,7 @@ namespace LoRaWan.Tests.Integration
                             true => (request1, request2),
                             false => (request2, request1)
                         };
-                        
+
                         Assert.True(succeededRequest.ProcessingSucceeded);
                         Assert.True(failedRequest.ProcessingFailed);
                         Assert.Equal(LoRaDeviceRequestFailedReason.DeduplicationDrop, failedRequest.ProcessingFailedReason);
@@ -138,7 +174,7 @@ namespace LoRaWan.Tests.Integration
                 var connectionManager = new LoRaDeviceClientConnectionManager(cache, new TestOutputLogger<LoRaDeviceClientConnectionManager>(this.testOutputHelper));
                 var concentratorDeduplication = new ConcentratorDeduplication(cache, new TestOutputLogger<IConcentratorDeduplication>(this.testOutputHelper));
                 var requestHandler = CreateDefaultLoRaDataRequestHandler(networkServerConfiguration, frameCounterUpdateStrategyProvider, loRaDeviceApi, concentratorDeduplication);
-                var loRaDevice = TestUtils.CreateFromSimulatedDevice(simulatedDevice, connectionManager, requestHandler);
+                var loRaDevice = TestUtils.CreateFromSimulatedDevice(this.simulatedDevice, connectionManager, requestHandler);
                 loRaDevice.Deduplication = mode;
                 connectionManager.Register(loRaDevice, LoRaDeviceClient.Object);
                 var loraDeviceCache = CreateDeviceCache(loRaDevice);

Tests/Integration/ProcessingTests.cs
@@ -12,7 +12,6 @@ namespace LoRaWan.Tests.Integration
     using LoRaTools.LoRaMessage;
     using LoRaWan.NetworkServer;
     using LoRaWan.Tests.Common;
-    using Microsoft.AspNetCore.Mvc.TagHelpers;
     using Microsoft.Azure.Devices.Client;
     using Microsoft.Azure.Devices.Shared;
     using Microsoft.Extensions.Caching.Memory;