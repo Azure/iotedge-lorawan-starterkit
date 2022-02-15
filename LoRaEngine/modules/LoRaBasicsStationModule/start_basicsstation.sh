6c7b2565a4bf6e084a2ba3d2949f513b6b528f7a
danigian <1955514+danigian@users.noreply.github.com>
2022-02-15 14:49:05 +0100

    Adjusting start_basicsstation script to include CORECELL environment variable

--- LoRaEngine/modules/LoRaBasicsStationModule/start_basicsstation.sh
+++ LoRaEngine/modules/LoRaBasicsStationModule/start_basicsstation.sh
@@ -3,7 +3,6 @@ set -e
 export MODULE_WORKDIR=$(cd $(dirname $0) && pwd)
 . "$MODULE_WORKDIR/helper-functions.sh"
 
-
 if [[ -z "$TC_URI" ]] && [[ -z "$CUPS_URI" ]]; then
     echo "One of CUPS_URI or TC_URI should be specified. Exiting..."
     exit 1
@@ -13,10 +12,17 @@ if [[ -z "$STATION_PATH" ]]; then
     STATION_PATH=/basicstation
 fi
 
+if [[ "$CORECELL" == true ]]; then
+    mv corecell.station.conf station.conf
+else
+    mv sx1301.station.conf station.conf
+fi
+
 resetPin
 setFixedStationEui
 conditionallySetupCups
 conditionallySetupTc
+setLogLevel
 
 if [[ -z "$RADIODEV" ]]; then
     if [[ -z "$SPI_DEV" ]] || [[ $SPI_DEV == '$LBS_SPI_DEV' ]]; then
@@ -28,15 +34,16 @@ fi
 
 #start basestation
 echo "Starting base station..."
-if [[ -z "$SPI_SPEED" ]] || [[ "$SPI_SPEED" == '$LBS_SPI_SPEED' ]] || [ "$SPI_SPEED" == "8" ]; then
-    echo "Spi speed set to 8 mbps"
+if [[ "$CORECELL" == true ]]; then
+    echo "Starting Corecell (SX1302) binary"
+    $STATION_PATH/station.corecell -f
+elif [[ -z "$SPI_SPEED" ]] || [[ "$SPI_SPEED" == '$LBS_SPI_SPEED' ]] || [ "$SPI_SPEED" == "8" ]; then
+    echo "Starting SX1301 binary with spi speed set to 8 mbps"
     $STATION_PATH/station.std -f
+elif [ "$SPI_SPEED" == "2" ]; then
+    echo "Starting SX1301 binary with spi speed set to 2 mbps"
+    $STATION_PATH/station.spispeed2 -f
 else
-    if [ "$SPI_SPEED" == "2" ]; then
-        echo "Spi speed set to 2 mbps"
-        $STATION_PATH/station.spispeed2 -f
-    else
-        echo "The value $SPI_SPEED is not supported as custom value. Supported values are 2 or 8"
-        exit 1;
-    fi
+    echo "The value $SPI_SPEED is not supported as custom value. Supported values are 2 or 8"
+    exit 1;
 fi
