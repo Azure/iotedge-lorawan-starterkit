adcf6d7277d75ff18d4267a59bf4187db746cbab
Maggie Salak <masalak@microsoft.com>
2022-02-23 14:15:47 +0100

    enhance the integration test with different gateways and fcnt

--- Tests/Integration/DeduplicationTestWithRedis.cs
+++ Tests/Integration/DeduplicationTestWithRedis.cs
@@ -8,11 +8,12 @@ namespace LoRaWan.Tests.Integration
     using LoraKeysManagerFacade;
     using LoraKeysManagerFacade.FunctionBundler;
     using LoRaWan.Tests.Common;
+    using Microsoft.Azure.Devices;
     using Moq;
     using Xunit;
 
     /// <summary>
-    /// Tests to run against a real redis instance.
+    /// Tests to run against a real Redis instance.
     /// </summary>
     [Collection(RedisFixture.CollectionName)]
     public class DeduplicationTestWithRedis : IClassFixture<RedisFixture>
@@ -31,13 +32,19 @@ namespace LoRaWan.Tests.Integration
         }
 
         [Theory]
-        [InlineData(1, 1)]
-        [InlineData(1, 2)]
-        public async Task When_Called_By_Multiple_Gateways_Should_Detect_Duplicates(uint fcntUp1, uint fcntUp2)
+        [InlineData("gateway1", 1, "gateway1", 1)]
+        [InlineData("gateway1", 1, "gateway1", 2)]
+        [InlineData("gateway1", 1, "gateway2", 1)]
+        [InlineData("gateway1", 1, "gateway2", 2)]
+        public async Task When_Called_Multiple_Times_With_Same_Device_Should_Detect_Duplicates(string gateway1, uint fcnt1, string gateway2, uint fcnt2)
         {
+            this.serviceClientMock.Setup(
+                x => x.InvokeDeviceMethodAsync(It.IsAny<string>(), LoraKeysManagerFacadeConstants.NetworkServerModuleId, It.IsAny<CloudToDeviceMethod>()))
+                .ReturnsAsync(new CloudToDeviceMethodResult() { Status = 200 });
+
             var devEUI = TestEui.GenerateDevEui();
 
-            var req1 = new FunctionBundlerRequest() { GatewayId = "gateway1", ClientFCntUp = fcntUp1, ClientFCntDown = fcntUp1 };
+            var req1 = new FunctionBundlerRequest() { GatewayId = gateway1, ClientFCntUp = fcnt1, ClientFCntDown = fcnt1 };
             var pipeline1 = new FunctionBundlerPipelineExecuter(new IFunctionBundlerExecutionItem[] { this.deduplicationExecutionItem }, devEUI, req1);
             var res1 = await this.deduplicationExecutionItem.ExecuteAsync(pipeline1);
 
@@ -45,20 +52,34 @@ namespace LoRaWan.Tests.Integration
             Assert.NotNull(pipeline1.Result.DeduplicationResult);
             Assert.False(pipeline1.Result.DeduplicationResult.IsDuplicate);
 
-            var req2 = new FunctionBundlerRequest() { GatewayId = "gateway2", ClientFCntUp = fcntUp2, ClientFCntDown = fcntUp2 };
+            var req2 = new FunctionBundlerRequest() { GatewayId = gateway2, ClientFCntUp = fcnt2, ClientFCntDown = fcnt2 };
             var pipeline2 = new FunctionBundlerPipelineExecuter(new IFunctionBundlerExecutionItem[] { this.deduplicationExecutionItem }, devEUI, req2);
             var res2 = await this.deduplicationExecutionItem.ExecuteAsync(pipeline2);
 
             Assert.NotNull(pipeline2.Result.DeduplicationResult);
-            if (fcntUp1 == fcntUp2)
+
+            // same gateway -> no duplicate
+            if (gateway1 == gateway2)
+            {
+                Assert.Equal(FunctionBundlerExecutionState.Continue, res2);
+                Assert.False(pipeline2.Result.DeduplicationResult.IsDuplicate);
+            }
+            // different gateway, the same fcnt -> duplicate
+            else if (fcnt1 == fcnt2)
             {
                 Assert.Equal(FunctionBundlerExecutionState.Abort, res2);
                 Assert.True(pipeline2.Result.DeduplicationResult.IsDuplicate);
             }
+            // different gateway, higher fcnt -> no duplicate
             else
             {
                 Assert.Equal(FunctionBundlerExecutionState.Continue, res2);
                 Assert.False(pipeline2.Result.DeduplicationResult.IsDuplicate);
+
+                // gateway1 should be notified that it needs to drop connection for the device
+                this.serviceClientMock.Verify(x => x.InvokeDeviceMethodAsync(gateway1, LoraKeysManagerFacadeConstants.NetworkServerModuleId,
+                    It.Is<CloudToDeviceMethod>(m => m.MethodName == LoraKeysManagerFacadeConstants.CloudToDeviceDropConnection
+                    && m.GetPayloadAsJson().Contains(devEUI.ToString()))));
             }
         }
 
