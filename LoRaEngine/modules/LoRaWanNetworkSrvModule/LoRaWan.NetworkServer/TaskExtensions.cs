a8e160f8a8170e6d3e52fe4289462799eec53a06
Bastian Burger <22341213+bastbu@users.noreply.github.com>
2022-02-28 18:56:27 +0100

    Add helper methods for task exception fetching

---
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/TaskExtensions.cs
@@ -0,0 +1,63 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#nullable enable
+
+namespace LoRaWan.NetworkServer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Threading.Tasks;
+
+    internal static class TaskExtensions
+    {
+        public static IList<Exception> GetExceptions(this IList<Task> tasks)
+        {
+            var result = new List<Exception>();
+
+            foreach (var task in tasks)
+            {
+                if (!task.IsCompleted)
+                {
+                    throw new InvalidOperationException("All tasks must have completed.");
+                }
+
+                if (task.IsCompletedSuccessfully) continue;
+
+                if (task.IsCanceled && task.TryGetCanceledException(out var ex))
+                    result.Add(ex);
+
+                if (task.IsFaulted && task.Exception is { } someException)
+                {
+                    result.Add(someException.InnerExceptions switch
+                    {
+                        { Count: 1 } => someException.InnerExceptions[0],
+                        _ => someException
+                    });
+                }
+            }
+
+            return result;
+        }
+
+        public static bool TryGetCanceledException(this Task task, [NotNullWhen(true)] out OperationCanceledException? exception)
+        {
+            exception = null;
+
+            if (!task.IsCanceled)
+                return false;
+
+            try
+            {
+                task.GetAwaiter().GetResult();
+                return false;
+            }
+            catch (OperationCanceledException ex)
+            {
+                exception = ex;
+                return true;
+            }
+        }
+    }
+}
