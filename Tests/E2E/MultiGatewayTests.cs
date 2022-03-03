f55fef7545268bf5657b1a2ee627766648ea391c
danigian <1955514+danigian@users.noreply.github.com>
2022-03-03 14:16:28 +0100

    Adjusting multigw tests

--- Tests/E2E/MultiGatewayTests.cs
+++ Tests/E2E/MultiGatewayTests.cs
@@ -22,11 +22,91 @@ namespace LoRaWan.Tests.E2E
         }
 
         [RetryFact]
-        public async Task Test_MultiGW_OTTA_Join_Single()
+        public Task Test_OTAA_Deduplication_Strategy_Drop()
         {
-            var device = TestFixtureCi.Device27_OTAA;
+            return Test_Deduplication_Strategies(TestFixtureCi.Device27_OTAA, true);
+        }
+
+        [RetryFact]
+        public Task Test_ABP_Deduplication_Strategy_Mark()
+        {
+            return Test_Deduplication_Strategies(TestFixtureCi.Device29_ABP, false);
+        }
+
+        [RetryFact]
+        public Task Test_ABP_Deduplication_Strategy_Drop()
+        {
+            return Test_Deduplication_Strategies(TestFixtureCi.Device28_ABP, false);
+        }
+
+        private async Task Test_Deduplication_Strategies(TestDeviceInfo device, bool isOtaa)
+        {
             LogTestStart(device);
 
+            if (isOtaa)
+            {
+                await OtaaSetupAndJoinAssertAsync(device);
+            }
+            else
+            {
+                await ArduinoDevice.setDeviceModeAsync(LoRaArduinoSerial._device_mode_t.LWABP);
+                await ArduinoDevice.setIdAsync(device.DevAddr, device.DeviceID, null);
+                await ArduinoDevice.setKeyAsync(device.NwkSKey, device.AppSKey, null);
+
+                await ArduinoDevice.SetupLora(TestFixtureCi.Configuration);
+            }
+
+            for (var i = 0; i < 10; i++)
+            {
+                var msg = PayloadGenerator.Next().ToString(CultureInfo.InvariantCulture);
+                await ArduinoDevice.transferPacketWithConfirmedAsync(msg, 10);
+                await Task.Delay(Constants.DELAY_FOR_SERIAL_AFTER_SENDING_PACKET);
+
+                // After transferPacket: Expectation from serial
+                // +CMSG: ACK Received
+                await AssertUtils.ContainsWithRetriesAsync("+CMSG: ACK Received", ArduinoDevice.SerialLogs);
+
+                var allGwGotIt = await TestFixtureCi.ValidateMultiGatewaySources((log) => log.IndexOf($"deduplication Strategy: {device.Deduplication}", StringComparison.OrdinalIgnoreCase) != -1);
+                if (allGwGotIt)
+                {
+                    var notDuplicate = "\"IsDuplicate\":false";
+                    var isDuplicate = "\"IsDuplicate\":true";
+
+                    var notDuplicateResult = await TestFixtureCi.SearchNetworkServerModuleAsync((s) => s.IndexOf(notDuplicate, StringComparison.Ordinal) != -1, new SearchLogOptions(notDuplicate));
+                    var duplicateResult = await TestFixtureCi.SearchNetworkServerModuleAsync((s) => s.IndexOf(isDuplicate, StringComparison.Ordinal) != -1, new SearchLogOptions(isDuplicate));
+
+                    Assert.NotNull(notDuplicateResult.MatchedEvent);
+                    Assert.NotNull(duplicateResult.MatchedEvent);
+
+                    Assert.NotEqual(duplicateResult.MatchedEvent.SourceId, notDuplicateResult.MatchedEvent.SourceId);
+
+                    switch (device.Deduplication)
+                    {
+                        case NetworkServer.DeduplicationMode.Mark:
+                            var expectedProperty = "dupmsg";
+                            await TestFixture.AssertIoTHubDeviceMessageExistsAsync(device.DeviceID, expectedProperty, "true", new SearchLogOptions(expectedProperty) { SourceIdFilter = duplicateResult.MatchedEvent.SourceId, TreatAsError = true });
+                            await TestFixture.AssertIoTHubDeviceMessageExistsAsync(device.DeviceID, expectedProperty, "true", new SearchLogOptions(expectedProperty) { SourceIdFilter = notDuplicateResult.MatchedEvent.SourceId, TreatAsError = true });
+                            break;
+                        case NetworkServer.DeduplicationMode.Drop:
+                            var logMsg = $"{device.DeviceID}: duplication strategy indicated to not process message";
+                            var droppedLog = await TestFixtureCi.SearchNetworkServerModuleAsync((log) => log.StartsWith(logMsg, StringComparison.Ordinal), new SearchLogOptions(logMsg) { SourceIdFilter = duplicateResult.MatchedEvent.SourceId });
+                            Assert.NotNull(droppedLog.MatchedEvent);
+
+                            var expectedPayload = $"{{\"value\":{msg}}}";
+                            await TestFixtureCi.AssertIoTHubDeviceMessageExistsAsync(device.DeviceID, expectedPayload, new SearchLogOptions(expectedPayload) { TreatAsError = true });
+                            break;
+                        case NetworkServer.DeduplicationMode.None:
+                        default:
+                            throw new SwitchExpressionException();
+                    }
+                }
+
+                TestFixtureCi.ClearLogs();
+            }
+        }
+
+        private async Task OtaaSetupAndJoinAssertAsync(TestDeviceInfo device)
+        {
             await ArduinoDevice.setDeviceModeAsync(LoRaArduinoSerial._device_mode_t.LWOTAA);
             await ArduinoDevice.setIdAsync(device.DevAddr, device.DeviceID, device.AppEui);
             await ArduinoDevice.setKeyAsync(device.NwkSKey, device.AppSKey, device.AppKey);
@@ -44,103 +124,6 @@ namespace LoRaWan.Tests.E2E
             Assert.True(joinRefused.Found);
 
             await TestFixtureCi.WaitForTwinSyncAfterJoinAsync(ArduinoDevice.SerialLogs, device.DevEui);
-
-            // expecting both gw to start picking up messages
-            // and sending to IoT hub.
-            var bothReported = false;
-            for (var i = 0; i < 5; i++)
-            {
-                var msg = PayloadGenerator.Next().ToString(CultureInfo.InvariantCulture);
-                await ArduinoDevice.transferPacketAsync(msg, 10);
-
-                await Task.Delay(Constants.DELAY_FOR_SERIAL_AFTER_SENDING_PACKET);
-
-                // After transferPacket: Expectation from serial
-                // +MSG: Done
-                await AssertUtils.ContainsWithRetriesAsync("+MSG: Done", ArduinoDevice.SerialLogs);
-
-                var expectedPayload = $"{{\"value\":{msg}}}";
-                await TestFixtureCi.AssertIoTHubDeviceMessageExistsAsync(device.DeviceID, expectedPayload, new SearchLogOptions(expectedPayload));
-
-                bothReported = await TestFixtureCi.ValidateMultiGatewaySources((log) => log.StartsWith($"{device.DeviceID}: sending message", StringComparison.OrdinalIgnoreCase));
-                if (bothReported)
-                {
-                    break;
-                }
-            }
-
-            Assert.True(bothReported);
-        }
-
-        [RetryFact]
-        public Task Test_Deduplication_Strategies_Mark()
-        {
-            return Test_Deduplication_Strategies("Device29_ABP", "Mark");
-        }
-
-        [RetryFact]
-        public Task Test_Deduplication_Strategies_Drop()
-        {
-            return Test_Deduplication_Strategies("Device28_ABP", "Drop");
-        }
-
-        private async Task Test_Deduplication_Strategies(string devicePropertyName, string strategy)
-        {
-            var device = TestFixtureCi.GetDeviceByPropertyName(devicePropertyName);
-            LogTestStart(device);
-
-            await ArduinoDevice.setDeviceModeAsync(LoRaArduinoSerial._device_mode_t.LWABP);
-            await ArduinoDevice.setIdAsync(device.DevAddr, device.DeviceID, null);
-            await ArduinoDevice.setKeyAsync(device.NwkSKey, device.AppSKey, null);
-
-            await ArduinoDevice.SetupLora(TestFixtureCi.Configuration);
-
-            for (var i = 0; i < 10; i++)
-            {
-                var msg = PayloadGenerator.Next().ToString(CultureInfo.InvariantCulture);
-                await ArduinoDevice.transferPacketWithConfirmedAsync(msg, 10);
-                await Task.Delay(Constants.DELAY_FOR_SERIAL_AFTER_SENDING_PACKET);
-
-                // After transferPacket: Expectation from serial
-                // +CMSG: ACK Received
-                await AssertUtils.ContainsWithRetriesAsync("+CMSG: ACK Received", ArduinoDevice.SerialLogs);
-
-                var allGwGotIt = await TestFixtureCi.ValidateMultiGatewaySources((log) => log.IndexOf($"deduplication Strategy: {strategy}", StringComparison.OrdinalIgnoreCase) != -1);
-                if (allGwGotIt)
-                {
-                    var notDuplicate = "\"IsDuplicate\":false";
-                    var isDuplicate = "\"IsDuplicate\":true";
-
-                    var notDuplicateResult = await TestFixtureCi.SearchNetworkServerModuleAsync((s) => s.IndexOf(notDuplicate, StringComparison.Ordinal) != -1, new SearchLogOptions(notDuplicate));
-                    var duplicateResult = await TestFixtureCi.SearchNetworkServerModuleAsync((s) => s.IndexOf(isDuplicate, StringComparison.Ordinal) != -1, new SearchLogOptions(isDuplicate));
-
-                    Assert.NotNull(notDuplicateResult.MatchedEvent);
-                    Assert.NotNull(duplicateResult.MatchedEvent);
-
-                    Assert.NotEqual(duplicateResult.MatchedEvent.SourceId, notDuplicateResult.MatchedEvent.SourceId);
-
-                    switch (strategy)
-                    {
-                        case "Mark":
-                            var expectedProperty = "dupmsg";
-                            await TestFixture.AssertIoTHubDeviceMessageExistsAsync(device.DeviceID, expectedProperty, "true", new SearchLogOptions(expectedProperty) { SourceIdFilter = duplicateResult.MatchedEvent.SourceId, TreatAsError = true });
-                            await TestFixture.AssertIoTHubDeviceMessageExistsAsync(device.DeviceID, expectedProperty, "true", new SearchLogOptions(expectedProperty) { SourceIdFilter = notDuplicateResult.MatchedEvent.SourceId, TreatAsError = true });
-                            break;
-                        case "Drop":
-                            var logMsg = $"{device.DeviceID}: duplication strategy indicated to not process message";
-                            var droppedLog = await TestFixtureCi.SearchNetworkServerModuleAsync((log) => log.StartsWith(logMsg, StringComparison.Ordinal), new SearchLogOptions(logMsg) { SourceIdFilter = duplicateResult.MatchedEvent.SourceId });
-                            Assert.NotNull(droppedLog.MatchedEvent);
-
-                            var expectedPayload = $"{{\"value\":{msg}}}";
-                            await TestFixtureCi.AssertIoTHubDeviceMessageExistsAsync(device.DeviceID, expectedPayload, new SearchLogOptions(expectedPayload) { TreatAsError = true });
-                            break;
-                        default:
-                            throw new SwitchExpressionException();
-                    }
-                }
-
-                TestFixtureCi.ClearLogs();
-            }
         }
     }
 }
