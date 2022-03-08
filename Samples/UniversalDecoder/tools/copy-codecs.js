c759267419b20ec705c2af9bbf5c7b3c2b736b3c
Atif Aziz <code@raboof.com>
2022-03-08 12:12:03 +0100

    Build decoders list for static loading in Universal Decoders

--- Samples/UniversalDecoder/tools/copy-codecs.js
+++ Samples/UniversalDecoder/tools/copy-codecs.js
@@ -3,12 +3,15 @@
 const glob = require('glob');
 const fs = require('fs');
 const fse = require('fs-extra');
+const path = require('path');
+const esprima = require('esprima');
 
 var args = process.argv.slice(2);
 const srcDir = args[0] || './node_modules/lorawan-devices/vendor';
 const dstDir = args[1] || './codecs';
+const indexFilePath = path.join(dstDir, '../decoders.js');
 
-glob.sync(`**/*`,
+const index = glob.sync(`**/*`,
   {
     cwd: srcDir,
     nodir: true,
@@ -25,9 +28,18 @@ glob.sync(`**/*`,
     fse.copySync(srcPath, dstPath);
 
     if (f.endsWith(".js")) {
-      fs.appendFile(dstPath, '\nmodule.exports={decodeUplink};', function (err) {
-        if (err) throw err;
-        console.log(`Patching ${dstPath}`);
-      });
+      const tree = esprima.parseScript(fs.readFileSync(srcPath).toString());
+      if (tree.body.find(n => n.type === 'FunctionDeclaration' && n.id.name === 'decodeUplink')) {
+        fs.appendFile(dstPath, '\nmodule.exports={decodeUplink};', function (err) {
+          if (err) throw err;
+          console.log(`Patching ${dstPath}`);
+        });
+        return dstPath;
+      }
     }
-});
+  })
+  .filter(f => f)
+  .map(f => [path.basename(f).split('.')[0], f]);
+
+fs.writeFileSync(indexFilePath,
+  `module.exports = {\n${index.map(([k, v]) => `${JSON.stringify(k)}: require(${JSON.stringify(v)})`).join(',\n  ')}\n};\n`);
