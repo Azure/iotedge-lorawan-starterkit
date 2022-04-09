6b749d1cccc9fd1c5d92c211822ad541fcfba09a
Kevin BEAUGRAND <9513635+kbeaugrand@users.noreply.github.com>
2022-04-09 14:17:45 +0200

    Simplify statements in IoTHubRegistryManager

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/LoraTools/IoTHubRegistryManager.cs
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoraTools/IoTHubRegistryManager.cs
@@ -25,84 +25,43 @@ namespace LoRaTools
             this.instance = registryManagerProvider() ?? throw new InvalidOperationException("RegistryManager provider provided a null RegistryManager.");
         }
 
-        public Task<Configuration> AddConfigurationAsync(Configuration configuration)
-        {
-            return this.instance.AddConfigurationAsync(configuration);
-        }
+        public Task<Configuration> AddConfigurationAsync(Configuration configuration) => this.instance.AddConfigurationAsync(configuration);
 
-        public Task<Device> AddDeviceAsync(Device edgeGatewayDevice)
-        {
-            return this.instance.AddDeviceAsync(edgeGatewayDevice);
-        }
+        public Task<Device> AddDeviceAsync(Device edgeGatewayDevice) => this.instance.AddDeviceAsync(edgeGatewayDevice);
 
-        public Task<BulkRegistryOperationResult> AddDeviceWithTwinAsync(Device device, Twin twin)
-        {
-            return this.instance.AddDeviceWithTwinAsync(device, twin);
-        }
+        public Task<BulkRegistryOperationResult> AddDeviceWithTwinAsync(Device device, Twin twin) => this.instance.AddDeviceWithTwinAsync(device, twin);
 
-        public Task<Module> AddModuleAsync(Module moduleToAdd)
-        {
-            return this.instance.AddModuleAsync(moduleToAdd);
-        }
+        public Task<Module> AddModuleAsync(Module moduleToAdd) => this.instance.AddModuleAsync(moduleToAdd);
 
         public Task ApplyConfigurationContentOnDeviceAsync(string deviceName, ConfigurationContent deviceConfigurationContent)
-        {
-            return this.instance.ApplyConfigurationContentOnDeviceAsync(deviceName, deviceConfigurationContent);
-        }
+            => this.instance.ApplyConfigurationContentOnDeviceAsync(deviceName, deviceConfigurationContent);
 
-        public IQuery CreateQuery(string query)
-        {
-            return this.instance.CreateQuery(query);
-        }
+        public IQuery CreateQuery(string query) => this.instance.CreateQuery(query);
 
-        public IQuery CreateQuery(string query, int? pageSize)
-        {
-            return this.instance.CreateQuery(query, pageSize);
-        }
+        public IQuery CreateQuery(string query, int? pageSize) => this.instance.CreateQuery(query, pageSize);
 
-        public void Dispose()
-        {
-            this.instance?.Dispose();
-        }
+        public void Dispose() => this.instance?.Dispose();
 
-        public Task<Device> GetDeviceAsync(string deviceId)
-        {
-            return this.instance.GetDeviceAsync(deviceId);
-        }
+        public Task<Device> GetDeviceAsync(string deviceId) => this.instance.GetDeviceAsync(deviceId);
 
         public Task<Twin> GetTwinAsync(string deviceId, CancellationToken cancellationToken)
-        {
-            return this.instance.GetTwinAsync(deviceId, cancellationToken);
-        }
+            => this.instance.GetTwinAsync(deviceId, cancellationToken);
 
-        public Task<Twin> GetTwinAsync(string deviceName)
-        {
-            return this.instance.GetTwinAsync(deviceName);
-        }
+        public Task<Twin> GetTwinAsync(string deviceName) => this.instance.GetTwinAsync(deviceName);
 
         public Task<Twin> UpdateTwinAsync(string deviceId, string moduleId, Twin deviceTwin, string eTag)
-        {
-            return this.instance.UpdateTwinAsync(deviceId, moduleId, deviceTwin, eTag);
-        }
+            => this.instance.UpdateTwinAsync(deviceId, moduleId, deviceTwin, eTag);
 
         public Task<Twin> UpdateTwinAsync(string deviceId, Twin twin, string eTag, CancellationToken cancellationToken)
-        {
-            return this.instance.UpdateTwinAsync(deviceId, twin, eTag, cancellationToken);
-        }
+            => this.instance.UpdateTwinAsync(deviceId, twin, eTag, cancellationToken);
 
         public Task<Twin> UpdateTwinAsync(string deviceName, Twin twin, string eTag)
-        {
-            return this.instance.UpdateTwinAsync(deviceName, twin, eTag);
-        }
+            => this.instance.UpdateTwinAsync(deviceName, twin, eTag);
 
         public Task<Twin> UpdateTwinAsync(string deviceId, string moduleId, Twin deviceTwin, string eTag, CancellationToken cancellationToken)
-        {
-            return this.instance.UpdateTwinAsync(deviceId, moduleId, deviceTwin, eTag, cancellationToken);
-        }
+            => this.instance.UpdateTwinAsync(deviceId, moduleId, deviceTwin, eTag, cancellationToken);
 
         public Task RemoveDeviceAsync(string deviceId)
-        {
-            return this.instance.RemoveDeviceAsync(deviceId);
-        }
+            => this.instance.RemoveDeviceAsync(deviceId);
     }
 }
