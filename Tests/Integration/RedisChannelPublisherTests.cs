c25ba03075174cf674bc4772bfd9e0f4b9050978
Bastian Burger <22341213+bastbu@users.noreply.github.com>
2022-05-17 14:04:07 -0700

    Add assertions to Publish_Async tests

--- Tests/Integration/RedisChannelPublisherTests.cs
+++ Tests/Integration/RedisChannelPublisherTests.cs
@@ -10,26 +10,38 @@ namespace LoRaWan.Tests.Integration
     using LoraKeysManagerFacade;
     using Xunit.Abstractions;
     using Microsoft.Extensions.Logging.Abstractions;
+    using StackExchange.Redis;
+    using Moq;
 
     public class RedisChannelPublisherTests : FunctionTestBase, IClassFixture<RedisFixture>
     {
         private readonly IChannelPublisher channelPublisher;
         private readonly ITestOutputHelper testOutputHelper;
+        private readonly ConnectionMultiplexer redis;
 
         public RedisChannelPublisherTests(RedisFixture redis, ITestOutputHelper testOutputHelper)
         {
             if (redis is null) throw new ArgumentNullException(nameof(redis));
             this.channelPublisher = new RedisChannelPublisher(redis.Redis, NullLogger<RedisChannelPublisher>.Instance);
             this.testOutputHelper = testOutputHelper;
+            this.redis = redis.Redis;
         }
 
         [Fact]
         public async Task Publish_Aysnc()
         {
+            // arrange
             var message = "test message";
             var channel = "channel1";
-            Console.WriteLine("Publishing message...");
+            var assert = new Mock<Action<ChannelMessage>>();
+            this.testOutputHelper.WriteLine("Publishing message...");
+            (await this.redis.GetSubscriber().SubscribeAsync(channel)).OnMessage(assert.Object);
+
+            // act
             await this.channelPublisher.PublishAsync(channel, message);
+
+            // assert
+            assert.Verify(a => a.Invoke(It.Is<ChannelMessage>(actual => actual.Message == message)), Times.Once);
         }
     }
 }
