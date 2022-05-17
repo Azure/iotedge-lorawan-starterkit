b4885396c631424f9977480eacd3ddb438206f49
mkcomer <61518615+mkcomer@users.noreply.github.com>
2022-05-17 15:17:44 -0700

    Update LoRaEngine/LoraKeysManagerFacade/ RedisChannelPublisher.cs
    Co-authored-by: Daniele Antonio Maggio <1955514+danigian@users.noreply.github.com>

--- LoRaEngine/LoraKeysManagerFacade/ RedisChannelPublisher.cs
+++ LoRaEngine/LoraKeysManagerFacade/ RedisChannelPublisher.cs
@@ -12,20 +12,18 @@ namespace LoraKeysManagerFacade
     public class RedisChannelPublisher : IChannelPublisher
     {
         private readonly ConnectionMultiplexer redis;
-        private readonly ISubscriber subscriber;
         private readonly ILogger logger;
 
         public RedisChannelPublisher(ConnectionMultiplexer redis, ILogger<RedisChannelPublisher> logger)
         {
             this.redis = redis;
             this.logger = logger;
-            this.subscriber = this.redis.GetSubscriber();
         }
 
         public async Task PublishAsync(string channel, LnsRemoteCall lnsRemoteCall)
         {
             this.logger.LogDebug("Publishing message to channel '{Channel}'.", channel);
-            _ = await this.subscriber.PublishAsync(channel, JsonSerializer.Serialize(lnsRemoteCall));
+            _ = await this.redis.GetSubscriber().PublishAsync(channel, JsonSerializer.Serialize(lnsRemoteCall));
         }
     }
 }
