75b6fca19ed71467fc459634445e4793391c4077
danigian <1955514+danigian@users.noreply.github.com>
2022-05-17 13:56:03 -0700

    Adding empty CloudControlHost

---
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/CloudControlHost.cs
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace LoRaWan.NetworkServer
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Hosting;
+
+    internal class CloudControlHost : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
