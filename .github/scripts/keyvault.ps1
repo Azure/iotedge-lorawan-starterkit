d961795a2c10baf3dfc6c6e9a949bc28cd9621f9
Nora Abi Akar <noraabiakar@microsoft.com>
2022-10-19 16:15:08 +0200

    add az cli login to each stage in the CI and update keyvault reading script

--- .github/scripts/keyvault.ps1
+++ .github/scripts/keyvault.ps1
@@ -1,10 +1,21 @@
+using namespace System.Collections.Concurrent
+
 param(
   [array]$array
 )
 
+$map = [ConcurrentDictionary[string, object]]::new()
+
 $array | ForEach-Object -Parallel {
   $key = $_
   $val = az keyvault secret show --name $key --vault-name "nabki-keyvault" --query value -o tsv 
-  "$key = $val" >> $env:GITHUB_ENV
-  Write-Host("::add-mask::$val")
+  $collector = $using:map
+  $res = $collector.TryAdd($key, $val)
 } -ThrottleLimit 10
+
+foreach($key in $map.Keys)
+{
+  $val = $map[$key]
+  "$key= $val" >> $env:GITHUB_ENV
+  Write-Host("::add-mask::$val")
+}
