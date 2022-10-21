dffa09553e1ac2780582eccafd903fd299721663
Nora Abi Akar <noraabiakar@microsoft.com>
2022-10-21 11:04:42 +0200

    temporarily disable deployment deletion

--- .github/actions/iotedgedev/entrypoint.sh
+++ .github/actions/iotedgedev/entrypoint.sh
@@ -4,6 +4,6 @@
 # then the file proceed to remove the previous deployment and replace it with the current one
 sudo -E az extension add --name azure-iot
 sudo -E iotedgedev genconfig -f $2/$1 -P $3  --fail-on-validation-error
-sudo -E az iot edge deployment delete --login "$IOTHUB_CONNECTION_STRING" --deployment-id "$IOT_EDGE_DEPLOYMENT_ID"
+# sudo -E az iot edge deployment delete --login "$IOTHUB_CONNECTION_STRING" --deployment-id "$IOT_EDGE_DEPLOYMENT_ID"
 sudo -E az iot edge deployment create --login "$IOTHUB_CONNECTION_STRING" --content "config/${1//'.template'}" --deployment-id "$IOT_EDGE_DEPLOYMENT_ID" --target-condition "deviceId='$DEVICE_ID'"
 
