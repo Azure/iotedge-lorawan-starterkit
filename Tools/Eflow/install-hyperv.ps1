88a28206e90d90010cb9608060efb2e6180213ee
Nora Abi Akar <noraabiakar@microsoft.com>
2022-10-28 11:53:39 +0200

    cleanup eflow sccripts

---
+++ Tools/Eflow/install-hyperv.ps1
@@ -0,0 +1,15 @@
+<# .SYNOPSIS
+    Hyper-V installation Script
+.DESCRIPTION
+    This script automatically installs Hyper-V on a Windows Server VM to allow eflow provisioning.
+    The VM requires a restart after this script is run before eflow can be provisioned.
+.NOTES
+     Author     :
+        Mikhail Chatillon       - chmikhai@microsoft.com
+        Daniele Antonio Maggio  - daniele.maggio@microsoft.com
+#>
+
+# Install Hyper-V
+Install-WindowsFeature -Name Hyper-V
+Install-WindowsFeature -Name Hyper-V-PowerShell
+Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -All -NoRestart
